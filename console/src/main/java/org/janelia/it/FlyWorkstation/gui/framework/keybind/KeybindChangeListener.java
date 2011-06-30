06bd65afa7edf5e3034a82539a00e2de1d32a25b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-06-30 15:00:42 +0000

    javadocs
    Former-commit-id: 7f093b3dc4f04311b4aee432a5bd724b5afbe66c

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/keybind/KeybindChangeListener.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/keybind/KeybindChangeListener.java
@@ -1,6 +1,10 @@
 package org.janelia.it.FlyWorkstation.gui.framework.keybind;
 
-
+/**
+ * Listener for key binding events.
+ *
+ * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
+ */
 public interface KeybindChangeListener extends java.util.EventListener {
     /**
      * This method gets called when a key binding is added, removed or when
