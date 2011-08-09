e4215f91b881b17916d7e1e1c8001e0513328be9
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-08-09 15:32:36 +0000

    add modelmgr initialization
    Former-commit-id: e22611c1a9ebbe71c060ca968a781384ad1b9b92

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/dataview/DataviewApp.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/dataview/DataviewApp.java
@@ -8,6 +8,10 @@ package org.janelia.it.FlyWorkstation.gui.dataview;
 
 import javax.swing.*;
 
+import org.janelia.it.FlyWorkstation.api.entity_model.management.ModelMgr;
+import org.janelia.it.FlyWorkstation.api.facade.concrete_facade.ejb.EJBFacadeManager;
+import org.janelia.it.FlyWorkstation.api.facade.facade_mgr.FacadeManager;
+
 /**
  * An graphical interface for viewing the Entity model: EntityTypes, EntityAttributes, Entities and EntityData.
  *
@@ -26,10 +30,15 @@ public class DataviewApp {
     }
 
     private static void newDataviewer() throws Exception {
+    	
+        final ModelMgr modelMgr = ModelMgr.getModelMgr();
+        modelMgr.registerFacadeManagerForProtocol(FacadeManager.getEJBProtocolString(), EJBFacadeManager.class, "JACS EJB Facade Manager");
+        
         mainFrame = new DataviewFrame();
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setTitle("JACS Data Viewer");
         mainFrame.pack();
         mainFrame.setVisible(true);
+        
     }
 }
