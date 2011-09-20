e69191ec910f5f7eecae2b28c8fcfb103fb4778b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-09-20 15:57:27 +0000

    renamed LazyTreeNodeExpansionWorker to the more accurate name LazyTreeNodeLoader added ability to retain expansion state when loading additional nodes
    Former-commit-id: f2820a9179e19b046ebb69343213bdcb319fef47

---
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/tree/LazyTreeNodeLoader.java
@@ -0,0 +1,88 @@
+package org.janelia.it.FlyWorkstation.gui.framework.tree;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+
+import org.janelia.it.FlyWorkstation.gui.util.SimpleWorker;
+
+/**
+ * A worker thread which loads the children or ancestors for a node in a DynamicTree. 
+ * 
+ * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
+ */
+public class LazyTreeNodeLoader extends SimpleWorker {
+
+	private final Set<String> expanded = new HashSet<String>();
+	
+    private final DynamicTree dynamicTree;
+    private final DefaultMutableTreeNode node;
+    private final boolean recurse;
+
+    public LazyTreeNodeLoader(DynamicTree dynamicTree, DefaultMutableTreeNode node, boolean recurse) {
+        this.dynamicTree = dynamicTree;
+        this.node = node;
+        this.recurse = recurse;
+        storeExpansionState(node);
+    }
+
+    @Override
+    protected void doStuff() throws Exception {
+        dynamicTree.loadLazyNodeData(node, recurse);
+    }
+
+    @Override
+    protected void hadSuccess() {
+        dynamicTree.recreateChildNodes(node, recurse);
+        restoreExpansionState(node);
+        doneLoading();
+    }
+
+    @Override
+    protected void hadError(Throwable error) {
+        error.printStackTrace();
+    }
+
+    /**
+     * Override this method to do something once the nodes have been loaded. Called in the EDT.
+     */
+    protected void doneLoading() {
+    }
+    
+    private void storeExpansionState(DefaultMutableTreeNode node) {
+    	if (dynamicTree.getTree().isExpanded(new TreePath(node.getPath()))) {
+    		expanded.add(getPath(node));
+    	}
+
+        for (Enumeration e = node.children(); e.hasMoreElements(); ) {
+            DefaultMutableTreeNode childNode = (DefaultMutableTreeNode) e.nextElement();
+            storeExpansionState(childNode);
+        }
+    }
+    
+    private void restoreExpansionState(DefaultMutableTreeNode node) {
+    	if (expanded.contains(getPath(node))) {
+    		dynamicTree.expand(node, true);
+    	}
+
+        for (Enumeration e = node.children(); e.hasMoreElements(); ) {
+            DefaultMutableTreeNode childNode = (DefaultMutableTreeNode) e.nextElement();
+            restoreExpansionState(childNode);
+        }
+    }
+    
+    private String getPath(DefaultMutableTreeNode node) {
+    	StringBuffer sb = new StringBuffer();
+    	TreeNode curr = node;
+    	while(curr != null) {
+    		if (node != curr) sb.insert(0, "/");
+    		sb.insert(0, curr.toString());
+    		curr = curr.getParent();
+    	}
+    	return sb.toString();
+    }
+}
