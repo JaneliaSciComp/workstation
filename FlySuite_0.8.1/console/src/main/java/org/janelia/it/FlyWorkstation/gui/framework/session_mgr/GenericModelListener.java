b622d3c2334c367f945f421457f01f1373f2a56d
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-07-26 14:05:34 +0000

    Former-commit-id: 62fab4780c2771bf11f043cc29ee54d94f0d4188

--- FlySuite_0.8.1/console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/session_mgr/GenericModelListener.java
+++
@@ -1,7 +0,0 @@
-package org.janelia.it.FlyWorkstation.gui.framework.session_mgr;
-
-public interface GenericModelListener {
-
-    public void modelPropertyChanged(Object key, Object oldValue, Object newValue);
-
-}
