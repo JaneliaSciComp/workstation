d5af8dba9e87e733e5eca95b7173e34ff48a8f2b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-08-01 23:42:49 +0000

    allow eval annotation from anywhere added manual organization on button press
    Former-commit-id: eefc96213b171cbf3128be0d1c66c035f02c9713

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/ScreenEvaluationDialog.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/ScreenEvaluationDialog.java
@@ -1,25 +1,22 @@
 package org.janelia.it.FlyWorkstation.gui.dialogs;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
 
 import loci.plugins.config.SpringUtilities;
 
 import org.janelia.it.FlyWorkstation.api.entity_model.access.ModelMgrAdapter;
 import org.janelia.it.FlyWorkstation.api.entity_model.management.ModelMgr;
 import org.janelia.it.FlyWorkstation.gui.framework.outline.Annotations;
-import org.janelia.it.FlyWorkstation.gui.framework.outline.EntityOutline;
 import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr;
+import org.janelia.it.FlyWorkstation.gui.framework.viewer.RootedEntity;
 import org.janelia.it.FlyWorkstation.gui.util.SimpleWorker;
 import org.janelia.it.FlyWorkstation.shared.util.ModelMgrUtils;
 import org.janelia.it.jacs.model.entity.Entity;
@@ -37,10 +34,10 @@ import org.janelia.it.jacs.shared.utils.EntityUtils;
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
 public class ScreenEvaluationDialog extends ModalDialog {
-
-	private static final boolean DEBUG = false;
 	
 	private static final String SCORE_ONTOLOGY_NAME = "Expression Pattern Evaluation";
+	private static final String TOP_LEVEL_FOLDER_NAME = "FlyLight Pattern Evaluation";
+	
 	private static final String MAA_INTENSITY_NAME = "MAA Intensity Score";
 	private static final String MAA_DISTRIBUTION_NAME = "MAA Distribution Score";
 	private static final String CA_INTENSITY_NAME = "CA Intensity Score";
@@ -49,20 +46,27 @@ public class ScreenEvaluationDialog extends ModalDialog {
 	private static final String TOOLTIP_AUTO_MOVE = "Automatically move samples to the correct evaluation folder after annotation";
 	private static final String TOOLTIP_MOVE_NOW = "Move samples in the current folder to the correct folders";
 	
-	private JButton okButton;
 	private JButton cancelButton;
 	private JCheckBox autoMoveCheckbox;
 	private JButton moveButton;
-    	
+
+	private Map<String,Entity> compEntityMap = new HashMap<String,Entity>();
+	private Map<String,Entity> folderCache = new HashMap<String,Entity>();
+	private Map<String,Map<Long,String>> reverseFolderCache = new HashMap<String,Map<Long,String>>();
+	
 	public ScreenEvaluationDialog() {
 
+		if (!isAccessible()) return;
+
+		setModalityType(ModalityType.MODELESS);
+		
 		JPanel attrPanel = new JPanel(new SpringLayout());
         attrPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
 
         JLabel autoMoveLabel = new JLabel("Automatically move samples upon annotation?");
         autoMoveLabel.setToolTipText(TOOLTIP_AUTO_MOVE);
         autoMoveCheckbox = new JCheckBox();
-        autoMoveCheckbox.setSelected(true);
+        autoMoveCheckbox.setSelected(false);
         autoMoveCheckbox.setToolTipText(TOOLTIP_AUTO_MOVE);
         autoMoveCheckbox.addActionListener(new ActionListener() {
 			@Override
@@ -77,7 +81,7 @@ public class ScreenEvaluationDialog extends ModalDialog {
         JLabel moveNowLabel = new JLabel("Move samples in current folder now");
         moveNowLabel.setToolTipText(TOOLTIP_MOVE_NOW);
         moveButton = new JButton("Move");
-        moveButton.setEnabled(false);
+        moveButton.setEnabled(true);
         moveButton.setToolTipText(TOOLTIP_MOVE_NOW);
         moveButton.addActionListener(new ActionListener() {
 			@Override
@@ -92,17 +96,8 @@ public class ScreenEvaluationDialog extends ModalDialog {
         add(attrPanel, BorderLayout.CENTER);
         SpringUtilities.makeCompactGrid(attrPanel, attrPanel.getComponentCount()/2, 2, 6, 6, 6, 6);
 		
-        okButton = new JButton("OK");
-        okButton.setToolTipText("Close and save changes");
-        okButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-	            setVisible(false);
-			}
-		});
-
-        cancelButton = new JButton("Cancel");
-        cancelButton.setToolTipText("Close without saving changes");
+        cancelButton = new JButton("Close");
+        cancelButton.setToolTipText("Close this dialog");
         cancelButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -114,12 +109,48 @@ public class ScreenEvaluationDialog extends ModalDialog {
         buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
         buttonPane.setBorder(BorderFactory.createEmptyBorder(20, 10, 10, 10));
         buttonPane.add(Box.createHorizontalGlue());
-        buttonPane.add(okButton);
-        buttonPane.add(Box.createRigidArea(new Dimension(10, 0)));
         buttonPane.add(cancelButton);
         add(buttonPane, BorderLayout.SOUTH);
         
+        init();
         addListeners();
+	}
+
+	public void init() {
+
+		SimpleWorker worker = new SimpleWorker() {
+			
+			@Override
+			protected void doStuff() throws Exception {
+				
+				Entity topLevelFolder = null;
+				for(Entity entity : ModelMgr.getModelMgr().getEntitiesByName(TOP_LEVEL_FOLDER_NAME)) {
+					if (ModelMgrUtils.isOwner(entity)) {
+						topLevelFolder = entity;
+					}
+				}
+				
+				if (topLevelFolder==null) {
+					throw new IllegalStateException("Cannot find top-level folder named "+TOP_LEVEL_FOLDER_NAME);
+				}
+				
+				ModelMgrUtils.loadLazyEntity(topLevelFolder, false);
+				for(Entity child : topLevelFolder.getOrderedChildren()) {
+					compEntityMap.put(child.getName(), child);
+				}
+			}
+			
+			@Override
+			protected void hadSuccess() {
+			}
+			
+			@Override
+			protected void hadError(Throwable error) {
+				SessionMgr.getSessionMgr().handleException(error);
+			}
+		};
+		
+		worker.execute();
 	}
 	
 	public boolean isAccessible() {
@@ -132,6 +163,16 @@ public class ScreenEvaluationDialog extends ModalDialog {
 	
 	private void addListeners() {
 	
+		// TODO: move to after init
+//		KeyStroke accel = KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, SystemInfo.isMac?java.awt.Event.META_MASK:java.awt.Event.CTRL_MASK);
+//    	SessionMgr.getBrowser().getRootPane().getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(accel,"moveAction");
+//    	SessionMgr.getBrowser().getRootPane().getActionMap().put("moveAction",new AbstractAction("moveAction") {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				organizeEntitiesInCurrentFolder();
+//			}
+//		});
+		
 		ModelMgr.getModelMgr().addModelMgrObserver(new ModelMgrAdapter() {
 			@Override
 			public void annotationsChanged(final long entityId) {
@@ -168,11 +209,43 @@ public class ScreenEvaluationDialog extends ModalDialog {
 	
 	private void organizeEntitiesInCurrentFolder() {
 		
+		final List<Long> entityIds = new ArrayList<Long>();
+		for(RootedEntity rootedEntity : SessionMgr.getBrowser().getActiveViewer().getRootedEntities()) {
+			entityIds.add(rootedEntity.getEntityId());
+		}
+		
+		SimpleWorker worker = new SimpleWorker() {
+			
+			@Override
+			protected void doStuff() throws Exception {
+				int total = entityIds.size()+1;
+				int curr = 1;
+				setProgress(curr, total);
+				for(Long entityId : entityIds) {
+					organizeEntity(entityId);
+					setProgress(++curr, total);
+				}
+			}
+			
+			@Override
+			protected void hadSuccess() {
+			}
+			
+			@Override
+			protected void hadError(Throwable error) {
+				SessionMgr.getSessionMgr().handleException(error);
+			}
+		};
+
+		worker.setProgressMonitor(new ProgressMonitor(SessionMgr.getBrowser(), "Organizing...", "", 0, 100));
+		worker.execute();
 	}
 	
+	
 	private void organizeEntity(final Long entityId) throws Exception {
-
-		final EntityOutline entityOutline = SessionMgr.getBrowser().getEntityOutline();
+		
+		System.out.println("ScreenEvaluationDialog.organizeEntity: "+entityId);
+		
 		// TODO: should have a centralized way to do this
 		Annotations annotations = new Annotations();
 		annotations.reload(entityId);
@@ -195,113 +268,98 @@ public class ScreenEvaluationDialog extends ModalDialog {
 		}
 		
 		if (intensity!=null && distribution!=null) {
-			
-			// The current evaluation
+
+			// The latest evaluation
 			int i = getValueFromAnnotation(intensity);
 			int d = getValueFromAnnotation(distribution);
 			
-			// Current folders
-			DefaultMutableTreeNode distNode = null;
-			DefaultMutableTreeNode intNode = null;
-			DefaultMutableTreeNode compartmentNode = null;
+			// The entity 
+			Entity entity = ModelMgr.getModelMgr().getEntityById(entityId+"");
+			String compartment = entity.getName();
 			
-			Set<DefaultMutableTreeNode> nodes = entityOutline.getNodesByEntityId(entityId);
-			if (nodes==null) {
-				System.out.println("Entity not found in tree: "+entityId);
-				return;
-			}
+			List<EntityData> parentEds = ModelMgr.getModelMgr().getParentEntityDatas(entityId);
+			Map<Long,String> distCache = getDistCache(compartment);
 			
-			for(DefaultMutableTreeNode node : nodes) {
-				DefaultMutableTreeNode parent = (DefaultMutableTreeNode)node.getParent();
-				Entity parentEntity = entityOutline.getEntity(parent);
-				if (parentEntity!=null && parentEntity.getName().startsWith("Distribution ")) {
-					DefaultMutableTreeNode grandparent = (DefaultMutableTreeNode)parent.getParent();	
-					Entity grandparentEntity = entityOutline.getEntity(grandparent);
-					if (grandparentEntity!=null && grandparentEntity.getName().startsWith("Intensity ")) {
-						distNode = parent;
-						intNode = grandparent;
-						compartmentNode = (DefaultMutableTreeNode)grandparent.getParent();
-						break;
-					}
+			Integer ci = null;
+			Integer cd = null;
+			List<EntityData> currEds = new ArrayList<EntityData>();
+			for(EntityData parentEd : parentEds) {
+				Long parentId = parentEd.getParentEntity().getId();
+				String key = distCache.get(parentId);
+				if (key==null) {
+					continue;
+				}
+				currEds.add(parentEd);
+				if (ci==null || ci!=i) {
+					ci = getIntensityValueFromKey(key);
+				}
+				if (cd==null || cd!=d) {
+					cd = getDistributionValueFromKey(key);
 				}
 			}
 			
-			if (distNode==null || intNode==null || compartmentNode==null) {
-				throw new IllegalStateException("Sample not found in evaulation folder hierarchy");
-			}
+			if (ci==null || cd==null) return;
+			
+			String oldKey = getKey(compartment, ci, cd);
+			String newKey = getKey(compartment, i, d);
+			System.out.println("  Old: "+oldKey);
+			System.out.println("  New: "+newKey);
 
-			Entity distEntity = entityOutline.getEntity(distNode);
-			Entity intEntity = entityOutline.getEntity(intNode);
-			Entity compartmentEntity = entityOutline.getEntity(compartmentNode);
-			int ci = getValueFromFolderName(intEntity);
-			int cd = getValueFromFolderName(distEntity);
-			
-			if (DEBUG) System.out.println("current compartment="+compartmentEntity.getName());
-			if (DEBUG) System.out.println("current int="+intEntity.getName() +" ("+ci+" should be "+i+")");
-			if (DEBUG) System.out.println("current dist="+distEntity.getName() +" ("+cd+" should be "+d+")");
-			
-			if (ci==i) {
-				if (cd==d) {
-					if (DEBUG) System.out.println("Correct folder placement");		
-				}
-				else {
-					if (DEBUG) System.out.println("Distribution folder needs updating");
-					Entity targetFolder = null;
-					for(Entity distChild : intEntity.getChildren()) {
-						if (distChild.getName().endsWith(""+d)) {
-							targetFolder = distChild;
-						}
-					}
-					if (targetFolder==null) {
-						throw new IllegalStateException("Cannot find appropriate distribution folder");
-					}
-					moveToFolder(entityId,distEntity,targetFolder);
-				}
-			}
-			else {
-				if (DEBUG) System.out.println("Both folders need updating");
-				if (!EntityUtils.areLoaded(compartmentEntity.getEntityData())) {
-					ModelMgrUtils.loadLazyEntity(compartmentEntity, false);
-				}
-				Entity targetFolder = null;
-				for(Entity intChild : compartmentEntity.getChildren()) {
-					if (intChild.getName().endsWith(""+i)) {
-						if (!EntityUtils.areLoaded(intChild.getEntityData())) {
-							ModelMgrUtils.loadLazyEntity(intChild, false);
-						}
-						for(Entity distChild : intChild.getChildren()) {
-							if (distChild.getName().endsWith(""+d)) {
-								targetFolder = distChild;
-							}
-						}
-					}
-				}
-				if (targetFolder==null) {
-					throw new IllegalStateException("Cannot find appropriate intensity and distribution folders");
-				}
-				moveToFolder(entityId,distEntity,targetFolder);
+			if (!oldKey.equals(newKey) || currEds.size()>1) {
+				Entity distNew = folderCache.get(newKey);
+				moveToFolder(entityId,currEds,distNew);
 			}
 		}
 	}
-	
-	private void moveToFolder(long entityId, Entity currFolder, Entity targetFolder) throws Exception {
-		synchronized(currFolder) {
-			ModelMgr modelMgr = ModelMgr.getModelMgr();
-			// Add to new folder
-			List<Long> childrenIds = new ArrayList<Long>();
-			childrenIds.add(entityId);
-			modelMgr.addChildren(targetFolder.getId(), childrenIds, EntityConstants.ATTRIBUTE_ENTITY);
-			if (DEBUG) System.out.println("moved to folder "+targetFolder.getName()+" id="+targetFolder.getId());
-			// Remove from old folder
-			EntityData toDelete = null;
-			for(EntityData ed : new HashSet<EntityData>(currFolder.getEntityData())) {
-				if (ed.getChildEntity()!=null && ed.getChildEntity().getId()==entityId) {
-					toDelete = ed;
+
+	private Map<Long,String> getDistCache(String compartment) throws Exception {
+		
+		Map<Long,String> distCache = reverseFolderCache.get(compartment);
+		
+		if (distCache==null) {
+			System.out.println("  Caching folders for "+compartment);
+			distCache = new HashMap<Long,String>();
+
+			Entity compartmentEntity = compEntityMap.get(compartment);
+			
+			if (!EntityUtils.areLoaded(compartmentEntity.getEntityData())) {
+				ModelMgrUtils.loadLazyEntity(compartmentEntity, false);	
+			}
+			
+			for(Entity intChild : compartmentEntity.getChildren()) {
+				int i = getValueFromFolderName(intChild);
+				
+				if (!EntityUtils.areLoaded(intChild.getEntityData())) {
+					ModelMgrUtils.loadLazyEntity(intChild, false);
+				}
+				
+				for(Entity distChild : intChild.getChildren()) {
+					int d = getValueFromFolderName(distChild);
+					String key = getKey(compartment, i, d);
+					distCache.put(distChild.getId(), key);
+					System.out.println("    "+key+" = "+distChild.getId());
+					folderCache.put(key, distChild);
 				}
 			}
-			if (DEBUG) System.out.println("deleted from folder "+currFolder.getName()+" ed.id="+toDelete.getId());
-			if (toDelete!=null) {
-				modelMgr.removeEntityData(toDelete);
+			
+			reverseFolderCache.put(compartment, distCache);
+		}
+		
+		return distCache;
+	}
+	
+	private void moveToFolder(long entityId, List<EntityData> currEds, Entity targetFolder) throws Exception {
+		ModelMgr modelMgr = ModelMgr.getModelMgr();
+		// Add to new folder
+		List<Long> childrenIds = new ArrayList<Long>();
+		childrenIds.add(entityId);
+		modelMgr.addChildren(targetFolder.getId(), childrenIds, EntityConstants.ATTRIBUTE_ENTITY);
+		System.out.println("added to folder "+targetFolder.getName()+" id="+targetFolder.getId());
+		// Remove from old folders
+		if (currEds!=null) {
+			for(EntityData currEd : currEds) {
+			System.out.println("deleted from folder "+currEd.getParentEntity().getName()+", ed.id="+currEd.getId());
+			modelMgr.removeEntityData(currEd);
 			}
 		}
 	}
@@ -326,6 +384,27 @@ public class ScreenEvaluationDialog extends ModalDialog {
 	
 	private String getKey(String compartment, int i, int d) {
 		return compartment+"/"+i+"/"+d;
+	}
+
+
+	private Integer getIntensityValueFromKey(String key) {
+		try {
+			String[] parts = key.split("/");
+			return Integer.parseInt(parts[1]);	
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	private Integer getDistributionValueFromKey(String key) {
+		try {
+			String[] parts = key.split("/");
+			return Integer.parseInt(parts[2]);	
+		}
+		catch (Exception e) {
+			return null;
+		}
 	}
 	
     public void showDialog() {
