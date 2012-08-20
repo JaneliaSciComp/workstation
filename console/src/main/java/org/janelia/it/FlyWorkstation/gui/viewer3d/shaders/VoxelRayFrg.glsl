0ca0c9e5774bcc3abb3e672903313db74cf9dafb
Christopher Bruns <brunsc@janelia.hhmi.org>
2012-08-20 16:49:25 +0000

    VoxelRay shader is much closer to working.  But there remains popping at the voxel boundaries.
    Former-commit-id: 715d8a5f179db7615280ea7ec5c8001446a685e5

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/shaders/VoxelRayFrg.glsl
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/shaders/VoxelRayFrg.glsl
@@ -15,9 +15,69 @@ uniform vec3 voxelMicrometers;
 //   units are micrometers
 varying vec3 cameraDirectionInVolume;
 
+varying vec3 principalViewAxis; // 
+
 void main()
 {
-	// pass through color of central voxel
+    // Direction of the eye ray is required to ray trace the voxel
+    vec3 eyeVec = normalize(cameraDirectionInVolume); // points TOWARD eye from this fragment
+    // Ray parameter "t" is proportional to ray length, and thus to voxel thickness.
+    float t = -0.5; // view ray parameter, zero at default midplane fragment
+    // We want to compute anything we need from t
+    vec3 dPosDt = eyeVec;
+    vec3 dTexDPos = vec3(1.0,1.0,1.0) / textureVoxels / voxelMicrometers;
+    vec3 dTexDt = dPosDt * dTexDPos;
+
+    // float tMin = -0.5 * dot(principalViewAxis, (vec3(1.0, 1.0, 1.0)/eyeVec)) + 0.05;
+    float tMin = 0.49 * dot(principalViewAxis, (vec3(1.0, 1.0, 1.0)/eyeVec));
+    float tMax = -tMin;
+    // Stupid fixed step ray trace
+    vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
+    const int maxSegments = 5; // Pass through up to 4 voxels
+    // float dT = 0.99 * (tMax - tMin) / float(maxSegments);
+    t = tMin;
+    // Which three of the six voxel bounding planes might a view ray cross?
+    vec3 planes = ceil(eyeVec * 0.99); // each component of "planes" is now either 0.0 or 1.0
+    planes = planes * voxelMicrometers; // scale the 1.0 to actual voxel dimensions
+    for(int i = 0; i < maxSegments; ++i) {
+        if (t >= tMax)
+            break;
+        
+        // Trace the view ray forward, toward the eye, until
+        // we hit a voxel boundary
+        vec3 textureCoordinate = gl_TexCoord[0].xyz + dTexDt * t;
+        vec3 p = fract(textureCoordinate * textureVoxels) * voxelMicrometers;
+        // Here it is! Solve three ray-tracing equations at once:
+        vec3 distances = (planes - p) / eyeVec; // distance to each of three voxel boundaries
+        float thickness = min(distances.x, min(distances.y, distances.z)); // This is how much voxel meat we are peering through.
+        // TODO - use this thickness.
+        float dT = thickness;
+
+        // actually use a texture coordinate halfway through the computed thickness        
+        vec4 tc = texture3D(volumeTexture, textureCoordinate + 0.5 * dT * dTexDt);
+        float a0 = tc.a;
+        float ta0 = dT * a0;
+        float alpha = ta0 / (1.0 - a0 + ta0); // 4 flops total.  No pow() required.
+        alpha = clamp(alpha, 0.0, 1.0);
+        if (alpha > 0.0) {
+            color = alpha * tc + (1.0 - alpha) * color;
+            color.a = alpha + (1.0 - alpha) * a0;
+        }
+        t += dT + 0.01;
+    }
+
+    gl_FragColor = color;
+    
+    // Debugging colorizations below
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);
+    // gl_FragColor = vec4(0.5 * (principalViewAxis + vec3(1.0,1.0,1.0)), 0.5);
+    // float c = tMin + 0.5;
+    // gl_FragColor = vec4(c,c,c,0.5);
+}
+
+void main0() // phase 1, central voxel only on plane, maybe works.
+{
+    // pass through color of central voxel
     gl_FragColor = texture3D(volumeTexture, gl_TexCoord[0].xyz);
     
     // Phase I - only compute the contribution of this central voxel
@@ -30,11 +90,12 @@ void main()
     // Compute view vector and fragment position in voxel
     vec3 eyeVec = normalize(cameraDirectionInVolume); // points TOWARD eye from this fragment
     // Transform point into unit voxel coordinates
+    // (gl_TexCoord[0] contains the unit volume frame coordinates in range 0-1)
     vec3 uvc = fract(gl_TexCoord[0].xyz * textureVoxels);
     // Scale by voxel size to get units in micrometers
     vec3 p = uvc * voxelMicrometers;
 
-	// RAY TRACING    
+    // RAY TRACING    
     // Three axis-aligned plane equations define the voxel surface
     // in the general direction of the viewer.  Our ray stops
     // when it hits the CLOSEST of these three planes.  The
