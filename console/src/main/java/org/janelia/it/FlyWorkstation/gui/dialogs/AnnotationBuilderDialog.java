68f6a909aafbe865c8e52486a0fdc5350707ae25
Todd Safford <saffordt@janelia.hhmi.org>
2012-11-14 15:27:12 +0000

    Finer-grained deletion of annotations; by term or by term-value Explicit text in the removal buttons for clarity
    Former-commit-id: 59fa5872dcaf2b56d1d05b0c5128e5d429d0ee83

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/AnnotationBuilderDialog.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/AnnotationBuilderDialog.java
@@ -78,7 +78,6 @@ public class AnnotationBuilderDialog extends JDialog{
 
             @Override
             public void keyReleased(KeyEvent e) {
-                //To change body of implemented methods use File | Settings | File Templates.
             }
         });
 
