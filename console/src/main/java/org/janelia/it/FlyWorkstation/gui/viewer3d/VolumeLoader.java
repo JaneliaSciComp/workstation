8822cbebc7da4e543d70cc53aa59ee4798cfcf66
Les Foster <fosterl@janelia.hhmi.org>
2013-02-06 19:23:15 +0000

    Method-level refactoring in anticipation of splitting off classes.
    Former-commit-id: 2686031754da90db3d8df9c3c77847ae30e19a58

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/VolumeLoader.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/VolumeLoader.java
@@ -23,7 +23,6 @@ import org.janelia.it.FlyWorkstation.gui.viewer3d.texture.TextureDataBean;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.texture.TextureDataI;
 
 import java.nio.ByteOrder;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.zip.DataFormatException;
@@ -35,6 +34,10 @@ import java.io.InputStream;
 
 public class VolumeLoader 
 {
+    public enum FileType{
+        TIF, LSM, V3DMASK, V3DSIGNAL, MP4, UNKNOWN
+    };
+
     private static final String CONSOLIDATED_SIGNAL_FILE = "ConsolidatedSignal2";
     private static final String REFERENCE_FILE = "Reference2";
     private static final String TIF_EXT = "TIF";
@@ -59,7 +62,199 @@ public class VolumeLoader
         this.resolver = resolver;
     }
 
-	public boolean loadLociReader(BufferedImageReader in)
+    public boolean loadVolume(String unCachedFileName)
+    {
+        try {
+            this.unCachedFileName = unCachedFileName;
+            String localFileName = resolver.getResolvedFilename( unCachedFileName );
+
+            String extension = FilenameUtils.getExtension(localFileName).toUpperCase();
+            System.out.println("FILENAME: " + localFileName);
+            String baseName = FilenameUtils.getBaseName(localFileName);
+
+            resolveColorSpace(baseName, extension);
+            switch ( getFileType( localFileName, baseName, extension ) ) {
+                case TIF:
+                    loadLociReader(localFileName, new TiffReader());
+                    break;
+                case LSM:
+                    loadLociReader(localFileName, new ZeissLSMReader());
+                    break;
+                case V3DSIGNAL:
+                    loadV3dRaw(new BufferedInputStream(
+                            new FileInputStream(localFileName))
+                    );
+                    break;
+                case V3DMASK:
+                    loadV3dMask(new BufferedInputStream(
+                            new FileInputStream(localFileName))
+                    );
+                    break;
+                case MP4:
+                    loadMpegVideo(localFileName);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown filename/extension combination " + baseName + "/" + extension);
+            }
+
+
+//            if (extension.startsWith(TIF_EXT)) {
+//                loadLociReader(localFileName, new TiffReader());
+//            } else if (extension.startsWith(LSM_EXT)) {
+//                loadLociReader(localFileName, new ZeissLSMReader());
+//            } else if (extension.startsWith(V3D_EXT)) {
+//                if ( baseName.startsWith("ConsolidatedLabel") ) {
+//                    loadV3dMask(new BufferedInputStream(
+//                            new FileInputStream(localFileName))
+//                    );
+//                }
+//                else {
+//                    loadV3dRaw(new BufferedInputStream(
+//                            new FileInputStream(localFileName))
+//                    );
+//                }
+//            } else if (extension.startsWith(MP4_EXT)) {
+//                loadMpegVideo(localFileName);
+//            } else {
+//                throw new RuntimeException("Extension type " + extension + " not yet implemented.");
+//            }
+
+//            IFormatReader reader = null;
+//            if (extension.startsWith(TIF_EXT)) {
+//                reader = new TiffReader();
+//            } else if (extension.startsWith(LSM_EXT)) {
+//                reader = new ZeissLSMReader();
+//            }
+//            if (reader != null) {
+//                loadLociReader(localFileName, reader);
+//            } else if (extension.startsWith(V3D_EXT)) {
+//                InputStream v3dRawStream = new BufferedInputStream(
+//                        new FileInputStream(localFileName));
+//                // todo find the right way to switch this on.
+//                if ( baseName.startsWith("ConsolidatedLabel") ) {
+//                    loadV3dMask(v3dRawStream);
+//                }
+//                else {
+//                    loadV3dRaw(v3dRawStream);
+//                }
+//            } else if (extension.startsWith(MP4_EXT)) {
+//                loadMpegVideo(localFileName);
+//            } else {
+//                throw new RuntimeException("Extension type " + extension + " not yet implemented.");
+//            }
+
+            // Because we use premultiplied transparency...
+            if ( ! isMask )
+                setAlphaToSaturateColors(colorSpace);
+
+            return true;
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        return false;
+    }
+
+    private FileType getFileType( String filename, String baseName, String extension ) {
+        System.out.println("FILENAME: " + filename);
+
+        if (extension.startsWith(TIF_EXT)) {
+            return FileType.TIF;
+        }
+        else if (extension.startsWith(LSM_EXT)) {
+            return FileType.LSM;
+        }
+        else if (extension.startsWith(MP4_EXT)) {
+            return FileType.MP4;
+        }
+        else if (extension.startsWith(V3D_EXT) && baseName.startsWith("ConsolidatedLabel")) {
+            return FileType.V3DMASK;
+        }
+        else if (extension.startsWith(V3D_EXT)) {
+            return FileType.V3DSIGNAL;
+        }
+        else {
+            return FileType.UNKNOWN;
+        }
+    }
+
+    private void loadLociReader(String localFileName, IFormatReader reader) throws FormatException, IOException {
+        BufferedImageReader in = new BufferedImageReader(reader);
+        in.setId(localFileName);
+        loadLociReader(in);
+    }
+
+    private void resolveColorSpace(String baseName, String extension) {
+        // Default to linear color space
+        // But look for some exceptions we know about
+        colorSpace = TextureColorSpace.COLOR_SPACE_LINEAR;
+        if (baseName.startsWith(CONSOLIDATED_SIGNAL_FILE))
+            colorSpace = TextureColorSpace.COLOR_SPACE_SRGB;
+        if (baseName.startsWith(REFERENCE_FILE))
+            colorSpace = TextureColorSpace.COLOR_SPACE_SRGB;
+        // assume all mpegs are in sRGB color space
+        if (extension.startsWith(MP4_EXT))
+            colorSpace = TextureColorSpace.COLOR_SPACE_SRGB;
+
+    }
+
+    public void populateVolumeAcceptor(VolumeDataAcceptor dataAcceptor) {
+
+        TextureDataI textureData = null;
+
+        if ( isMask ) {
+            textureData = new MaskTextureDataBean( maskByteArray, sx, sy, sz );
+        }
+        else {
+            textureData = new TextureDataBean( argbIntArray, sx, sy, sz );
+        }
+
+        textureData.setColorSpace(colorSpace);
+        textureData.setVolumeMicrometers(new Double[]{(double) sx, (double) sy, (double) sz});
+        textureData.setVoxelMicrometers(new Double[]{1.0, 1.0, 1.0});
+        if ( header != null ) {
+            textureData.setHeader(header);
+        }
+        textureData.setByteOrder(pixelByteOrder);
+        textureData.setPixelByteCount(pixelBytes);
+        textureData.setFilename( unCachedFileName );
+        textureData.setChannelCount(channelCount);
+        dataAcceptor.setTextureData( textureData );
+    }
+
+    /**
+     * Set alpha component of each voxel assuming that R,G,B
+     * values represent a saturated color with premultiplied alpha.
+     * Similar to Vaa3D.  In other words, alpha = max(R,G,B)
+     */
+    private void setAlphaToSaturateColors(TextureColorSpace space) {
+        if ( space == null )
+            return;
+
+        // Use modified alpha value for sRGB textures
+        int[] alphaMap = new int[256];
+        double exponent = 1.0;
+        if (space == TextureColorSpace.COLOR_SPACE_SRGB)
+            exponent  = 2.2;
+        for (int i = 0; i < 256; ++i) {
+            double i0 = i / 255.0;
+            double i1 = Math.pow(i0, exponent);
+            alphaMap[i] = (int)(i1 * 255.0 + 0.5);
+        }
+        int numVoxels = argbIntArray.length;
+        for (int v = 0; v < numVoxels; ++v) {
+            int argb = argbIntArray[v];
+            int red   = (argb & 0x00ff0000) >>> 16;
+            int green = (argb & 0x0000ff00) >>> 8;
+            int blue  = (argb & 0x000000ff);
+            int alpha = Math.max(red, Math.max(green, blue));
+            alpha = alphaMap[alpha];
+            argb = (argb & 0x00ffffff) | (alpha << 24);
+            argbIntArray[v] = argb;
+        }
+    }
+
+    private boolean loadLociReader(BufferedImageReader in)
 	throws IOException, FormatException
 	{
 		sx = in.getSizeX();
@@ -82,7 +277,7 @@ public class VolumeLoader
 		return true;
 	}
 	
-	public boolean loadMpegVideo(String fileName) 
+	private boolean loadMpegVideo(String fileName)
 	{
 		IMediaReader mediaReader = ToolFactory.makeReader(fileName);
 		// use premultiplied alpha for this opengl mip technique
@@ -92,7 +287,7 @@ public class VolumeLoader
 		return true;
 	}
 	
-	public void loadV3dRaw(InputStream inputStream) 
+	private void loadV3dRaw(InputStream inputStream)
 	throws IOException, DataFormatException
 	{
 		V3dRawImageStream sliceStream = new V3dRawImageStream(inputStream);
@@ -140,7 +335,7 @@ public class VolumeLoader
         header = sliceStream.getHeaderKey();
 	}
 
-    public void loadV3dMask(InputStream inputStream)
+    private void loadV3dMask(InputStream inputStream)
             throws IOException, DataFormatException
     {
         isMask = true;
@@ -193,88 +388,7 @@ public class VolumeLoader
         header = sliceStream.getHeaderKey();
     }
 
-    public boolean loadVolume(String unCachedFileName)
-	{
-		try {
-            this.unCachedFileName = unCachedFileName;
-            String localFileName = resolver.getResolvedFilename( unCachedFileName );
-
-            String extension = FilenameUtils.getExtension(localFileName).toUpperCase();
-            System.out.println("FILENAME: " + localFileName);
-            // Default to linear color space
-            colorSpace = TextureColorSpace.COLOR_SPACE_LINEAR;
-            // But look for some exceptions we know about
-            String baseName = FilenameUtils.getBaseName(localFileName);
-            if (baseName.startsWith(CONSOLIDATED_SIGNAL_FILE))
-                colorSpace = TextureColorSpace.COLOR_SPACE_SRGB;
-            if (baseName.startsWith(REFERENCE_FILE))
-                colorSpace = TextureColorSpace.COLOR_SPACE_SRGB;
-
-            IFormatReader reader = null;
-            if (extension.startsWith(TIF_EXT)) {
-                reader = new TiffReader();
-            } else if (extension.startsWith(LSM_EXT)) {
-                reader = new ZeissLSMReader();
-            }
-            if (reader != null) {
-                BufferedImageReader in = new BufferedImageReader(reader);
-                in.setId(localFileName);
-                loadLociReader(in);
-            } else if (extension.startsWith(V3D_EXT)) {
-                InputStream v3dRawStream = new BufferedInputStream(
-                        new FileInputStream(localFileName));
-                // todo find the right way to switch this on.
-                if ( baseName.startsWith("ConsolidatedLabel") ) {
-                    loadV3dMask(v3dRawStream);
-                }
-                else {
-                    loadV3dRaw(v3dRawStream);
-                }
-            } else if (extension.startsWith(MP4_EXT)) {
-                loadMpegVideo(localFileName);
-                // assume all mpegs are in sRGB color space
-                colorSpace = TextureColorSpace.COLOR_SPACE_SRGB;
-            } else {
-                throw new RuntimeException("Extension type " + extension + " not yet implemented.");
-            }
-
-            // Because we use premultiplied transparency...
-            if ( ! isMask )
-                setAlphaToSaturateColors(colorSpace);
-
-            return true;
-        }
-        catch (Exception exc) {
-			exc.printStackTrace();
-		}
-		return false;
-	}
-
-	public void populateVolumeAcceptor(VolumeDataAcceptor dataAcceptor) {
-
-        TextureDataI textureData = null;
-
-        if ( isMask ) {
-            textureData = new MaskTextureDataBean( maskByteArray, sx, sy, sz );
-        }
-        else {
-            textureData = new TextureDataBean( argbIntArray, sx, sy, sz );
-        }
-
-        textureData.setColorSpace(colorSpace);
-        textureData.setVolumeMicrometers(new Double[]{(double) sx, (double) sy, (double) sz});
-        textureData.setVoxelMicrometers(new Double[]{1.0, 1.0, 1.0});
-        if ( header != null ) {
-            textureData.setHeader(header);
-        }
-        textureData.setByteOrder(pixelByteOrder);
-        textureData.setPixelByteCount(pixelBytes);
-        textureData.setFilename( unCachedFileName );
-        textureData.setChannelCount(channelCount);
-        dataAcceptor.setTextureData( textureData );
-	}
-	
-	private class VolumeFrameListener 
+	private class VolumeFrameListener
 	extends MediaListenerAdapter
 	{
 		// mpeg loading state variables
@@ -322,39 +436,6 @@ public class VolumeLoader
 			++frameIndex;
 		}
 	}
-	
-	/**
-	 * Set alpha component of each voxel assuming that R,G,B
-	 * values represent a saturated color with premultiplied alpha.
-	 * Similar to Vaa3D.  In other words, alpha = max(R,G,B)
-	 */
-	public void setAlphaToSaturateColors(TextureColorSpace space) {
-        if ( space == null )
-            return;
-
-		// Use modified alpha value for sRGB textures
-		int[] alphaMap = new int[256];
-		double exponent = 1.0;
-		if (space == TextureColorSpace.COLOR_SPACE_SRGB)
-			exponent  = 2.2;
-		for (int i = 0; i < 256; ++i) {
-			double i0 = i / 255.0;
-			double i1 = Math.pow(i0, exponent);
-			alphaMap[i] = (int)(i1 * 255.0 + 0.5);
-		}
-		int numVoxels = argbIntArray.length;
-		for (int v = 0; v < numVoxels; ++v) {
-			int argb = argbIntArray[v];
-			int red   = (argb & 0x00ff0000) >>> 16;
-			int green = (argb & 0x0000ff00) >>> 8;
-			int blue  = (argb & 0x000000ff);
-			int alpha = Math.max(red, Math.max(green, blue));
-			alpha = alphaMap[alpha];
-			argb = (argb & 0x00ffffff) | (alpha << 24);
-			argbIntArray[v] = argb;
-		}
-	}
-	
 
 	private void storeFramePixels(int frameIndex, BufferedImage image) 
 	{
