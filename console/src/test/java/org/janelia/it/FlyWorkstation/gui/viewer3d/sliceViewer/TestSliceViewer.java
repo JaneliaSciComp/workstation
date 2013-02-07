ca6f0e46c33d14871578194b7eb35c1ba273ab35
Christopher Bruns <brunsc@janelia.hhmi.org>
2013-02-07 15:48:58 +0000

    Create interactive test program for SliceViewer
    Former-commit-id: 69bcb255d5b3d7414741164ad41eb55de09af8cf

---
+++ console/src/test/java/org/janelia/it/FlyWorkstation/gui/viewer3d/sliceViewer/TestSliceViewer.java
@@ -0,0 +1,30 @@
+package org.janelia.it.FlyWorkstation.gui.viewer3d.sliceViewer;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.janelia.it.FlyWorkstation.gui.viewer3d.sliceViewer.SliceViewer;
+
+public class TestSliceViewer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Test SliceViewer");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                JLabel label = new JLabel("Test SliceViewer");
+                frame.getContentPane().add(label);
+                SliceViewer sliceViewer = new SliceViewer();
+                frame.getContentPane().add(sliceViewer);
+
+                //Display the window.
+                frame.pack();
+                frame.setSize( frame.getContentPane().getPreferredSize() );
+                frame.setVisible(true);
+            }
+        });
+	}
+}
