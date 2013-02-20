34cf6ea158bbdb05bfb879f3a26d93a4b1ee94b0
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-02-20 17:45:42 +0000

    added reset window
    Former-commit-id: d086d938bfa04c6d908737b3f54f46f1d971669b

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/console/ViewMenu.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/console/ViewMenu.java
@@ -1,6 +1,11 @@
 package org.janelia.it.FlyWorkstation.gui.framework.console;
 
 import javax.swing.*;
+
+import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr;
+import org.janelia.it.FlyWorkstation.gui.util.SystemInfo;
+
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -12,9 +17,11 @@ import java.awt.event.KeyEvent;
  * Time: 1:16 PM
  */
 public class ViewMenu extends JMenu {
+    private static String BROWSER_POSITION = "BROWSER_POSITION_ON_SCREEN";
     Browser console;
     JMenuItem ontologyMenuItem;
     JMenuItem dataMenuItem;
+    JMenuItem resetWindow;
     //JMenuItem alignBoardItem;
 
     public ViewMenu(Browser console) {
@@ -38,6 +45,25 @@ public class ViewMenu extends JMenu {
         });
         dataMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, java.awt.Event.META_MASK));
 
+        resetWindow = new JCheckBoxMenuItem("Reset Window", true);
+        resetWindow.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int offsetY = 0;
+                String lafName = (String) SessionMgr.getSessionMgr().getModelProperty(SessionMgr.DISPLAY_LOOK_AND_FEEL);
+                if (SystemInfo.isMac && lafName!=null && lafName.contains("synthetica")) {
+                    offsetY=20;
+                }
+                
+                BrowserPosition consolePosition = (BrowserPosition) SessionMgr.getSessionMgr().getModelProperty(BROWSER_POSITION);
+                consolePosition.setBrowserLocation(new Point(0, offsetY));
+                SessionMgr.getSessionMgr().setModelProperty(BROWSER_POSITION, consolePosition);        
+                setSize(consolePosition.getBrowserSize());
+                setLocation(consolePosition.getBrowserLocation());
+            }
+        });
+        resetWindow.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, java.awt.Event.META_MASK));
+
+        
         addMenuItems();
     }
 
@@ -45,7 +71,7 @@ public class ViewMenu extends JMenu {
         removeAll();
         add(dataMenuItem);
         add(ontologyMenuItem);
-
+        add(resetWindow);
     }
 
     private void viewActionPerformed(String viewComponent) {
