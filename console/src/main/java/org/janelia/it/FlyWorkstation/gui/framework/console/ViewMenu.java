8c35272f42fb3e652adf698f0443237b1880fa50
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-02-23 15:27:57 +0000

    added perspectives
    Former-commit-id: 08007a3a6237bad51d8e8f2b12851f80d1446a8c

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/console/ViewMenu.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/console/ViewMenu.java
@@ -1,12 +1,17 @@
 package org.janelia.it.FlyWorkstation.gui.framework.console;
 
-import javax.swing.*;
-
-import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr;
-
+import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr;
+import org.janelia.it.FlyWorkstation.gui.util.SystemInfo;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,54 +20,52 @@ import java.awt.event.KeyEvent;
  * Time: 1:16 PM
  */
 public class ViewMenu extends JMenu {
-    Browser console;
-    JMenuItem ontologyMenuItem;
-    JMenuItem dataMenuItem;
-    JMenuItem resetWindow;
-    //JMenuItem alignBoardItem;
 
-    public ViewMenu(Browser console) {
+    public ViewMenu(final Browser browser) {
         super("View");
         this.setMnemonic('V');
-        this.console = console;
-
-        ontologyMenuItem = new JCheckBoxMenuItem("Ontology Editor", true);
-        ontologyMenuItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                viewActionPerformed(Browser.VIEW_ONTOLOGY);
-            }
-        });
-        ontologyMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, java.awt.Event.META_MASK));
-
-        dataMenuItem = new JCheckBoxMenuItem("Data Panel", true);
+        
+        JMenuItem dataMenuItem = new JCheckBoxMenuItem("Data Panel", true);
         dataMenuItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                viewActionPerformed(Browser.VIEW_OUTLINES);
+                browser.toggleViewComponentState(Browser.VIEW_OUTLINES);
             }
         });
-        dataMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, java.awt.Event.META_MASK));
+        dataMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, SystemInfo.isMac?Event.META_MASK:Event.CTRL_MASK));
+        add(dataMenuItem);
 
-        resetWindow = new JCheckBoxMenuItem("Reset Window", true);
+        JMenuItem ontologyMenuItem = new JCheckBoxMenuItem("Ontology Editor", true);
+        ontologyMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                browser.toggleViewComponentState(Browser.VIEW_ONTOLOGY);
+            }
+        });
+        ontologyMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, SystemInfo.isMac?Event.META_MASK:Event.CTRL_MASK));
+        add(ontologyMenuItem);
+        
+
+        JMenuItem perspectivesMenu = new JMenu("Open Perspective");
+        
+        for(final Perspective perspective : Perspective.values()) {
+
+            JMenuItem perspectiveMenuItem = new JMenuItem(perspective.getName());
+            perspectiveMenuItem.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    browser.setPerspective(perspective);
+                }
+            });
+            perspectivesMenu.add(perspectiveMenuItem);
+        }
+        
+        add(perspectivesMenu);
+        
+        JMenuItem resetWindow = new JMenuItem("Reset Window");
         resetWindow.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 SessionMgr.getBrowser().resetWindow();
             }
         });
-        resetWindow.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, java.awt.Event.META_MASK));
-
-        
-        addMenuItems();
-    }
-
-    private void addMenuItems() {
-        removeAll();
-        add(dataMenuItem);
-        add(ontologyMenuItem);
+        resetWindow.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, SystemInfo.isMac?Event.META_MASK:Event.CTRL_MASK));
         add(resetWindow);
     }
-
-    private void viewActionPerformed(String viewComponent) {
-        console.toggleViewComponentState(viewComponent);
-    }
-
 }
