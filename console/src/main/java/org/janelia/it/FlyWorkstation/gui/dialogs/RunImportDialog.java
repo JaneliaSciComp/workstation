28742775cf97d0d9900ccf203aad4acdc79cd4a5
Todd Safford <saffordt@janelia.hhmi.org>
2013-03-08 16:03:23 +0000

    Changes to the Import mechanism to allow for a file chooser if the user has the mount.
    Former-commit-id: 8904d93d20c020cf038ea3ff09fc105dc9b4cc1e

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/RunImportDialog.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/RunImportDialog.java
@@ -1,10 +1,10 @@
 package org.janelia.it.FlyWorkstation.gui.dialogs;
 
-import loci.plugins.config.SpringUtilities;
 import org.janelia.it.FlyWorkstation.api.entity_model.management.ModelMgr;
 import org.janelia.it.FlyWorkstation.gui.framework.console.Browser;
 import org.janelia.it.FlyWorkstation.gui.framework.console.Perspective;
 import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr;
+import org.janelia.it.FlyWorkstation.shared.util.ConsoleProperties;
 import org.janelia.it.FlyWorkstation.shared.util.Utils;
 import org.janelia.it.FlyWorkstation.shared.workers.SimpleWorker;
 import org.janelia.it.jacs.model.tasks.Event;
@@ -17,6 +17,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -28,7 +30,6 @@ import java.util.HashSet;
  */
 public class RunImportDialog extends ModalDialog{
 
-    private static final String INPUT_DIR = "";
     private static final String TOP_LEVEL_FOLDER_NAME = "%USER%'s Imported Data";
 
     private static final String TOOLTIP_INPUT_DIR       = "Root directory of the tree that should be loaded into the database";
@@ -36,19 +37,19 @@ public class RunImportDialog extends ModalDialog{
 
     private static final int REFRESH_DELAY = 3000;
 
-    private final JTextField inputDirectoryField;
     private final JTextField topLevelFolderField;
+    private JTextField pathTextField;
+    private JFileChooser fileChooser;
 
     private long taskID;
 
     private Timer refreshTimer;
 
     public RunImportDialog() {
-        final JPanel attrPanel;
-
         setTitle("Import");
 
-        attrPanel = new JPanel(new SpringLayout());
+        JPanel attrPanel = new JPanel();
+        attrPanel.setLayout(new GridBagLayout());
         attrPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10),
                 BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Import Information")));
 
@@ -58,26 +59,63 @@ public class RunImportDialog extends ModalDialog{
         topLevelFolderField.setText(filter(TOP_LEVEL_FOLDER_NAME));
         topLevelFolderField.setToolTipText(TOOLTIP_TOP_LEVEL_FOLDER);
         topLevelFolderLabel.setLabelFor(topLevelFolderField);
-        attrPanel.add(topLevelFolderLabel);
-        attrPanel.add(topLevelFolderField);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0; c.gridy = 0;
+        attrPanel.add(topLevelFolderLabel, c);
+        c.gridx = 1;
+        attrPanel.add(topLevelFolderField, 1);
 
-        JLabel inputDirectoryLabel = new JLabel("Input Directory (Linux mounted)");
-        inputDirectoryLabel.setToolTipText(TOOLTIP_INPUT_DIR);
-        inputDirectoryField = new JTextField(40);
-        inputDirectoryField.setText(filter(INPUT_DIR));
-        inputDirectoryField.setToolTipText(TOOLTIP_INPUT_DIR);
-        inputDirectoryLabel.setLabelFor(inputDirectoryField);
-        attrPanel.add(inputDirectoryLabel);
-        attrPanel.add(inputDirectoryField);
+        String pathText="";
+        pathTextField = new JTextField(40);
+        pathTextField.setText(pathText);
+        setSize(400, 400);
+        File pathTest = new File(ConsoleProperties.getString("remote.defaultLinuxPath"));
+        c.gridx = 0; c.gridy = 1;
+        JLabel pathLabel = new JLabel("Path:");
+        pathLabel.setToolTipText(TOOLTIP_INPUT_DIR);
+        attrPanel.add(new JLabel("Path:"), c);
+        c.gridx = 1;
+        attrPanel.add(pathTextField, c);
+        if (pathTest.exists() && pathTest.canRead()) {
+            fileChooser  = new JFileChooser(pathTest);
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+            JButton _filePathButton = null;
+            try {
+                _filePathButton = new JButton(Utils.getClasspathImage("magnifier.png"));
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (_filePathButton != null) {
+                _filePathButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        int returnVal = fileChooser.showOpenDialog(RunImportDialog.this);
+                        if (returnVal == JFileChooser.APPROVE_OPTION) {
+                            pathTextField.setText(fileChooser.getSelectedFile().getAbsolutePath());
+                        }
+                    }
+                });
+            }
+            c.gridx = 2;
+            attrPanel.add(_filePathButton, c);
+        }
 
         add(attrPanel, BorderLayout.CENTER);
-        SpringUtilities.makeCompactGrid(attrPanel, attrPanel.getComponentCount()/2, 2, 6, 6, 6, 6);
+//        SpringUtilities.makeCompactGrid(attrPanel, attrPanel.getComponentCount()/3, 2, 6, 6, 6, 6);
 
         JButton okButton = new JButton("Import");
         okButton.setToolTipText("Import a directory or file");
         okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String path = pathTextField.getText();
+                if (null==path || "".equals(path) || !path.startsWith("/groups/")) {
+                    JOptionPane.showMessageDialog(RunImportDialog.this,
+                            "Please define a valid network path (/groups/...)", "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
                 runImport();
             }
         });
@@ -106,14 +144,11 @@ public class RunImportDialog extends ModalDialog{
 
         Utils.setWaitingCursor(this);
 
-        final String inputDirPath = inputDirectoryField.getText();
-        final String topLevelFolderName = topLevelFolderField.getText();
-
         SimpleWorker executeWorker = new SimpleWorker() {
 
             @Override
             protected void doStuff() throws Exception {
-                startImport(inputDirPath, topLevelFolderName);
+                startImport();
             }
 
             @Override
@@ -141,21 +176,22 @@ public class RunImportDialog extends ModalDialog{
         packAndShow();
     }
 
-    private void startImport(String path, String topLevelFolderName) {
-
+    private void startImport() {
         try {
             String process;
             Task task;
             String owner = SessionMgr.getSubjectKey();
-
             process = "FileTreeLoader";
+
+            String path = pathTextField.getText();
+            String topLevelFolderName = topLevelFolderField.getText().trim();
             task = new FileTreeLoaderPipelineTask(new HashSet<Node>(),
                     owner, new ArrayList<Event>(), new HashSet<TaskParameter>(), path, topLevelFolderName);
             task.setJobName("Import Files Task");
             task = ModelMgr.getModelMgr().saveOrUpdateTask(task);
             taskID = task.getObjectId();
-            ModelMgr.getModelMgr().submitJob(process, task.getObjectId());
 
+            ModelMgr.getModelMgr().submitJob(process, task.getObjectId());
             refreshTimer = new Timer(REFRESH_DELAY, new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
