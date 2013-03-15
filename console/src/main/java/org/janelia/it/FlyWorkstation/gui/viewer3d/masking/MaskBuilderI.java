d16ddb33918a28c34339b4ca847f7150209fec70
Les Foster <fosterl@janelia.hhmi.org>
2013-03-15 15:58:24 +0000

    Saving in case it breaks build.
    Former-commit-id: 22f9a65e6a0e57e4429c3dc8d4bd5f65daeba942

---
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/masking/MaskBuilderI.java
@@ -0,0 +1,29 @@
+package org.janelia.it.FlyWorkstation.gui.viewer3d.masking;
+
+import org.janelia.it.FlyWorkstation.gui.viewer3d.VolumeDataAcceptor;
+import org.janelia.it.FlyWorkstation.gui.viewer3d.renderable.RenderableBean;
+import org.janelia.it.FlyWorkstation.gui.viewer3d.texture.TextureDataI;
+
+import java.nio.ByteOrder;
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fosterl
+ * Date: 3/13/13
+ * Time: 4:10 PM
+ *
+ * Implement this to make something that can build a mask texture volume suitable for use in the alignment board viewer.
+ */
+public interface MaskBuilderI {
+    int GPU_MULTIBYTE_DIVISIBILITY_VALUE = 4;
+
+    byte[] getVolumeMask();
+    Integer[] getVolumeMaskVoxels();
+    ByteOrder getPixelByteOrder();
+    int getPixelByteCount();
+    TextureDataI getCombinedTextureData();
+    VolumeDataAcceptor.TextureColorSpace getTextureColorSpace();
+    void setRenderables(Collection<RenderableBean> renderables);
+
+}
