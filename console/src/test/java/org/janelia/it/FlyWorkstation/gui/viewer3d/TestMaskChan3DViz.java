460c101086b6289b7de1687aebffcb607e63f6c5
Les Foster <fosterl@janelia.hhmi.org>
2013-04-01 18:17:01 +0000

    Sanity-checking the channel meta data settings, as they come in;  only setting channel meta data once for each acceptor.
    Former-commit-id: da67b88249c2a75b9eea5771af562c0b79a39d9e

--- console/src/test/java/org/janelia/it/FlyWorkstation/gui/viewer3d/TestMaskChan3DViz.java
+++ console/src/test/java/org/janelia/it/FlyWorkstation/gui/viewer3d/TestMaskChan3DViz.java
@@ -1,17 +1,15 @@
 
 package org.janelia.it.FlyWorkstation.gui.viewer3d;
 
-import org.janelia.it.FlyWorkstation.gui.viewer3d.loader.MaskChanDataAcceptorI;
-import org.janelia.it.FlyWorkstation.gui.viewer3d.loader.MaskChanMultiFileLoader;
+import org.janelia.it.FlyWorkstation.gui.framework.viewer.alignment_board.RenderablesLoadWorker;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.masking.*;
+import org.janelia.it.FlyWorkstation.gui.viewer3d.renderable.MaskChanRenderableData;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.renderable.RenderableBean;
-import org.janelia.it.jacs.model.entity.Entity;
+import org.janelia.it.FlyWorkstation.gui.viewer3d.renderable.RenderableDataSourceI;
+import org.janelia.it.FlyWorkstation.gui.viewer3d.resolver.TrivialFileResolver;
 
 import javax.swing.*;
 import java.awt.Dimension;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -25,6 +23,7 @@ public class TestMaskChan3DViz {
 
     private static final long MOCK_UID = 777L;
     private static final String MASK_EXTENSION = ".mask";
+    private static final Dimension FRAME_SIZE = new Dimension(100, 10);
 
     /**
 	 * @param args filenames for all mask and all channel files, in that order, to be displayed.
@@ -44,179 +43,28 @@ public class TestMaskChan3DViz {
                 }
 
                 try {
-                    List<RenderableBean> renderableBeans = new ArrayList<RenderableBean>();
-
-                    /* Establish all renderables to be displayed. */
-
-                    // Establish the renderable for the "signal".
-                    int nextTranslatedNum = 0;
-                    RenderableBean signalBean = new RenderableBean();
-                    signalBean.setLabelFileNum(0);
-                    signalBean.setTranslatedNum(nextTranslatedNum ++);
-                    signalBean.setRgb(
-                            new byte[]{
-                                    (byte) 0f, (byte) 0f, (byte) 0f, RenderMappingI.NON_RENDERING
-                            }
-                    );
-                    renderableBeans.add( signalBean );
-
-                    // Must divie-up the inputs between compartments and Fragments.
-
-                    // Establish the compartment renderables.
-                    int nextArgument = 0;
-                    for ( int i = 0; i < args.length; i ++ ) {
-                        if ( ! args[ i ].toLowerCase().contains( "/compartment_" ) ) {
-                            break;
-                        }
-                        String fn = args[ i ];
-                        if ( fn.endsWith( MASK_EXTENSION ) ) {
-                            RenderableBean renderableBean = new RenderableBean();
-
-                            // File name should tell its own index number.
-                            int underPos = fn.lastIndexOf( '_' );
-                            String beanNumStr = fn.substring( underPos + 1, fn.indexOf( MASK_EXTENSION ) );
-                            int beanNum = Integer.parseInt( beanNumStr );
-                            renderableBean.setLabelFileNum( beanNum );
-                            renderableBean.setTranslatedNum( nextTranslatedNum );
-                            renderableBean.setRgb(
-                                    new byte[] { 0, 0, 0, RenderMappingI.COMPARTMENT_RENDERING }
-                            );
-                            renderableBeans.add( renderableBean );
-
-                            nextTranslatedNum ++;
-                        }
-                        nextArgument ++;
-                    }
-
-                    int lastNFArg = nextArgument;
-
-                    // Establish the fragment renderables.
-                    int colorDriver = 20;
-                    for ( int i = nextArgument; i < args.length; i += 2 ) {
-                        RenderableBean renderableBean = new RenderableBean();
-
-                        String fn = args[ i ];
-                        if ( fn.endsWith( MASK_EXTENSION ) ) {
-                            // File name should tell its own index number.
-                            int underPos = fn.lastIndexOf( '_' );
-                            String beanNumStr = fn.substring( underPos + 1, fn.indexOf( MASK_EXTENSION ) );
-                            int beanNum = Integer.parseInt( beanNumStr );
-
-                            renderableBean.setTranslatedNum( nextTranslatedNum );
-                            renderableBean.setLabelFileNum( beanNum );
-                            int rComp = colorDriver % 5;
-                            int gComp = colorDriver % 7;
-                            int bComp = colorDriver % 11;
-                            renderableBean.setRgb(
-                                    new byte[] {
-                                            (byte)((rComp * 31) % 255), (byte)((gComp * 23) % 255), (byte)((bComp * 19) % 255),
-                                            RenderMappingI.FRAGMENT_RENDERING
-                                    }
-                            );
-                            renderableBeans.add(renderableBean);
-
-                            nextTranslatedNum ++;
-                            colorDriver ++;
-                        }
-                        else {
-                            throw new IllegalArgumentException(
-                                  "Wrong file order.  Expecting .mask at position " + i + " in command-line arguments."
-                            );
-                        }
-                    }
-
-                    /* Establish all volume builders for this test. */
-                    ArrayList<MaskChanDataAcceptorI> acceptors = new ArrayList<MaskChanDataAcceptorI>();
-
-                    // Establish the means for extracting the volume mask.
-                    RenderablesMaskBuilder vmb = new RenderablesMaskBuilder();
-                    vmb.setRenderables( renderableBeans );
-
-                    // Establish the means for extracting the signal data.
-                    RenderablesChannelsBuilder vcb = new RenderablesChannelsBuilder();
-
-                    // Setup the loader to traverse all this data on demand.
-                    MaskChanMultiFileLoader neuronFragmentLoader = new MaskChanMultiFileLoader();
-                    acceptors.add( vmb );
-                    acceptors.add( vcb );
-
-                    neuronFragmentLoader.setAcceptors(acceptors);
-
-                    MaskChanMultiFileLoader compartmentLoader = new MaskChanMultiFileLoader();
-                    compartmentLoader.setAcceptors( Arrays.<MaskChanDataAcceptorI>asList( vmb ) );
-
-                    // Iterating through these files will cause all the relevant data to be loaded into
-                    // the acceptors, which here includes only the mask builder.
-                    for ( int i = 0; i < lastNFArg; i+= 2 ) {
-                        InputStream maskStream = new BufferedInputStream( new FileInputStream( args[ i ] ) );
-                        InputStream chanStream = new BufferedInputStream( new FileInputStream( args[ i + 1 ] ) );
-
-                        // Only the neuron fragment renderables are relevant to this load.  The signal renderable
-                        // is kept separate.  Hence skip the first (0th) renderable.
-                        System.out.println( "Reading " + args[ i ] );
-                        compartmentLoader.read(renderableBeans.get((i / 2) + 1), maskStream, chanStream);
-
-                        maskStream.close();
-                        chanStream.close();
-                    }
-
-                    compartmentLoader.close();
-
-                    // Iterating through these files will cause all the relevant data to be loaded into
-                    // the acceptors, which here are the mask builder and the channels builder.
-                    for ( int i = lastNFArg; i < renderableBeans.size(); i += 2 ) {
-                        //int fnPos = args[i].lastIndexOf('/');
-                        //String filename = args[i].substring( fnPos + 1 ).toLowerCase();
-
-                        InputStream maskStream = new BufferedInputStream( new FileInputStream( args[ i ] ) );
-                        InputStream chanStream = new BufferedInputStream( new FileInputStream( args[ i + 1 ] ) );
-
-                        // Only the neuron fragment renderables are relevant to this load.  The signal renderable
-                        // is kept separate.  Hence skip the first (0th) renderable.
-                        System.out.println( "Reading " + args[ i ] );
-                        neuronFragmentLoader.read(renderableBeans.get((i / 2) + 1), maskStream, chanStream);
-
-                        maskStream.close();
-                        chanStream.close();
-                    }
-
-                    neuronFragmentLoader.close();
-
-                    // For DEBUG
-                    //vcb.test();
 
                     // Setup a testing color-wheel mapping.
-                    ConfigurableColorMapping colorMapping = new ConfigurableColorMapping();
-                    colorMapping.setRenderables( renderableBeans );
-
                     JFrame frame = new JFrame("Test MipWidget for Masking");
                     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
                     JLabel label = new JLabel("Test MipWidget for Masking");
                     frame.getContentPane().add(label);
-                    frame.setSize( new Dimension( 600, 622 ) );
+                    frame.setSize( FRAME_SIZE );
                     Mip3d mipWidget = new Mip3d();
 
                     mipWidget.clear();
 
                     // Now, the vmb and the vcb are fully populated with all data. Can hand that into the
                     // mip3d.
-                    //    WORKS
-                    //String fn = "/Volumes/jacsData/filestore/system/Separation/294/370/1742138165818294370/separate/fastLoad/ConsolidatedSignal2_25.mp4";
-                    //VolumeLoader volumeLoader = new VolumeLoader(new TrivialFileResolver());
-                    //volumeLoader.loadVolume( fn );
-
-                    //mipWidget.loadVolume( fn, new TrivialFileResolver() );
-
-                    if ( ! mipWidget.setVolume( vcb, vmb, colorMapping, 1.0f ) ) {
-                        throw new RuntimeException( "Failed to load." );
-                    }
-
-                    //mipWidget.loadVolume( fn, vmb, new TrivialFileResolver(), colorMapping, 1.0f );
-                    frame.getContentPane().add( mipWidget );
+                    RenderablesLoadWorker loadWorker = new RenderablesLoadWorker(
+                            (JComponent)frame.getContentPane(), new Chan3DVizDataSource( args ), mipWidget
+                    );
+                    loadWorker.setResolver( new TrivialFileResolver() );
+                    loadWorker.execute();
 
                     //Display the window.
-                    frame.pack();
-                    frame.setSize( frame.getContentPane().getPreferredSize() );
+                    //args.frame.pack();
+                    //frame.setSize( frame.getContentPane().getPreferredSize() );
                     frame.setVisible(true);
 
                 }
@@ -227,4 +75,114 @@ public class TestMaskChan3DViz {
         });
 	}
 
+    /** Implements the data source, to avoid having to mock up an entire context just to test. */
+    public static class Chan3DVizDataSource implements RenderableDataSourceI {
+
+        private String[] args;
+        public Chan3DVizDataSource( String[] args ) {
+            this.args = args;
+        }
+
+        @Override
+        public String getName() {
+            return TestMaskChan3DViz.class.getSimpleName() + " Data Source";
+        }
+
+        @Override
+        public Collection<MaskChanRenderableData> getRenderableDatas() {
+
+            Collection<MaskChanRenderableData> rtnVal = new ArrayList<MaskChanRenderableData>();
+
+            // Establish the renderable for the "signal".
+            int nextTranslatedNum = 0;
+            RenderableBean signalBean = new RenderableBean();
+            signalBean.setLabelFileNum(0);
+            signalBean.setTranslatedNum(nextTranslatedNum ++);
+            signalBean.setRgb(
+                    new byte[]{
+                            (byte) 0f, (byte) 0f, (byte) 0f, RenderMappingI.NON_RENDERING
+                    }
+            );
+
+            MaskChanRenderableData signalData = new MaskChanRenderableData();
+            signalData.setBean(signalBean);
+            signalData.setCompartment( false );
+
+            // Must divie-up the inputs between compartments and Fragments.
+
+            // Establish the compartment renderables.
+            int nextArgument = 0;
+            for ( int i = 0; i < args.length; i ++ ) {
+                if ( ! args[ i ].toLowerCase().contains( "/compartment_" ) ) {
+                    break;
+                }
+                String fn = args[ i ];
+                if ( fn.endsWith( MASK_EXTENSION ) ) {
+                    RenderableBean renderableBean = new RenderableBean();
+
+                    // File name should tell its own index number.
+                    int underPos = fn.lastIndexOf( '_' );
+                    String beanNumStr = fn.substring( underPos + 1, fn.indexOf( MASK_EXTENSION ) );
+                    int beanNum = Integer.parseInt( beanNumStr );
+                    renderableBean.setLabelFileNum(beanNum);
+                    renderableBean.setTranslatedNum(nextTranslatedNum);
+                    renderableBean.setRgb(
+                            new byte[]{0, 0, 0, RenderMappingI.COMPARTMENT_RENDERING}
+                    );
+
+                    MaskChanRenderableData data = new MaskChanRenderableData();
+                    data.setBean( renderableBean );
+                    data.setCompartment( true );
+                    data.setMaskPath( fn );
+                    rtnVal.add(data);
+
+                    nextTranslatedNum ++;
+                }
+                nextArgument ++;
+            }
+
+            // Establish the fragment renderables.
+            int colorDriver = 20;
+            for ( int i = nextArgument; i < args.length; i += 2 ) {
+                RenderableBean renderableBean = new RenderableBean();
+
+                String fn = args[ i ];
+                if ( fn.endsWith( MASK_EXTENSION ) ) {
+                    // File name should tell its own index number.
+                    int underPos = fn.lastIndexOf( '_' );
+                    String beanNumStr = fn.substring( underPos + 1, fn.indexOf( MASK_EXTENSION ) );
+                    int beanNum = Integer.parseInt( beanNumStr );
+
+                    renderableBean.setTranslatedNum( nextTranslatedNum );
+                    renderableBean.setLabelFileNum( beanNum );
+                    int rComp = colorDriver % 5;
+                    int gComp = colorDriver % 7;
+                    int bComp = colorDriver % 11;
+                    renderableBean.setRgb(
+                            new byte[]{
+                                    (byte) ((rComp * 31) % 255), (byte) ((gComp * 23) % 255), (byte) ((bComp * 19) % 255),
+                                    RenderMappingI.FRAGMENT_RENDERING
+                            }
+                    );
+
+                    MaskChanRenderableData data = new MaskChanRenderableData();
+                    data.setBean( renderableBean );
+                    data.setCompartment( false );
+                    data.setMaskPath( args[ i ] );
+                    data.setChannelPath( args[ i + 1 ] );
+                    rtnVal.add(data);
+
+                    nextTranslatedNum ++;
+                    colorDriver ++;
+                }
+                else {
+                    throw new IllegalArgumentException(
+                            "Wrong file order.  Expecting .mask at position " + i + " in command-line arguments."
+                    );
+                }
+            }
+
+            return rtnVal;
+        }
+    }
 }
