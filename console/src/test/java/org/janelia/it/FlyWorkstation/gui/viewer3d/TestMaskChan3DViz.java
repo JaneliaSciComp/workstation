a3367ea6ef51303af6f6e4644f4cd7d2b0414b3d
Les Foster <fosterl@janelia.hhmi.org>
2013-04-13 19:31:50 +0000

    Removed direct calls to MIP3d from the RenderablesLoadWorker, and made a new interface for the general callbacks to control the AB Viewer.
    Former-commit-id: 373b95217cce659ff8332b1b15aea0b769a8d83b

--- console/src/test/java/org/janelia/it/FlyWorkstation/gui/viewer3d/TestMaskChan3DViz.java
+++ console/src/test/java/org/janelia/it/FlyWorkstation/gui/viewer3d/TestMaskChan3DViz.java
@@ -1,15 +1,20 @@
 
 package org.janelia.it.FlyWorkstation.gui.viewer3d;
 
+import org.janelia.it.FlyWorkstation.gui.framework.viewer.alignment_board.AlignmentBoardControllable;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.alignment_board.AlignmentBoardSettings;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.alignment_board.RenderablesLoadWorker;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.gui_elements.AlignmentBoardSettingsDialog;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.masking.ConfigurableColorMapping;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.masking.RenderMappingI;
 import org.janelia.it.FlyWorkstation.gui.viewer3d.resolver.TrivialFileResolver;
+import org.janelia.it.FlyWorkstation.gui.viewer3d.texture.TextureDataI;
+import org.jdesktop.swingx.JXTaskPaneContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
-import java.awt.Dimension;
+import java.awt.*;
 
 /**
  * This test-run class / standalone program will pull in a mask/channel pair and display the results.
@@ -60,10 +65,9 @@ public class TestMaskChan3DViz {
 
                     RenderMappingI renderMapping = new ConfigurableColorMapping();
                     RenderablesLoadWorker loadWorker = new RenderablesLoadWorker(
-                            (JComponent)frame.getContentPane(),
                             new Chan3DVizDataSource( args ),
-                            mipWidget,
                             renderMapping,
+                            new TestControlCallback( mipWidget, renderMapping, frame.getContentPane() ),
                             settings
                     );
                     loadWorker.setResolver( new TrivialFileResolver() );
@@ -81,5 +85,71 @@ public class TestMaskChan3DViz {
             }
         });
 	}
+
+    public static class TestControlCallback implements AlignmentBoardControllable {
+        private Logger logger = LoggerFactory.getLogger( TestControlCallback.class );
+        private Mip3d mip3d;
+        private RenderMappingI renderMapping;
+
+        private Container container;
+
+        public TestControlCallback( Mip3d mip3d, RenderMappingI renderMapping, Container container ) {
+            this.mip3d = mip3d;
+            this.renderMapping = renderMapping;
+            this.container = container;
+        }
+
+        @Override
+        public void clearDisplay() {
+            mip3d.clear();
+        }
+
+        @Override
+        public void loadVolume(TextureDataI signalTexture, TextureDataI maskTexture) {
+
+            if ( ! mip3d.setVolume(
+                    signalTexture, maskTexture, renderMapping, (float)AlignmentBoardSettingsDialog.DEFAULT_GAMMA
+            ) ) {
+                logger.error( "Failed to load volume to mip3d." );
+            }
+
+        }
+
+        @Override
+        public void dataLoadComplete() {
+            mip3d.refresh();
+
+            // Strip any "show-loading" off the viewer.
+            container.removeAll();
+
+            // Add this last.  "show-loading" removes it.  This way, it is shown only
+            // when it becomes un-busy.
+            container.add(mip3d, BorderLayout.CENTER);
+        }
+
+        @Override
+        public void loadCompletion(boolean successful, boolean loadFiles, Throwable error) {
+            if ( successful ) {
+                container.invalidate();
+                container.validate();
+                container.repaint();
+
+                if ( loadFiles ) {
+                    mip3d.refresh();
+                }
+                else {
+                    mip3d.refreshRendering();
+                }
+
+            }
+            else {
+                container.removeAll();
+                container.invalidate();
+                container.validate();
+                container.repaint();
+                throw new RuntimeException( error );
+            }
+        }
+    }
 
 }
