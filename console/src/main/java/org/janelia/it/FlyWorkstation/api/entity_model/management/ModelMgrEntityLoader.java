c6d6b737238e342445c8a30407f94d22758c9fec
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-04-27 16:14:23 +0000

    added client-side loaders
    Former-commit-id: 7fd0e4a60b08c620e17d7654844aa99f9c3b7348

---
+++ console/src/main/java/org/janelia/it/FlyWorkstation/api/entity_model/management/ModelMgrEntityLoader.java
@@ -0,0 +1,30 @@
+package org.janelia.it.FlyWorkstation.api.entity_model.management;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.janelia.it.jacs.model.entity.Entity;
+import org.janelia.it.jacs.model.entity.EntityData;
+import org.janelia.it.jacs.shared.utils.entity.AbstractEntityLoader;
+
+/**
+ * Client-side implementation of the entity loader interface, using the ModelMgr.
+ * 
+ * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
+ */
+public class ModelMgrEntityLoader implements AbstractEntityLoader {
+
+	public ModelMgrEntityLoader() {
+	}
+	
+	@Override
+	public Set<EntityData> getParents(Entity entity) throws Exception {
+		return new HashSet<EntityData>(ModelMgr.getModelMgr().getParentEntityDatas(entity.getId()));
+	}
+
+	@Override
+	public Entity populateChildren(Entity entity) throws Exception {
+	    ModelMgr.getModelMgr().loadLazyEntity(entity, false);
+		return entity;
+	}	
+}
