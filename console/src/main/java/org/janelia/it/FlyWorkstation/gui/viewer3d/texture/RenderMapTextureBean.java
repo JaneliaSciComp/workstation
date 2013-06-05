bd4e4376553710dcd586b564a301e79d0812440d
Les Foster <fosterl@janelia.hhmi.org>
2013-06-05 14:12:23 +0000

    More flexible solution to the multiple-sizing problem for the renderMap+selexCoords texture.  Using 6K. May experiment with smaller multiples....
    Former-commit-id: 93952a81b0922600e927334811299e868ceaf6fd

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/texture/RenderMapTextureBean.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/viewer3d/texture/RenderMapTextureBean.java
@@ -23,13 +23,14 @@ public class RenderMapTextureBean implements TextureDataI {
 
     private static final int BYTES_PER_ENTRY = 4;
     private static final int MAP_SIZE = 65536;
-    private static final int MAX_COORD_SETS = 170;   // This number yields end Y value divisible by 4.
+    private static final int MAX_COORD_SETS = 256;   // This number yields end Y value divisible by 4.
     private static final int ENTRIES_PER_COORD_SET = 6;
     private static final int BYTES_PER_COORD_SET = ENTRIES_PER_COORD_SET * BYTES_PER_ENTRY;
     private static final String HEADER = "Map of Colors to Neuron Fragment Numbers and Crop Coord Sets";
     private static final int LINE_WIDTH = 256;
     private static final float CONVENTIONAL_COORD_MULTIPLIER = 2048.0f;
-    private static final int DIVISIBILITY_VALUE = 512;
+    // This must be divisible by (4 x line-width) for the graphics card.  It must also be divisible by entries-per-set.
+    private static final int DIVISIBILITY_VALUE = (4 * LINE_WIDTH) * ENTRIES_PER_COORD_SET;
 
     private RenderMappingI renderMapping;
     private CropCoordSet cropCoordSet;
