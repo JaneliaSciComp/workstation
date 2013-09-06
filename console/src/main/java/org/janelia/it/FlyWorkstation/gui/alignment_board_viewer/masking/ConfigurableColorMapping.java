4a917b1f638a9117013b31e9d578c4ef52a2ea78
Les Foster <fosterl@janelia.hhmi.org>
2013-09-06 13:21:04 +0000

    Ordering the RenderableBean objects before any use, rather than only before multi-mask rank calculations.
    Former-commit-id: 5e647bfcd22d7c6c38f0473633251af5562d8833

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/alignment_board_viewer/masking/ConfigurableColorMapping.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/alignment_board_viewer/masking/ConfigurableColorMapping.java
@@ -1,8 +1,9 @@
 package org.janelia.it.FlyWorkstation.gui.alignment_board_viewer.masking;
 
+import org.janelia.it.FlyWorkstation.gui.alignment_board_viewer.renderable.InvertingComparator;
+import org.janelia.it.FlyWorkstation.gui.alignment_board_viewer.renderable.RBComparator;
 import org.janelia.it.FlyWorkstation.gui.alignment_board_viewer.renderable.RenderableBean;
 import org.janelia.it.jacs.model.entity.Entity;
-import org.janelia.it.jacs.model.entity.EntityConstants;
 
 import java.util.*;
 
@@ -141,60 +142,4 @@ public class ConfigurableColorMapping implements RenderMappingI {
         return prioritizedMasks;
     }
 
-    private static class RBComparator implements Comparator<RenderableBean> {
-
-        private Map<String,Integer> rankMapping;
-        {
-            rankMapping = new HashMap<String,Integer>();
-            rankMapping.put( EntityConstants.TYPE_NEURON_FRAGMENT, 1 );
-            rankMapping.put( EntityConstants.TYPE_COMPARTMENT, 3 );
-            rankMapping.put( EntityConstants.TYPE_SAMPLE, 9 );
-            rankMapping.put( EntityConstants.TYPE_COMPARTMENT_SET, 10 );
-        }
-
-        /**
-         * Comparator for sorting renderable beans.  Should return descending order.
-         *
-         * @param second one handed in as 1st param.
-         * @param first one handed in as 2nd param.
-         * @return Negative : right < left; positive: right > left; 0: same</>
-         */
-        @Override
-        public int compare(RenderableBean first, RenderableBean second) {
-            int rtnVal = 0;
-            if ( first == null  &&   second == null ) {
-                return 0;
-            }
-            else if ( first == null ) {
-                rtnVal = 1;
-            }
-            else if ( second == null ) {
-                rtnVal = -1;
-            }
-            else if ( first.getType().equals(second.getType()) ) {
-                // Must compare the contents.  Ranks among same-typed renderables are ordered by size.
-                rtnVal = (int)(first.getVoxelCount() - second.getVoxelCount());
-            }
-            else {
-                String firstTypeName = first.getType();
-                int typeRankFirst = rankMapping.get(firstTypeName);
-                String secondTypeName = second.getType();
-                int typeRankSecond = rankMapping.get(secondTypeName);
-
-                rtnVal = typeRankSecond - typeRankFirst;
-            }
-            return rtnVal;
-        }
-
-    }
-
-    private static class InvertingComparator implements Comparator<RenderableBean> {
-        private Comparator<RenderableBean> wrappedComparator;
-        public InvertingComparator( Comparator<RenderableBean> wrappedComparator ) {
-            this.wrappedComparator = wrappedComparator;
-        }
-        public int compare(RenderableBean first, RenderableBean second) {
-            return wrappedComparator.compare( first, second ) * -1;
-        }
-    }
 }
