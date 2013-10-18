fb296fccd2017e53b8e05f84a00543ed999165ba
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-10-18 20:46:04 +0000

    added yoshi alignment spaces changed signature
    Former-commit-id: 9f2528a4949ab1dd28783769c32ce55c0475de0f

--- console/src/main/java/org/janelia/it/FlyWorkstation/model/domain/AlignmentContextFactory.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/model/domain/AlignmentContextFactory.java
@@ -1,27 +1,30 @@
 package org.janelia.it.FlyWorkstation.model.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created with IntelliJ IDEA.
- * User: fosterl
- * Date: 10/3/13
- * Time: 1:46 PM
- *
+ * Created with IntelliJ IDEA. User: fosterl Date: 10/3/13 Time: 1:46 PM
+ * 
  * Gets me all the alignment spaces.
  */
 public class AlignmentContextFactory {
+    
     public AlignmentContextFactory() {
     }
 
     /**
      * Returns all alignment contexts known.
-     *
+     * 
      * @return anything for which an alignemnt board (etc.) may be created.
      */
-    public AlignmentContext[] getAllAlignmentContexts() {
+    public List<AlignmentContext> getAllAlignmentContexts() {
         // TODO get this information from the database.
-        return new AlignmentContext[] {
-                new AlignmentContext("Unified 20x Alignment Space", "0.62x0.62x0.62", "1024x512x218"),
-                new AlignmentContext("Unified 20x Alignment Space", "0.38x0.38x0.38", "1712x1370x492")
-        };
+        List<AlignmentContext> contexts = new ArrayList<AlignmentContext>();
+        contexts.add(new AlignmentContext("Unified 20x Alignment Space", "0.62x0.62x0.62", "1024x512x218"));
+        contexts.add(new AlignmentContext("Unified 20x Alignment Space", "0.38x0.38x0.38", "1712x1370x492"));
+        contexts.add(new AlignmentContext("Yoshi 20x Alignment Space", "0.46x0.46x0.46", "1184x592x218"));
+        contexts.add(new AlignmentContext("Yoshi 63x Subsampled Alignment Space", "0.38x0.38x0.38", "1450x725x436"));
+        return contexts;
     }
 }
