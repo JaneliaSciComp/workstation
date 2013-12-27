e6a4b1190699a794e3b2295d37fd90ca045d36e6
Todd Safford <saffordt@janelia.hhmi.org>
2013-12-27 13:21:11 +0000

    Changes to support the new long term storage in nobackup
    Former-commit-id: 4d7bc2d116539283efb3e9dd70d34137d647d7da

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/viewer/baseball_card/BaseballCard.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/framework/viewer/baseball_card/BaseballCard.java
@@ -1,8 +1,6 @@
 package org.janelia.it.FlyWorkstation.gui.framework.viewer.baseball_card;
 
 import org.janelia.it.FlyWorkstation.api.entity_model.management.ModelMgr;
-import org.janelia.it.FlyWorkstation.gui.framework.table.DynamicColumn;
-import org.janelia.it.FlyWorkstation.gui.framework.table.DynamicTable;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.DynamicImagePanel;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.ImagesPanel;
 import org.janelia.it.FlyWorkstation.shared.workers.SimpleWorker;
@@ -16,7 +14,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +29,7 @@ import java.util.concurrent.Callable;
  */
 public class BaseballCard {
     // images/Aligned63xScale_signal.png"
-    private static final String DEFAULT_IMAGE_PATH = "/archive/scicomp/jacsData/filestore/nerna/Alignment/819/554/1874649241884819554/align/Aligned63xScale_signal.png"; // Get Stacy's black watermark.
+    private static final String DEFAULT_IMAGE_PATH = "/nobackup/jacs/jacsData/filestore/nerna/Alignment/819/554/1874649241884819554/align/Aligned63xScale_signal.png"; // Get Stacy's black watermark.
     public static final int IMAGE_WIDTH = 100;
     public static final int IMAGE_HEIGHT = 100;
     private Entity entity;
