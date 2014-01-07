23a5d1acfb49802ce9ff66322576a71af4f9d118
Les Foster <fosterl@janelia.hhmi.org>
2014-01-07 17:52:10 +0000

    Updated test, with more scenarios.
    Former-commit-id: 9729756e7cd27a02d9d1be28e9b6bf8aec033077

--- console/src/test/java/org/janelia/it/FlyWorkstation/gui/alignment_board_viewer/channel_split/NBitChannelSplitStrategyTest.java
+++ console/src/test/java/org/janelia/it/FlyWorkstation/gui/alignment_board_viewer/channel_split/NBitChannelSplitStrategyTest.java
@@ -30,6 +30,8 @@ public class NBitChannelSplitStrategyTest {
 
     @Test
     public void splitTwoByteChannels() throws Exception {
+        System.out.println("Split 2-byte channels.");
+
         // Will keep only this one meta-data, but keep switching its renderable bean.
         ChannelMetaData channelMetaData = new ChannelMetaData();
         channelMetaData.byteCount = 2;
@@ -40,22 +42,7 @@ public class NBitChannelSplitStrategyTest {
         channelMetaData.redChannelInx = 0;
 
         // The beans of interest will be those numbers seen in bulding up the mask-tracker.
-        Set<Integer> masks = new HashSet<Integer>();
-        for ( MultiMaskTracker.MultiMaskBean bean: tracker.getMultiMaskBeans().values() ) {
-            masks.addAll( bean.getAltMasks() );
-        }
-
-        Set<RenderableBean> renderables = new HashSet<RenderableBean>();
-        for ( Integer altMask: masks ) {
-            Long randomCount = (long)(new Random( System.currentTimeMillis() ).nextInt( 8 ));
-            RenderableBean newRenderable = new RenderableBean();
-            newRenderable.setVoxelCount( randomCount );
-            newRenderable.setLabelFileNum( altMask );
-            newRenderable.setTranslatedNum( altMask );
-            newRenderable.setType( "Neuron Fragment" );
-
-            renderables.add(newRenderable);
-        }
+        Set<RenderableBean> renderables = getMockRenderableBeans();
 
         // Finally, traverse input values to see how the outputs look.
         NBitChannelSplitStrategy channelSplitter = new NBitChannelSplitStrategy(tracker, 4);
@@ -70,8 +57,9 @@ public class NBitChannelSplitStrategyTest {
         channelsData[ 3 ] = (byte)3;
         channelsData[ 4 ] = (byte)121;
         channelsData[ 5 ] = (byte)0;
-        byte[] finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 1, channelsData, 57);
-        dumpAttempt(1, channelsData, finalChannelsData);
+        int multiMaskId = 57;
+        byte[] finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 1, channelsData, multiMaskId);
+        dumpAttempt(1, channelsData, finalChannelsData, multiMaskId);
 
         channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
         channelsData[ 0 ] = (byte)6;
@@ -80,8 +68,8 @@ public class NBitChannelSplitStrategyTest {
         channelsData[ 3 ] = (byte)9;
         channelsData[ 4 ] = (byte)4;
         channelsData[ 5 ] = (byte)4;
-        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 2, channelsData, 57);
-        dumpAttempt(2, channelsData, finalChannelsData);
+        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 2, channelsData, multiMaskId);
+        dumpAttempt(2, channelsData, finalChannelsData, multiMaskId);
 
         channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
         channelsData[ 0 ] = (byte)0;
@@ -90,21 +78,101 @@ public class NBitChannelSplitStrategyTest {
         channelsData[ 3 ] = (byte)7;
         channelsData[ 4 ] = (byte)121;
         channelsData[ 5 ] = (byte)0;
-        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 4, channelsData, 57);
-        dumpAttempt(4, channelsData, finalChannelsData);
+        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 4, channelsData, multiMaskId);
+        dumpAttempt(4, channelsData, finalChannelsData, multiMaskId);
+        System.out.println();
     }
 
     @Test
     public void splitOneByteChannels() throws Exception {
-        // Will keep only this one meta-data, but keep switching its renderable bean.
-        ChannelMetaData channelMetaData = new ChannelMetaData();
-        channelMetaData.byteCount = 1;
-        channelMetaData.channelCount = 4;
-        channelMetaData.rawChannelCount = 3;
-        channelMetaData.blueChannelInx = 2;
-        channelMetaData.greenChannelInx = 1;
-        channelMetaData.redChannelInx = 0;
+        System.out.println();
+        System.out.println("Split 1-byte channels.");
+        ChannelMetaData channelMetaData = makeOneByteChannelMetaData();
+        Set<RenderableBean> renderables = getMockRenderableBeans();
 
+        // Finally, traverse input values to see how the outputs look.
+        NBitChannelSplitStrategy channelSplitter = new NBitChannelSplitStrategy(tracker, 4);
+        channelMetaData.renderableBean = renderables.iterator().next();
+        // Channels-data is little endian.  Plugging in values with that assumption.
+        byte[] channelsData = null;
+
+        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
+        channelsData[ 0 ] = (byte)115;
+        channelsData[ 1 ] = (byte)255;
+        channelsData[ 2 ] = (byte)7;
+        channelsData[ 3 ] = (byte)3;
+        int multiMaskId = 57;
+        int origMask = 1;
+        byte[] finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, origMask, channelsData, multiMaskId);
+        dumpAttempt(origMask, channelsData, finalChannelsData, multiMaskId);
+
+        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
+        channelsData[ 0 ] = (byte)6;
+        channelsData[ 1 ] = (byte)6;
+        channelsData[ 2 ] = (byte)9;
+        channelsData[ 3 ] = (byte)9;
+        origMask = 2;
+        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, origMask, channelsData, multiMaskId);
+        dumpAttempt(origMask, channelsData, finalChannelsData, multiMaskId);
+
+        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
+        channelsData[ 0 ] = (byte)0;
+        channelsData[ 1 ] = (byte)3;
+        channelsData[ 2 ] = (byte)25;
+        channelsData[ 3 ] = (byte)7;
+        origMask = 4;
+        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, origMask, channelsData, multiMaskId);
+        dumpAttempt(origMask, channelsData, finalChannelsData, multiMaskId);
+
+        System.out.println();
+    }
+
+    @Test
+    public void splitDepthOfFive() {
+        System.out.println();
+        System.out.println("-=====================Testing Split Depth of 5");
+        // Here, setup with the normal boilerplate.
+        ChannelMetaData channelMetaData = makeOneByteChannelMetaData();
+        NBitChannelSplitStrategy channelSplitter = new NBitChannelSplitStrategy(tracker, 4);
+        Set<RenderableBean> renderables = getMockRenderableBeans();
+        channelMetaData.renderableBean = renderables.iterator().next();
+
+        // Next, mock out the test scenario.
+        byte[] channelsData = null;
+
+        /*  This dump is output from the MultiMaskTrackerTest.  Using it to drive _this_ test.
+        Looking at multimask 69
+        12
+        11
+        13
+        14
+        15
+         */
+        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
+        channelsData[ 0 ] = (byte)115;
+        channelsData[ 1 ] = (byte)255;
+        channelsData[ 2 ] = (byte)7;
+        channelsData[ 3 ] = (byte)3;
+
+        int multiMaskId = 69;
+        addSplitData(channelMetaData, channelSplitter, channelsData, 12, multiMaskId);
+        channelsData[ 1 ] = 2;
+        addSplitData(channelMetaData, channelSplitter, channelsData, 11, multiMaskId);
+        channelsData[ 0 ] = 17;
+        channelsData[ 3 ] = 27;
+        addSplitData(channelMetaData, channelSplitter, channelsData, 13, multiMaskId);
+        addSplitData(channelMetaData, channelSplitter, channelsData, 14, multiMaskId);
+        addSplitData(channelMetaData, channelSplitter, channelsData, 15, multiMaskId);
+        System.out.println();
+    }
+
+    private void addSplitData(ChannelMetaData channelMetaData, NBitChannelSplitStrategy channelSplitter, byte[] channelsData, int origMask, int multiMaskId) {
+        byte[] orableChannelPart;
+        orableChannelPart = channelSplitter.getUpdatedValue( channelMetaData, origMask, channelsData, multiMaskId );
+        dumpAttempt(origMask, channelsData, orableChannelPart, multiMaskId);
+    }
+
+    private Set<RenderableBean> getMockRenderableBeans() {
         // The beans of interest will be those numbers seen in bulding up the mask-tracker.
         Set<Integer> masks = new HashSet<Integer>();
         for ( MultiMaskTracker.MultiMaskBean bean: tracker.getMultiMaskBeans().values() ) {
@@ -122,44 +190,32 @@ public class NBitChannelSplitStrategyTest {
 
             renderables.add(newRenderable);
         }
-
-        // Finally, traverse input values to see how the outputs look.
-        NBitChannelSplitStrategy channelSplitter = new NBitChannelSplitStrategy(tracker, 4);
-        channelMetaData.renderableBean = renderables.iterator().next();
-        // Channels-data is little endian.  Plugging in values with that assumption.
-        byte[] channelsData = null;
-
-        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
-        channelsData[ 0 ] = (byte)115;
-        channelsData[ 1 ] = (byte)255;
-        channelsData[ 2 ] = (byte)7;
-        channelsData[ 3 ] = (byte)3;
-        byte[] finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 1, channelsData, 57);
-        dumpAttempt(1, channelsData, finalChannelsData);
-
-        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
-        channelsData[ 0 ] = (byte)6;
-        channelsData[ 1 ] = (byte)6;
-        channelsData[ 2 ] = (byte)9;
-        channelsData[ 3 ] = (byte)9;
-        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 2, channelsData, 57);
-        dumpAttempt(2, channelsData, finalChannelsData);
-
-        channelsData = new byte[ channelMetaData.channelCount * channelMetaData.byteCount ];
-        channelsData[ 0 ] = (byte)0;
-        channelsData[ 1 ] = (byte)3;
-        channelsData[ 2 ] = (byte)25;
-        channelsData[ 3 ] = (byte)7;
-        finalChannelsData = channelSplitter.getUpdatedValue(channelMetaData, 4, channelsData, 57);
-        dumpAttempt(4, channelsData, finalChannelsData);
+        return renderables;
     }
 
-    private void dumpAttempt(int altMask, byte[] channelsData, byte[] finalChannelsData) {
-        System.out.println("For channel split "+altMask+" of "+57);
+    private ChannelMetaData makeOneByteChannelMetaData() {
+        // Will keep only this one meta-data, but keep switching its renderable bean.
+        ChannelMetaData channelMetaData = new ChannelMetaData();
+        channelMetaData.byteCount = 1;
+        channelMetaData.channelCount = 4;
+        channelMetaData.rawChannelCount = 3;
+        channelMetaData.blueChannelInx = 2;
+        channelMetaData.greenChannelInx = 1;
+        channelMetaData.redChannelInx = 0;
+        return channelMetaData;
+    }
+
+    private void dumpAttempt(int origMask, byte[] channelsData, byte[] orableChannelsData, int multiMaskId) {
+        System.out.println("For channel split for incoming mask of "+origMask+" against multimask "+multiMaskId);
         System.out.print("input=");
         dumpResult(channelsData);
-        System.out.print("output=");
-        dumpResult( finalChannelsData );
+        System.out.print("OR-able Output=");
+        dumpResult( orableChannelsData );
+
+        MultiMaskTracker.MultiMaskBean multiMaskBean = tracker.getMultiMaskBean( multiMaskId );
+        int maskOffset = multiMaskBean.getMaskOffset( origMask );
+        System.out.println("Mask offset is " + maskOffset);
+        System.out.println();
     }
 
     private void dumpResult( byte[] result ) {
@@ -167,5 +223,10 @@ public class NBitChannelSplitStrategyTest {
             System.out.print( String.format( "%02x", result[ i ] ) );
         }
         System.out.println();
+        for ( int i = 0; i < result.length; i++ ) {
+            System.out.print( String.format( "%d ", result[ i ] ) );
+        }
+        System.out.println();
+
     }
 }
