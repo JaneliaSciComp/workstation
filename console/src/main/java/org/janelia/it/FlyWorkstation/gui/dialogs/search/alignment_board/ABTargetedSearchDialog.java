fb9ef3d6ada08ee8326dab84b460c26b0e73eaf8
Les Foster <fosterl@janelia.hhmi.org>
2014-01-30 19:31:21 +0000

    Next phase in this evolution--with some dead code to save in repos.  Soon making better changes.
    Former-commit-id: 3bf3de6426d9da06fd5a61c2ce8561d21a70e46e

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/search/alignment_board/ABTargetedSearchDialog.java
+++ console/src/main/java/org/janelia/it/FlyWorkstation/gui/dialogs/search/alignment_board/ABTargetedSearchDialog.java
@@ -1,7 +1,6 @@
 package org.janelia.it.FlyWorkstation.gui.dialogs.search.alignment_board;
 
-import org.apache.solr.client.solrj.SolrQuery;
-import org.janelia.it.FlyWorkstation.api.entity_model.management.ModelMgr;
+import org.hibernate.cfg.annotations.QueryBinder;
 import org.janelia.it.FlyWorkstation.gui.dialogs.ModalDialog;
 import org.janelia.it.FlyWorkstation.gui.dialogs.search.SearchConfiguration;
 import org.janelia.it.FlyWorkstation.gui.dialogs.search.SearchParametersPanel;
@@ -11,17 +10,12 @@ import org.janelia.it.FlyWorkstation.gui.framework.viewer.BaseballCardPanel;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.RootedEntityReceiver;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.baseball_card.BaseballCard;
 import org.janelia.it.FlyWorkstation.gui.framework.viewer.search.SolrResultsMetaData;
-import org.janelia.it.FlyWorkstation.model.domain.AlignmentContext;
-import org.janelia.it.FlyWorkstation.model.domain.EntityWrapperFactory;
-import org.janelia.it.FlyWorkstation.model.domain.Sample;
 import org.janelia.it.FlyWorkstation.model.entity.RootedEntity;
 import org.janelia.it.FlyWorkstation.model.viewer.AlignmentBoardContext;
 import org.janelia.it.FlyWorkstation.shared.workers.SimpleWorker;
 import org.janelia.it.jacs.compute.api.support.SolrQueryBuilder;
-import org.janelia.it.jacs.compute.api.support.SolrResults;
 import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
-import org.janelia.it.jacs.shared.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,10 +37,7 @@ import java.util.List;
  */
 public class ABTargetedSearchDialog extends ModalDialog {
 
-    private static final String SEARCH_HISTORY_MDL_PROP = "ABTargetedSearchDialog_SEARCH_HISTORY";
     private static final int DEFAULT_ROWS_PER_PAGE = 10;
-    private static final int MAX_RESULT_ROWS = 200;
-    private static final int MAX_QUERY_ROWS = 3500; //20000;
 
     private AlignmentBoardContext context;
     private Entity searchRoot;
@@ -107,7 +98,7 @@ public class ABTargetedSearchDialog extends ModalDialog {
         searchConfig.load();
         searchConfig.addConfigurationChangeListener(searchParamsPanel);
         searchParamsPanel.init(searchConfig);
-        SearchErrorHandler errorHandler = new SearchErrorHandler() {
+        SearchWorker.SearchErrorHandler errorHandler = new SearchWorker.SearchErrorHandler() {
             @Override
             public void handleError(Throwable th) {
                 ABTargetedSearchDialog.this.setVisible(false);
@@ -130,7 +121,7 @@ public class ABTargetedSearchDialog extends ModalDialog {
         searchParamsPanel.getActionMap().put("enterAction", searchAction);
 
 
-        List<String> searchHistory = (List<String>) SessionMgr.getSessionMgr().getModelProperty( SEARCH_HISTORY_MDL_PROP );
+        List<String> searchHistory = (List<String>) SessionMgr.getSessionMgr().getModelProperty( SearchWorker.SEARCH_HISTORY_MDL_PROP );
         if ( searchHistory == null ) {
             searchHistory = new ArrayList<String>();
         }
@@ -214,10 +205,10 @@ public class ABTargetedSearchDialog extends ModalDialog {
         private Long searchRootId;
         private AlignmentBoardContext context;
         private SearchParametersPanel queryBuilderSource;
-        private SearchErrorHandler errorHandler;
+        private SearchWorker.SearchErrorHandler errorHandler;
 
         public QueryLaunchAction(
-                SearchErrorHandler errorHandler,
+                SearchWorker.SearchErrorHandler errorHandler,
                 SearchParametersPanel queryBuilderSource,
                 String actionName,
                 BaseballCardPanel baseballCardPanel,
@@ -236,14 +227,55 @@ public class ABTargetedSearchDialog extends ModalDialog {
         public void actionPerformed(ActionEvent e) {
             // set the thing busy.
             showLoadingIndicator();
+
+            // Two searches: first gets the whole set of all things that can be matched.  On completion, it first up
+            // the second search.
             SearchWorker.SearchWorkerParam param = new SearchWorker.SearchWorkerParam();
-            param.setReceiver(baseballCardPanel);
+            param.setReceiver( baseballCardPanel );
             param.setContext(context.getAlignmentContext());
             param.setSearchRootId(searchRootId);
             param.setErrorHandler(errorHandler);
             param.setStartingRow( 0 );
             SimpleWorker worker = new SearchWorker( param, queryBuilderSource.getQueryBuilder(), context );
             worker.execute();
+
+/*
+            SearchWorker.SearchWorkerParam neuronSepQryParam = new SearchWorker.SearchWorkerParam();
+            RootedEntityReceiver rootedEntityReceiver = new RootedEntityReceiver() {
+                @Override
+                public void setRootedEntities(
+                        final List<RootedEntity> rootedNeuronSepEntities,
+                        SolrResultsMetaData solrResultsMetaData
+                ) {
+                    // Can use the returned entities to winnow the second search.
+                    param.setReceiver( new RootedEntityReceiver() {
+                        public void setRootedEntities( List<RootedEntity> entities, SolrResultsMetaData metadata ) {
+                            // Will use the neuron sep entities to filter THESE entities.
+                            baseballCardPanel.setRootedEntities( entities, metadata );
+                        }
+                    });
+                }
+            };
+
+            // This is the "all-in-space" search.
+            neuronSepQryParam.setContext(context.getAlignmentContext());
+            neuronSepQryParam.setErrorHandler(errorHandler);
+            neuronSepQryParam.setStartingRow( 0 );
+            SolrQueryBuilder neuronSepQryBuilder = queryBuilderSource.getQueryBuilder();
+            String alignSpaceName = context.getAlignmentContext().getAlignmentSpaceName();
+            String oRes = context.getAlignmentContext().getOpticalResolution();
+            String pRes = context.getAlignmentContext().getPixelResolution();
+            neuronSepQryBuilder.setSearchString(oRes + " " + pRes);
+            Map<String,Set<String>> searchFilters = new HashMap<String,Set<String>>();
+            Set<String> typeAlternates = new HashSet<String>();
+            typeAlternates.add(EntityConstants.TYPE_NEURON_SEPARATOR_PIPELINE_RESULT);
+            searchFilters.put("entity_type", typeAlternates);
+            neuronSepQryBuilder.setFilters(searchFilters);
+            SearchWorker neuronSepQryWorker = new SearchWorker(neuronSepQryParam, neuronSepQryBuilder, context);
+            neuronSepQryWorker.setMaxQueryRows( 0 );
+            neuronSepQryWorker.execute();
+*/
+
         }
         private void showLoadingIndicator() {
             baseballCardPanel.showLoadingIndicator();
@@ -251,232 +283,5 @@ public class ABTargetedSearchDialog extends ModalDialog {
 
     }
 
-    static interface SearchErrorHandler {
-        void handleError( Throwable th );
-    }
 
-    private static class SearchWorker extends SimpleWorker {
-
-        private Logger logger = LoggerFactory.getLogger(ABTargetedSearchDialog.class);
-        private SearchWorkerParam param;
-        private List<RootedEntity> rootedResults;
-        private SolrResultsMetaData resultsMetaData;
-        private SolrQueryBuilder queryBuilder;
-        private AlignmentBoardContext context;
-
-        public SearchWorker( SearchWorkerParam param, SolrQueryBuilder queryBuilder, AlignmentBoardContext context ) {
-            this.param = param;
-            this.queryBuilder = queryBuilder;
-            this.context = context;
-        }
-        @Override
-        protected void doStuff() throws Exception {
-            if ( param.getSearchRootId() != null ) {
-                queryBuilder.setRootId( param.getSearchRootId() );
-            }
-
-            Map<String,Set<String>> filters = new HashMap<String,Set<String>>();
-            Set<String> filterValues = new HashSet<String>();
-            filterValues.add( EntityConstants.TYPE_SAMPLE );
-            filterValues.add( EntityConstants.TYPE_NEURON_FRAGMENT );
-            filters.put( "entity_type", filterValues );
-
-            queryBuilder.setFilters( filters );
-            SolrQuery query = queryBuilder.getQuery();
-            query.setStart( param.getStartingRow() );
-            query.setRows( MAX_QUERY_ROWS );
-
-            SolrResults results = ModelMgr.getModelMgr().searchSolr( query );
-            List<Entity> resultList = results.getResultList();
-            rootedResults = getCompatibleRootedEntities( resultList );
-
-            resultsMetaData = new SolrResultsMetaData();
-            resultsMetaData.setNumHits( rootedResults.size() );
-            resultsMetaData.setRawNumHits( resultList.size() );
-            resultsMetaData.setSearchDuration(
-                    results.getResponse().getElapsedTime()
-            );
-
-            // Update search history.
-            String queryStr = queryBuilder.getSearchString();
-
-            if ( !StringUtils.isEmpty( queryStr ) ) {
-                resultsMetaData.setQueryStr( queryStr );
-                List<String> searchHistory = (List<String>)
-                        SessionMgr.getSessionMgr().getModelProperty(SEARCH_HISTORY_MDL_PROP);
-                if ( searchHistory == null ) {
-                    searchHistory = new ArrayList<String>();
-                }
-                if ( ! searchHistory.contains( queryStr ) ) {
-                    searchHistory.add( queryStr );
-                    // To preserve history, must push it into the model.
-                    SessionMgr.getSessionMgr().setModelProperty( SEARCH_HISTORY_MDL_PROP, searchHistory );
-                }
-            }
-         }
-
-        @Override
-        protected void hadSuccess() {
-            // Accept results and populate.
-            RootedEntityReceiver receiver = param.getReceiver();
-            receiver.setRootedEntities(
-                    rootedResults,
-                    resultsMetaData
-            );
-        }
-
-        @Override
-        protected void hadError(Throwable error) {
-            param.getErrorHandler().handleError( error );
-            SessionMgr.getSessionMgr().handleException(error);
-        }
-
-        /**
-         * Finds only the results that can be added to the context provided.  Also, moves up the hierarchy
-         * from raw entities to their rooted entities.
-         *
-         * @param entities from possibly many alingment contexts
-         * @return those from specific context.
-         */
-        private List<RootedEntity> getCompatibleRootedEntities( Collection<Entity> entities ) {
-            logger.info("Found {} raw entities.", entities.size());
-            List<RootedEntity> rtnVal = new ArrayList<RootedEntity>();
-
-            int nonCompatibleNeuronCount = 0;
-            int nonCompatibleSampleCount = 0;
-            // Next, walk each entity's tree looking for proper info.
-            MAX_OUT:
-            for ( Entity entity: entities ) {
-                try {
-                    // Now, to "prowl" the trees of the result list, to find out what can be added, here.
-                    if ( entity.getEntityTypeName().equals( EntityConstants.TYPE_SAMPLE ) ) {
-                        RootedEntity rootedEntity = null;
-                        Entity childEntity = entity.getChildren().iterator().next();
-                        rootedEntity =
-                                new RootedEntity( ModelMgr.getModelMgr().getAncestorWithType( childEntity, EntityConstants.TYPE_SAMPLE ) );
-
-                        if ( rootedEntity == null ) {
-                            logger.warn( "Did not find child/parent.  Instead wrapping with new rooted entity.");
-                            rootedEntity = new RootedEntity( entity );
-                        }
-
-                        if ( isSampleCompatible( param.getContext(), rootedEntity) ) {
-                            rtnVal.add( rootedEntity );
-                        }
-                        else {
-                            nonCompatibleSampleCount ++;
-                        }
-                    }
-                    else {
-                        // Find ancestor to figure out if it is compatible.
-                        if ( isNeuronCompatible(entity) ) {
-                            rtnVal.add( new RootedEntity( entity ) );
-                        }
-                        else {
-                            nonCompatibleNeuronCount ++;
-                        }
-
-                    }
-
-                } catch ( Exception ex ) {
-                    ex.printStackTrace();
-                    throw new RuntimeException( ex );
-                }
-
-                if ( rtnVal.size() >= MAX_RESULT_ROWS ) {
-                    logger.info("Hit maximum of {}.", MAX_RESULT_ROWS);
-                    break MAX_OUT;
-                }
-            }
-
-            logger.info( "Filtered to {} entities.", rtnVal.size() );
-            logger.info(
-                    "Non-compatible neurons: {}, non-compatible samples: {}.",
-                    nonCompatibleNeuronCount,
-                    nonCompatibleSampleCount
-            );
-            return rtnVal;
-        }
-
-        private boolean isSampleCompatible(AlignmentContext standardContext, RootedEntity entity) throws Exception {
-            boolean rtnVal;
-            boolean foundMatch = false;
-            Sample wrapper = (Sample) EntityWrapperFactory.wrap(entity);
-            List< AlignmentContext> contexts = wrapper.getAvailableAlignmentContexts();
-            Iterator<AlignmentContext> contextIterator = contexts.iterator();
-
-            while ( contextIterator.hasNext() && (! foundMatch) ) {
-                AlignmentContext nextContext = contextIterator.next();
-                if ( standardContext.equals( nextContext ) ) {
-                    foundMatch = true;
-                }
-
-            }
-
-            rtnVal = foundMatch;
-            return rtnVal;
-        }
-
-        private boolean isNeuronCompatible(Entity entity) throws Exception {
-
-            Entity separationEntity = ModelMgr.getModelMgr().getAncestorWithType( entity, EntityConstants.TYPE_NEURON_SEPARATOR_PIPELINE_RESULT );
-            if ( separationEntity == null ) {
-                return false;
-            }
-            Entity alignmentEntity = ModelMgr.getModelMgr().getAncestorWithType( separationEntity, EntityConstants.TYPE_ALIGNMENT_RESULT );
-            if ( alignmentEntity == null ) {
-                return false;
-            }
-            return context.isCompatibleAlignmentSpace( new RootedEntity( entity ), separationEntity, alignmentEntity, false );
-        }
-
-        public static class SearchWorkerParam {
-            private RootedEntityReceiver receiver;
-            private Long searchRootId;
-            private AlignmentContext context;
-            private SearchErrorHandler errorHandler;
-            private int startingRow;
-
-            public RootedEntityReceiver getReceiver() {
-                return receiver;
-            }
-
-            public void setReceiver(RootedEntityReceiver receiver) {
-                this.receiver = receiver;
-            }
-
-            public Long getSearchRootId() {
-                return searchRootId;
-            }
-
-            public void setSearchRootId(Long searchRootId) {
-                this.searchRootId = searchRootId;
-            }
-
-            public AlignmentContext getContext() {
-                return context;
-            }
-
-            public void setContext(AlignmentContext context) {
-                this.context = context;
-            }
-
-            public void setErrorHandler( SearchErrorHandler errorHandler ) {
-                this.errorHandler = errorHandler;
-            }
-
-            public SearchErrorHandler getErrorHandler() {
-                return errorHandler;
-            }
-
-            public int getStartingRow() {
-                return startingRow;
-            }
-
-            public void setStartingRow(int startingRow) {
-                this.startingRow = startingRow;
-            }
-        }
-
-    }
 }
