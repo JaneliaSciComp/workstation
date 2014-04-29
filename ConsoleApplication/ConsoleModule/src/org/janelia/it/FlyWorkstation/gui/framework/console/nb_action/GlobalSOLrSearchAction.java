2ab74345935278f91526fbc905aa8990200dc5c1
Les Foster <fosterl@janalia.hhmi.org>
2014-04-29 10:23:53 -0400

    Part of task FW-2411: Have recovered the search item, and given it its proper shortcut.
    Former-commit-id: 386a4c105801dccc562476fc91376bfece1423e5

---
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/FlyWorkstation/gui/framework/console/nb_action/GlobalSOLrSearchAction.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.janelia.it.FlyWorkstation.gui.framework.console.nb_action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Search",
+        id = "org.janelia.it.FlyWorkstation.gui.framework.console.nb_action.GlobalSOLrSearchAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_GlobalSOLrSearch"
+)
+@ActionReferences({
+    @ActionReference(path = "Menu/Search", position = 1000),
+    @ActionReference(path = "Shortcuts", name = "M-F")
+})
+@Messages("CTL_GlobalSOLrSearch=Search")
+public final class GlobalSOLrSearchAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    	SessionMgr.getBrowser().getGeneralSearchDialog().showDialog();
+    }
+}
