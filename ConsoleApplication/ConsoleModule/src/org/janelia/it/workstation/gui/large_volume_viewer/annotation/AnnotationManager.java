574fdead44068c167e4311e055cb6044f416345b
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2014-07-25 07:42:24 -0400

    added some timing code; left it in but commented out for future use
    Former-commit-id: 40fefdd0d907f9570621d2601bcf7a922b42ff7e

--- ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/AnnotationManager.java
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/AnnotationManager.java
@@ -1,5 +1,6 @@
 package org.janelia.it.workstation.gui.large_volume_viewer.annotation;
 
+import com.google.common.base.Stopwatch;
 import org.janelia.it.workstation.api.entity_model.management.ModelMgr;
 import org.janelia.it.workstation.geom.Vec3;
 import org.janelia.it.workstation.gui.framework.session_mgr.SessionMgr;
@@ -306,8 +307,12 @@ public class AnnotationManager
             protected void doStuff() throws Exception {
                 Vec3 finalLocation;
                 if (annotationModel.automatedRefinementEnabled()) {
+                    // Stopwatch stopwatch = new Stopwatch();
+                    // stopwatch.start();
                     PointRefiner refiner = new PointRefiner(quadViewUi.getSubvolumeProvider());
                     finalLocation = refiner.refine(xyz);
+                    // stopwatch.stop();
+                    // System.out.println("refined annotation; elapsed time = " + stopwatch.toString());
 
                     // System.out.println("add annotation: input point " + xyz);
                     // System.out.println("add annotation: refined point " + finalLocation);
