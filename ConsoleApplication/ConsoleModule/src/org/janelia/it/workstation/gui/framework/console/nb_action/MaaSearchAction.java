a129761613a043943fbb69d3f3c1d80458163b6c
lesfoster <lfoster.se.be@gmail.com>
2014-08-06 17:07:38 -0400

    Commented away the NetBeans annotations.  This Action is now unknown to the NetBeans RCP runnable.  Once it is confirmed that no users need to see this menu item at all (not even Arnim, should he ever return), this class may be completely jettisoned.
    Former-commit-id: 7a9972a502b3cb202f2e98fc92794be768c4d0fa

--- ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/framework/console/nb_action/MaaSearchAction.java
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/framework/console/nb_action/MaaSearchAction.java
@@ -11,6 +11,12 @@ import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
+/*
+NOTE: commented away these annotations.  This menu item is showing up
+ for all users, but should not.  I do not know whether this is required to
+ show for _any_ user. When I confirm it is not, this class may be deleted.
+
+   fosterl@janelia.hhmi.org
 
 @ActionID(
         category = "Search",
@@ -21,6 +27,7 @@ import org.openide.util.NbBundle.Messages;
 )
 @ActionReference(path = "Menu/Search", position = 1400)
 @Messages("CTL_MaaSearchAction=MAA Screen Search")
+*/
 public final class MaaSearchAction implements ActionListener {
 
     @Override
