3235c890ee4e9ddacc2745b3750ff642b5aebf80
Les Foster <fosterl@janalia.hhmi.org>
2014-09-03 09:42:58 -0400

    Reverting log level to debug, to avoid posting shader contents.
    Former-commit-id: 0098cee63aa95a1af81cefc6dcd0ca8290b97a9f

--- ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/viewer3d/shader/AbstractShader.java
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/viewer3d/shader/AbstractShader.java
@@ -93,7 +93,7 @@ public abstract class AbstractShader
 				programCollector.append("\n");
 			}
 			String progString = programCollector.toString();
-            logger.info("Shader contents: {}.", progString);
+            logger.debug("Shader contents: {}.", progString);
 			gl.glShaderSource(shaderId, 1, new String[]{progString}, (int[])null, 0);
 			gl.glCompileShader(shaderId);
 			
