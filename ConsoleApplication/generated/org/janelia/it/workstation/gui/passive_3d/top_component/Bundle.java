2bb8b4393b94f968612d169e9795dfa70dfdd258
Konrad Rokicki <rokickik@janelia.hhmi.org>
2014-10-22 14:35:45 -0400

    add bundle to git
    Former-commit-id: 0c423918e1c06f2e3429d91451b147dab31d38cd

---
+++ ConsoleApplication/generated/org/janelia/it/workstation/gui/passive_3d/top_component/Bundle.java
@@ -0,0 +1,27 @@
+package org.janelia.it.workstation.gui.passive_3d.top_component;
+/** Localizable strings for {@link org.janelia.it.workstation.gui.passive_3d.top_component}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Snapshot3d</i>
+     * @see Snapshot3dTopComponent
+     */
+    static String CTL_Snapshot3dAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Snapshot3dAction");
+    }
+    /**
+     * @return <i>LVV 3D Snapshot</i>
+     * @see Snapshot3dTopComponent
+     */
+    static String CTL_Snapshot3dTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Snapshot3dTopComponent");
+    }
+    /**
+     * @return <i>Large Volume Viewer's snapshot of data containing the crosshair.</i>
+     * @see Snapshot3dTopComponent
+     */
+    static String HINT_Snapshot3dTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_Snapshot3dTopComponent");
+    }
+    private void Bundle() {}
+}
