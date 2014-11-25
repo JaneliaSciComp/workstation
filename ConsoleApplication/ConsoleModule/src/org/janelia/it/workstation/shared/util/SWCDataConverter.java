c7cc48aca0b554a9fce8e45a04706edca6ea9f2d
Les Foster <fosterl@janalia.hhmi.org>
2014-11-25 10:08:49 -0500

    Second writeback cycle partially mitigated by using the alternate writeback when no auto-points exist.
    Former-commit-id: a5b5cf72d643865c873a9d7366af915d162c6433

--- ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/shared/util/SWCDataConverter.java
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/shared/util/SWCDataConverter.java
@@ -33,8 +33,8 @@ public class SWCDataConverter {
         double xcenter = center.getX();
         double ycenter = center.getY();
         double zcenter = center.getZ();
-        List<SWCNode> nodeList = null;
-        if (downsampleModulo == 0) {
+        List<SWCNode> nodeList;
+        if (downsampleModulo == 0 || neuron.getAnchoredPathMap().isEmpty()) {
             nodeList = nodesFromSubtrees(neuron, xcenter, ycenter, zcenter);
         }        
         else {
