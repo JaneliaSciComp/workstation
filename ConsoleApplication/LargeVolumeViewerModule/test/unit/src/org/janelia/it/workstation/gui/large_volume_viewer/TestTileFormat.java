c5f726d12e0bb750f1ac8f16cbe6ddc56fa94661
Les Foster <fosterl@janalia.hhmi.org>
2015-01-02 17:21:59 -0500

    FW-2850: Looking at unit tests.  Sanity test fails on zoom level non-zero (3), once setup for stage coordinates.  Numbers are 1696, -1864, -0.05 vs expected 160, -72, 0.05. Former-commit-id: 078c7366df5141dc33b4c1d0e372c661e8d6d81e

--- ConsoleApplication/LargeVolumeViewerModule/test/unit/src/org/janelia/it/workstation/gui/large_volume_viewer/TestTileFormat.java
+++ ConsoleApplication/LargeVolumeViewerModule/test/unit/src/org/janelia/it/workstation/gui/large_volume_viewer/TestTileFormat.java
@@ -56,68 +56,40 @@ public class TestTileFormat {
     @Category(TestCategories.FastTests.class)
     public void testTileIndexForXyz() {
 		TileFormat tileFormat = createAavFormat();
-		
-		// Test simple upper left front corner tile
-		Vec3 xyz1 = new Vec3(0, 0, 0);
-		TileIndex ix1 = tileFormat.tileIndexForXyz(xyz1, 0, CoordinateAxis.Z);
-		assertEquals(0, ix1.getX());
-		assertEquals(63, ix1.getY());
-		assertEquals(0, ix1.getZ());
+		sanityTest(tileFormat);		
+	}
 
-		// Test general tile
-		Vec3 xyz2 = new Vec3(29952.0, 24869.6, 1243.5);
-		TileIndex ix2 = tileFormat.tileIndexForXyz(xyz2, 0, CoordinateAxis.Z);
-		assertEquals(29, ix2.getX());
-		assertEquals(39, ix2.getY());
-		assertEquals(1243, ix2.getZ());
-		
-		sanityCheckXyz(new Vec3(0,0,0), tileFormat, 0);
-		sanityCheckXyz(new Vec3(2048,2048,0), tileFormat, 0);
-		sanityCheckXyz(new Vec3(2047,2047,0), tileFormat, 0);
-		sanityCheckXyz(new Vec3(2049,2049,0), tileFormat, 0);
-		
-		// Non-zero zoom
-		sanityCheckXyz(new Vec3(0,0,0), tileFormat, 3);
-		sanityCheckXyz(new Vec3(2048,2048,0), tileFormat, 3);
-		sanityCheckXyz(new Vec3(2047,2047,0), tileFormat, 3);
-		sanityCheckXyz(new Vec3(2049,2049,0), tileFormat, 3);
-		
-		// TODO - non-Z slices
+	@Test
+    @Category(TestCategories.FastTests.class)
+    public void testTileIndexForXyzAsStage() {
+		TileFormat tileFormat = createAavFormat();
+        // Add sufficient information to make these coords stage coords.
+        // Using convenient numbers for the offsets and pixel sizes.
+        tileFormat.setOrigin(new int[] {
+            200000, 140000, 19000
+        });
+        tileFormat.setVoxelMicrometers(
+            new double[] {
+                0.5, 0.25, 0.1
+            }
+        );
+        Vec3[] vecComparison = new Vec3[] {
+            new Vec3(160,  -72,  0.05),
+            new Vec3(2208, 1976, 0.05),
+            new Vec3(2208, 2232, 0.05),
+            new Vec3(2720, 2232, 0.05)
+        };
+        final int[] ixComparison1 = new int[]{ -195, 200, -19000 };
+        final int[] ixComparison2 = new int[] {-136, 103, -6565};
+
+		sanityTest(tileFormat, ixComparison1, ixComparison2, vecComparison);		
 	}
 
 	@Test
     @Category(TestCategories.FastTests.class)
 	public void testCornersForTileIndex() {
 		TileFormat tileFormat = createAavFormat();
-
-		// upper left front corner tile
-		TileIndex ix1 = new TileIndex(0, 63, 0,
-				0, 0, 
-				TileIndex.IndexStyle.OCTREE,
-				CoordinateAxis.Z);
-		Vec3 corners[] = cornersForTileIndex(ix1, tileFormat);
-		// Order of corners should be like this:
-		//  0 --- 1
-		//  |     |
-		//  |     |
-		//  2 --- 3
-		//
-		// X coordinate : the easy case
-		double epsilon = 1e-6;
-		assertEquals(0, corners[0].getX(), epsilon);
-		assertEquals(0, corners[2].getX(), epsilon);
-		assertEquals(1024.0, corners[1].getX(), epsilon);
-		assertEquals(1024.0, corners[3].getX(), epsilon);
-		// Y : trickier because of inversion between Raveler and image order
-		assertEquals(0.0, corners[0].getY(), epsilon);
-		assertEquals(0.0, corners[1].getY(), epsilon);
-		assertEquals(1024.0, corners[2].getY(), epsilon);
-		assertEquals(1024.0, corners[3].getY(), epsilon);
-		// Z : constant
-		assertEquals(0.5, corners[0].getZ(), epsilon);
-		assertEquals(0.5, corners[1].getZ(), epsilon);
-		assertEquals(0.5, corners[2].getZ(), epsilon);
-		assertEquals(0.5, corners[3].getZ(), epsilon);
+		testCorners(tileFormat);
 	}
 
 	/**
@@ -173,4 +145,76 @@ public class TestTileFormat {
 		return result;
 	}
 	
+    private void sanityTest(TileFormat tileFormat) {
+        // Test simple upper left front corner tile
+        Vec3[] vecComparison = new Vec3[] {
+            new Vec3(0,0,0),
+            new Vec3(2048,2048,0),
+            new Vec3(2047,2047,0),
+            new Vec3(2049,2049,0)
+        };
+        sanityTest(tileFormat, new int[]{ 0, 63, 0}, new int[] {29, 39, 1243}, vecComparison);
+        // TODO - non-Z slices
+    }
+
+    private void sanityTest(TileFormat tileFormat, int[] ixComparison1, int[] ixComparison2, Vec3[] cornerComparison) {
+        // Test simple upper left front corner tile
+        Vec3 xyz1 = new Vec3(0, 0, 0);
+        TileIndex ix1 = tileFormat.tileIndexForXyz(xyz1, 0, CoordinateAxis.Z);
+        assertEquals(ixComparison1[0], ix1.getX());
+        assertEquals(ixComparison1[1], ix1.getY());
+        assertEquals(ixComparison1[2], ix1.getZ());
+        
+        // Test general tile
+        Vec3 xyz2 = new Vec3(29952.0, 24869.6, 1243.5);
+        TileIndex ix2 = tileFormat.tileIndexForXyz(xyz2, 0, CoordinateAxis.Z);
+        assertEquals(ixComparison2[0], ix2.getX());
+        assertEquals(ixComparison2[1], ix2.getY());
+        assertEquals(ixComparison2[2], ix2.getZ());
+        
+        sanityCheckXyz(cornerComparison[0], tileFormat, 0);
+        sanityCheckXyz(cornerComparison[1], tileFormat, 0);
+        sanityCheckXyz(cornerComparison[2], tileFormat, 0);
+        sanityCheckXyz(cornerComparison[3], tileFormat, 0);
+        
+        // Non-zero zoom
+        sanityCheckXyz(cornerComparison[0], tileFormat, 3);
+        sanityCheckXyz(cornerComparison[1], tileFormat, 3);
+        sanityCheckXyz(cornerComparison[2], tileFormat, 3);
+        sanityCheckXyz(cornerComparison[3], tileFormat, 3);
+        
+        // TODO - non-Z slices
+    }
+
+    private void testCorners(TileFormat tileFormat) {
+        // upper left front corner tile
+        TileIndex ix1 = new TileIndex(0, 63, 0,
+                0, 0,
+                TileIndex.IndexStyle.OCTREE,
+                CoordinateAxis.Z);
+        Vec3 corners[] = cornersForTileIndex(ix1, tileFormat);
+        // Order of corners should be like this:
+        //  0 --- 1
+        //  |     |
+        //  |     |
+        //  2 --- 3
+        //
+        // X coordinate : the easy case
+        double epsilon = 1e-6;
+        assertEquals(0, corners[0].getX(), epsilon);
+        assertEquals(0, corners[2].getX(), epsilon);
+        assertEquals(1024.0, corners[1].getX(), epsilon);
+        assertEquals(1024.0, corners[3].getX(), epsilon);
+        // Y : trickier because of inversion between Raveler and image order
+        assertEquals(0.0, corners[0].getY(), epsilon);
+        assertEquals(0.0, corners[1].getY(), epsilon);
+        assertEquals(1024.0, corners[2].getY(), epsilon);
+        assertEquals(1024.0, corners[3].getY(), epsilon);
+        // Z : constant
+        assertEquals(0.5, corners[0].getZ(), epsilon);
+        assertEquals(0.5, corners[1].getZ(), epsilon);
+        assertEquals(0.5, corners[2].getZ(), epsilon);
+        assertEquals(0.5, corners[3].getZ(), epsilon);
+    }
+
 }
