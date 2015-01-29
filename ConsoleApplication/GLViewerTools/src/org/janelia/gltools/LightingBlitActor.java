0bdc8ac5b18ef3e79727779e781b59649ac9e04a
Christopher Bruns <brunsc@janelia.hhmi.org>
2015-01-29 13:42:03 -0500

    Horta now depends on shared workstation module. Jogl dependencies all migrated from shared module to independent modules.
    Former-commit-id: 41419352a1ab0b854f3e04445351c9f97e622cd4

--- ConsoleApplication/GLViewerTools/src/org/janelia/gltools/LightingBlitActor.java
+++ ConsoleApplication/GLViewerTools/src/org/janelia/gltools/LightingBlitActor.java
@@ -31,6 +31,7 @@
 package org.janelia.gltools;
 
 import java.io.IOException;
+import java.io.InputStream;
 import javax.media.opengl.GL2ES2;
 import javax.media.opengl.GL3;
 import org.janelia.geometry3d.AbstractCamera;
@@ -58,12 +59,20 @@ public class LightingBlitActor extends BasicScreenBlitActor
          // Simple shader to blit color buffer to screen
         BasicShaderProgram blitProgram = new BasicShaderProgram() {};
         try {
+            // debug crash here
+            String shaderPath = "/org/janelia/gltools/material/shader/"
+                                            + "RenderPassVrtx.glsl";
+            InputStream shaderStream = getClass().getResourceAsStream(shaderPath);
+            ShaderStep shaderStep = new ShaderStep(GL2ES2.GL_VERTEX_SHADER, shaderStream);
+            blitProgram.getShaderSteps().add(shaderStep);
+            /*
             blitProgram.getShaderSteps().add(new ShaderStep(
                         GL2ES2.GL_VERTEX_SHADER, 
                         getClass().getResourceAsStream(
                                     "/org/janelia/gltools/material/shader/"
                                             + "RenderPassVrtx.glsl"))
             );
+                    */
             blitProgram.getShaderSteps().add(new ShaderStep(
                         GL2ES2.GL_FRAGMENT_SHADER, 
                         getClass().getResourceAsStream(
