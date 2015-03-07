6650032c93e4bd357df1173bac4002947ca25d40
murphys <murphys@janelia.hhmi.org>
2015-03-07 08:52:56 -0500

    Simplified GS form
    Former-commit-id: ba3ef009a3a9737fca607cc9128d5f3ee672beed

--- ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/GeometricSearchTopComponent.java
+++ ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/GeometricSearchTopComponent.java
@@ -24,7 +24,7 @@ import org.openide.util.NbBundle.Messages;
         //iconBase="SET/PATH/TO/ICON/HERE", 
         persistenceType = TopComponent.PERSISTENCE_ALWAYS
 )
-@TopComponent.Registration(mode = "explorer", openAtStartup = false)
+@TopComponent.Registration(mode = "editor", openAtStartup = false)
 @ActionID(category = "Window", id = "org.janelia.it.workstation.gui.geometric_search.GeometricSearchTopComponent")
 @ActionReference(path = "Menu/Window" /*, position = 333 */)
 @TopComponent.OpenActionRegistration(
@@ -57,91 +57,17 @@ public final class GeometricSearchTopComponent extends TopComponent {
 
         geometricSearchTabPane = new javax.swing.JTabbedPane();
         searchTabPanel = new javax.swing.JPanel();
-        queryPanel = new javax.swing.JPanel();
-        queryLabel = new javax.swing.JLabel();
-        alignmentBoardPanel = new javax.swing.JPanel();
-        resultPanel = new javax.swing.JPanel();
-        resultLabel = new javax.swing.JLabel();
         adminTabPanel = new javax.swing.JPanel();
-
-        ////////////////// SEARCH TAB /////////////////////
-
-        org.openide.awt.Mnemonics.setLocalizedText(queryLabel, org.openide.util.NbBundle.getMessage(GeometricSearchTopComponent.class, "GeometricSearchTopComponent.queryLabel.text")); // NOI18N
-
-        // Query
-        javax.swing.GroupLayout queryPanelLayout = new javax.swing.GroupLayout(queryPanel);
-        queryPanel.setLayout(queryPanelLayout);
-        queryPanelLayout.setHorizontalGroup(
-            queryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, queryPanelLayout.createSequentialGroup()
-                .addContainerGap(20, Short.MAX_VALUE)
-                .addComponent(queryLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(17, 17, 17))
-        );
-        queryPanelLayout.setVerticalGroup(
-            queryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(queryPanelLayout.createSequentialGroup()
-                .addComponent(queryLabel)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-
-        // Alignment Board
-        javax.swing.GroupLayout alignmentBoardPanelLayout = new javax.swing.GroupLayout(alignmentBoardPanel);
-        alignmentBoardPanel.setLayout(alignmentBoardPanelLayout);
-        alignmentBoardPanelLayout.setHorizontalGroup(
-            alignmentBoardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
-        alignmentBoardPanelLayout.setVerticalGroup(
-            alignmentBoardPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 373, Short.MAX_VALUE)
-        );
-
-        // Result Panel
-        org.openide.awt.Mnemonics.setLocalizedText(resultLabel, org.openide.util.NbBundle.getMessage(GeometricSearchTopComponent.class, "GeometricSearchTopComponent.resultLabel.text")); // NOI18N
-
-        javax.swing.GroupLayout resultPanelLayout = new javax.swing.GroupLayout(resultPanel);
-        resultPanel.setLayout(resultPanelLayout);
-        resultPanelLayout.setHorizontalGroup(
-            resultPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(resultPanelLayout.createSequentialGroup()
-                .addGap(300, 300, 300)
-                .addComponent(resultLabel)
-                .addContainerGap(287, Short.MAX_VALUE))
-        );
-        resultPanelLayout.setVerticalGroup(
-            resultPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(resultPanelLayout.createSequentialGroup()
-                .addComponent(resultLabel)
-                .addGap(0, 99, Short.MAX_VALUE))
-        );
 
         javax.swing.GroupLayout searchTabPanelLayout = new javax.swing.GroupLayout(searchTabPanel);
         searchTabPanel.setLayout(searchTabPanelLayout);
         searchTabPanelLayout.setHorizontalGroup(
             searchTabPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(searchTabPanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(queryPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(searchTabPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(searchTabPanelLayout.createSequentialGroup()
-                        .addComponent(alignmentBoardPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addContainerGap())
-                    .addComponent(resultPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+            .addGap(0, 823, Short.MAX_VALUE)
         );
         searchTabPanelLayout.setVerticalGroup(
             searchTabPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(searchTabPanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(searchTabPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(searchTabPanelLayout.createSequentialGroup()
-                        .addComponent(queryPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addContainerGap())
-                    .addGroup(searchTabPanelLayout.createSequentialGroup()
-                        .addComponent(alignmentBoardPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(resultPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGap(0, 500, Short.MAX_VALUE)
         );
 
         geometricSearchTabPane.addTab(org.openide.util.NbBundle.getMessage(GeometricSearchTopComponent.class, "GeometricSearchTopComponent.searchTabPanel.TabConstraints.tabTitle"), searchTabPanel); // NOI18N
@@ -156,8 +82,6 @@ public final class GeometricSearchTopComponent extends TopComponent {
             adminTabPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 500, Short.MAX_VALUE)
         );
-
-        /////////////// ADMIN TAB ////////////////
 
         geometricSearchTabPane.addTab(org.openide.util.NbBundle.getMessage(GeometricSearchTopComponent.class, "GeometricSearchTopComponent.adminTabPanel.TabConstraints.tabTitle"), adminTabPanel); // NOI18N
 
@@ -182,12 +106,7 @@ public final class GeometricSearchTopComponent extends TopComponent {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel adminTabPanel;
-    private javax.swing.JPanel alignmentBoardPanel;
     private javax.swing.JTabbedPane geometricSearchTabPane;
-    private javax.swing.JLabel queryLabel;
-    private javax.swing.JPanel queryPanel;
-    private javax.swing.JLabel resultLabel;
-    private javax.swing.JPanel resultPanel;
     private javax.swing.JPanel searchTabPanel;
     // End of variables declaration//GEN-END:variables
     @Override
