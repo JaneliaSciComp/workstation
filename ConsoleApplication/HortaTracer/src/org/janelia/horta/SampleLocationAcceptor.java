dbe2cbf7f46846b64bec0ee43e44de39a10edf15
Les Foster <fosterl@janalia.hhmi.org>
2015-03-09 16:12:41 -0400

    FW-3011: Eliminated an inner class from NeuronTracerTopComponent. Former-commit-id: 2ddfc423657923a8777060696a7b899c44b169b1

---
+++ ConsoleApplication/HortaTracer/src/org/janelia/horta/SampleLocationAcceptor.java
@@ -0,0 +1,113 @@
+/* 
+ * Licensed under the Janelia Farm Research Campus Software Copyright 1.1
+ * 
+ * Copyright (c) 2014, Howard Hughes Medical Institute, All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     1. Redistributions of source code must retain the above copyright notice, 
+ *        this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright 
+ *        notice, this list of conditions and the following disclaimer in the 
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the Howard Hughes Medical Institute nor the names 
+ *        of its contributors may be used to endorse or promote products derived 
+ *        from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * REASONABLE ROYALTIES; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.janelia.horta;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import org.janelia.console.viewerapi.ViewerLocationAcceptor;
+import org.janelia.geometry3d.Vantage;
+import org.janelia.geometry3d.Vector3;
+import static org.janelia.horta.NeuronTracerTopComponent.BASE_YML_FILE;
+import org.janelia.horta.volume.StaticVolumeBrickSource;
+import org.janelia.scenewindow.SceneWindow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleLocationAcceptor implements ViewerLocationAcceptor {
+
+    private String currentSource;
+    private NeuronTraceLoader loader;
+    private YamlStreamLoader yamlLoader;
+    private SceneWindow sceneWindow;
+    private StaticVolumeBrickSource volumeSource;
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleLocationAcceptor.class);
+    
+    public SampleLocationAcceptor(
+            String currentSource, 
+            NeuronTraceLoader loader, 
+            YamlStreamLoader yamlLoader, 
+            SceneWindow sceneWindow, 
+            StaticVolumeBrickSource volumeSource) {
+        this.currentSource = currentSource;
+        this.loader = loader;
+        this.yamlLoader = yamlLoader;
+        this.sceneWindow = sceneWindow;
+        this.volumeSource = volumeSource;
+    }
+    
+    @Override
+    public void acceptLocation(URL focusUrl, double[] focusCoords) throws Exception {
+        // First ensure that this component uses same sample.
+        if (focusUrl != null) {
+            String urlStr = focusUrl.toString();
+            // Check: if same as current source, no need to change that.
+            if (!urlStr.equals(currentSource)) {
+                URI uri = focusUrl.toURI();
+                URI yamlUri = new URI(
+                        uri.getScheme(),
+                        uri.getAuthority(),
+                        uri.getPath() + "/" + BASE_YML_FILE,
+                        uri.getFragment()
+                );
+                logger.info("Constructed URI: {}.", uri);
+                URL yamlUrl = yamlUri.toURL();
+                InputStream stream1 = yamlUrl.openStream();
+                InputStream stream2 = yamlUrl.openStream();
+                volumeSource = yamlLoader.loadYaml(stream1, loader, stream2);
+            }
+
+                // Now, position this component over other component's
+            // focus.
+            if (focusCoords != null) {
+                Vantage v = sceneWindow.getVantage();
+                Vector3 focusVector3 = new Vector3(
+                        (float) focusCoords[0],
+                        (float) focusCoords[1],
+                        (float) focusCoords[2]
+                );
+                loader.animateToFocusXyz(focusVector3, v, 150);
+                    //                    v.setFocusPosition(focusVector3);
+                //                    v.notifyObservers();
+            } else {
+                logger.info("No focus coords provided.");
+            }
+
+            // Load up the tile.
+            loader.loadTileAtCurrentFocus(volumeSource);
+            sceneWindow.getGLAutoDrawable().display();
+        } else {
+            logger.warn("No URL location provided.");
+        }
+
+    }
+
+}
