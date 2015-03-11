79d9333c23c09fec23e602439dd0721230c86b42
murphys <murphys@janelia.hhmi.org>
2015-03-11 15:40:54 -0400

    Adding debug code
    Former-commit-id: ca7728da832db96da64eaa32b194a258db3f9e11

--- ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/search/GeometricSearchPanel.java
+++ ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/search/GeometricSearchPanel.java
@@ -2,6 +2,9 @@ package org.janelia.it.workstation.gui.geometric_search.search;
 
 import org.janelia.it.workstation.gui.framework.outline.Refreshable;
 
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
 import javax.swing.*;
 
 import org.janelia.it.workstation.gui.viewer3d.BaseRenderer;
@@ -18,7 +21,8 @@ import java.awt.*;
 public class GeometricSearchPanel extends JPanel implements Refreshable {
 
     private final Logger logger = LoggerFactory.getLogger(GeometricSearchPanel.class);
-    Mip3d mip3d;
+    TestMip3d mip3d;
+    TestGLJPanel testGLJPanel;
 
 
     @Override
@@ -31,6 +35,12 @@ public class GeometricSearchPanel extends JPanel implements Refreshable {
         }
 
         mip3d.refresh();
+
+//        if (testGLJPanel==null) {
+//            createMip3d();
+//        }
+        //testGLJPanel.display();
+
     }
 
     @Override
@@ -41,6 +51,7 @@ public class GeometricSearchPanel extends JPanel implements Refreshable {
 
     private void createMip3d() {
         logger.info("*** createMip3d()");
+
         if ( mip3d != null ) {
             mip3d.releaseMenuActions();
         }
@@ -51,10 +62,19 @@ public class GeometricSearchPanel extends JPanel implements Refreshable {
         double pixelsPerSceneUnit = Math.abs(BaseRenderer.DISTANCE_TO_SCREEN_IN_PIXELS / cameraFocusDistance);
         logger.info("createMip3d() cameraFocusDistance=" + cameraFocusDistance + " pixelsPerSceneUnit=" + pixelsPerSceneUnit);
         mip3d.getVolumeModel().getCamera3d().setPixelsPerSceneUnit(pixelsPerSceneUnit);
-        mip3d.addActor(new TestBlueTileActor());
         mip3d.resetView();
         mip3d.setResetFirstRedraw(true);
+        mip3d.addActor(new TestBlueTileActor());
+        mip3d.resetView();
+        mip3d.refresh();
         add(mip3d, BorderLayout.CENTER);
+
+//        if (testGLJPanel==null) {
+//            testGLJPanel=new TestGLJPanel();
+//            testGLJPanel.setVisible(true);
+//            add(testGLJPanel, BorderLayout.CENTER);
+//        }
+
     }
 
     public void displayReady() {
@@ -62,5 +82,22 @@ public class GeometricSearchPanel extends JPanel implements Refreshable {
         createMip3d();
         logger.info("*** displayReady() done");
     }
+
+
+//    protected void render( GL2 gl2, int width, int height ) {
+//        gl2.glClear( gl2.GL_COLOR_BUFFER_BIT );
+//
+//        // draw a triangle filling the window
+//        gl2.glLoadIdentity();
+//        gl2.glBegin( gl2.GL_TRIANGLES );
+//        gl2.glColor3f( 1, 0, 0 );
+//        gl2.glVertex2f( 0, 0 );
+//        gl2.glColor3f( 0, 1, 0 );
+//        gl2.glVertex2f( width, 0 );
+//        gl2.glColor3f( 0, 0, 1 );
+//        gl2.glVertex2f( width / 2, height );
+//        gl2.glEnd();
+//    }
+
 
 }
