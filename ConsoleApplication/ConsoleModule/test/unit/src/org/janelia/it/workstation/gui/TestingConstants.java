c9fdd5ea14c6063ab8868b74ab53dac104f3aaed
lesfoster <fosterl@janelia.hhmi.org>
2015-05-25 23:47:39 -0400

    Simplistic realization of this test constants set.  Should replace with that from desktop system.
    Former-commit-id: 00d8075b046e26711ba465462cdb20a2629356fe

---
+++ ConsoleApplication/ConsoleModule/test/unit/src/org/janelia/it/workstation/gui/TestingConstants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.janelia.it.workstation.gui;
+
+/**
+ *
+ * @author fosterl
+ */
+public class TestingConstants {
+ 
+    public static final String COMPARTMENT_MASK_FILE_NAME = "";
+    public static final String LOCAL_COMPARTMENT_MASK_FILE_PATH = "";
+    public static final String COMPARTMENT_CHAN_FILE_NAME = "";
+    public static final String LOCAL_COMPARTMENT_CHAN_FILE_PATH = "";
+    public static final String LOCAL_NEURON_MASK_FILE_PATH = "";
+    public static final String LOCAL_NEURON_CHAN_FILE_PATH = "";
+}
