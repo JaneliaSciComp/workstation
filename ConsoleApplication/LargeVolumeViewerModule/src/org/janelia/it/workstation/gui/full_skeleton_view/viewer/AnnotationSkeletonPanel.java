e4f5985ca2cb52dec3a98ec02a4e06df915cf3ce
Les Foster <fosterl@janalia.hhmi.org>
2015-05-27 17:24:00 -0400

    FW-3169: Saving code state. Former-commit-id: e121c047ecd5fa234441341a710967ff422c3e83

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/full_skeleton_view/viewer/AnnotationSkeletonPanel.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/full_skeleton_view/viewer/AnnotationSkeletonPanel.java
@@ -151,7 +151,7 @@ public class AnnotationSkeletonPanel extends JPanel {
         configurator.setAxisLengths( new double[] { boundingBox.getMaxX(), boundingBox.getMaxY(), boundingBox.getMaxZ() } );
         //configurator.setAxisLengths(new double[]{100.0, 100.0, 100.0});
         configurator.setContext(context);
-        configurator.setMatrixScope(MeshDrawActor.MatrixScope.EXTERNAL);                  
+        configurator.setMatrixScope(MeshDrawActor.MatrixScope.LOCAL);                  
         
         final NeuronTraceVtxAttribMgr attributeManager = new NeuronTraceVtxAttribMgr();        
         attributeManager.setDataSource(dataSource);
