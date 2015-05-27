b4c7b035599155ae201afd640c45773e0dc16eee
lesfoster <fosterl@janelia.hhmi.org>
2015-05-27 13:01:18 -0400

    Good testing scenario: portable method to verifiably-upload viable vertices+normals.
    Former-commit-id: b88423d9ecd0293cf48a9e1b67f960cb80f51844

--- ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/publication_quality/mesh/actor/MeshDrawActor.java
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/publication_quality/mesh/actor/MeshDrawActor.java
@@ -250,15 +250,60 @@ public class MeshDrawActor implements GLActor {
 
         gl.glGenBuffers(1, handleArr, 0);
         inxBufferHandle = handleArr[ 0];
-        // One little triangle.
-        // Three coords, 3 normals.
+        // Borrowed from the successful upload method.
         final float[] vtxData = new float[]{
-            0.0f, 0.0f, 0.0f, // Coord 1
-            0.0f, 0.0f, -1.0f, // Normal 1
-            230.0f, 0.0f, 0.0f, // Coord 2            
-            0.0f, 0.0f, -1.0f, // Normal 2
-            -230.0f, 230.0f, 0.0f, // Coord 3
-            0.0f, 0.0f, -1.0f, // Normal 3
+            -0.5f, -0.5f, 0.5f,
+            -0.57735026f, -0.57735026f, 0.57735026f,
+            -0.5f, 0.5f, 0.5f,
+            -0.70710677f, 0.0f, 0.70710677f,
+            -0.5f, 0.5f, -0.5f,
+            -0.70710677f, 0.0f, -0.70710677f,
+            -0.5f, -0.5f, -0.5f,
+            -0.57735026f, -0.57735026f, -0.57735026f,
+            0.5f, 0.5f, 0.5f,
+            0.0f, -1.0f, 0.0f,
+            0.5f, -0.5f, 0.5f,
+            0.57735026f, -0.57735026f, 0.57735026f,
+            0.5f, -0.5f, -0.5f,
+            0.57735026f, -0.57735026f, -0.57735026f,
+            0.5f, 0.5f, -0.5f,
+            0.70710677f, 0.0f, -0.70710677f,
+            -0.5f, 1.5f, 0.5f,
+            -0.57735026f, 0.57735026f, 0.57735026f,
+            0.5f, 1.5f, 0.5f,
+            0.0f, 1.0f, 0.0f,
+            0.5f, 1.5f, -0.5f,
+            0.57735026f, 0.57735026f, -0.57735026f,
+            -0.5f, 1.5f, -0.5f,
+            -0.57735026f, 0.57735026f, -0.57735026f,
+            0.5f, 1.5f, 1.5f,
+            -0.57735026f, 0.57735026f, 0.57735026f,
+            1.5f, 1.5f, 1.5f,
+            0.57735026f, 0.57735026f, 0.57735026f,
+            1.5f, 1.5f, 0.5f,
+            0.57735026f, 0.57735026f, -0.57735026f,
+            0.5f, 0.5f, 1.5f,
+            -0.57735026f, -0.57735026f, 0.57735026f,
+            1.5f, 0.5f, 1.5f,
+            0.57735026f, -0.57735026f, 0.57735026f,
+            1.5f, 0.5f, 0.5f,
+            0.57735026f, -0.57735026f, -0.57735026f,
+            -0.5f, 0.5f, 5.5f,
+            -0.57735026f, 0.57735026f, 0.57735026f,
+            0.5f, 0.5f, 5.5f,
+            0.57735026f, 0.57735026f, 0.57735026f,
+            0.5f, 0.5f, 4.5f,
+            0.57735026f, 0.57735026f, -0.57735026f,
+            -0.5f, 0.5f, 4.5f,
+            -0.57735026f, 0.57735026f, -0.57735026f,
+            -0.5f, -0.5f, 5.5f,
+            -0.57735026f, -0.57735026f, 0.57735026f,
+            -0.5f, -0.5f, 4.5f,
+            -0.57735026f, -0.57735026f, -0.57735026f,
+            0.5f, -0.5f, 5.5f,
+            0.57735026f, -0.57735026f, 0.57735026f,
+            0.5f, -0.5f, 4.5f,
+            0.57735026f, -0.57735026f, -0.57735026f,
         };
         final int combinedVtxSize = vtxData.length * BYTES_PER_FLOAT;
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(
@@ -268,16 +313,13 @@ public class MeshDrawActor implements GLActor {
         FloatBuffer vertexAttribBuffer = byteBuffer.asFloatBuffer();
         vertexAttribBuffer.put( vtxData );
 
+        dumpFloatBuffer(vertexAttribBuffer);
+        
         vertexAttribBuffer.rewind();
         long bufferBytes = vertexAttribBuffer.capacity() * BYTES_PER_FLOAT;
         vertexAttribBuffer.rewind();
-        // Allocate enough remote buffer data for all the vertices/attributes
-        // to be thrown across in segments.
+        // Bind buffer, and push buffer data.
         gl.glBindBuffer(GL2GL3.GL_ARRAY_BUFFER, vtxAttribBufferHandle);
-//        RenderBuffersBean buffersBean
-//                = configurator.getVertexAttributeManager()
-//                .getRenderIdToBuffers()
-//                .get(configurator.getRenderableId());
         gl.glBufferData(
                 GL2GL3.GL_ARRAY_BUFFER,
                 bufferBytes,
@@ -286,16 +328,63 @@ public class MeshDrawActor implements GLActor {
         );
         reportError(gl, "Push Vertex Buffer");
                 
-        final int[] indices = new int[]{0, 1, 2, 0, 2, 1};
+        final int[] indices = new int[]{
+            0, 1, 2,
+            2, 3, 0,
+            4, 1, 0,
+            0, 5, 4,
+            5, 0, 3,
+            3, 6, 5,
+            6, 3, 2,
+            2, 7, 6,
+            5, 6, 7,
+            7, 4, 5,
+            8, 9, 10,
+            10, 11, 8,
+            1, 8, 11,
+            11, 2, 1,
+            9, 8, 1,
+            1, 4, 9,
+            7, 2, 11,
+            11, 10, 7,
+            4, 7, 10,
+            10, 9, 4,
+            12, 13, 14,
+            14, 9, 12,
+            15, 12, 9,
+            9, 4, 15,
+            13, 12, 15,
+            15, 16, 13,
+            16, 15, 4,
+            4, 17, 16,
+            17, 4, 9,
+            9, 14, 17,
+            16, 17, 14,
+            14, 13, 16,
+            18, 19, 20,
+            20, 21, 18,
+            22, 18, 21,
+            21, 23, 22,
+            19, 18, 22,
+            22, 24, 19,
+            24, 22, 23,
+            23, 25, 24,
+            25, 23, 21,
+            21, 20, 25,
+            24, 25, 20,
+            20, 19, 24,
+        };
         final int combinedIndexSize = indices.length * BYTES_PER_INT;
-        byteBuffer = ByteBuffer.allocateDirect(
-                combinedIndexSize);
+        byteBuffer = ByteBuffer.allocateDirect(combinedIndexSize);
+        byteBuffer.order(ByteOrder.nativeOrder());
         byteBuffer.rewind();
         IntBuffer indexBuffer = byteBuffer.asIntBuffer();
         indexBuffer.put( indices );
-        indexCount = indices.length;
         indexBuffer.rewind();
         bufferBytes = indexBuffer.capacity() * BYTES_PER_INT;
+        indexCount = indexBuffer.capacity();
+        logger.info("Index Count = " + indexCount);
+        dumpIntBuffer(indexBuffer);
         indexBuffer.rewind();
                 
         gl.glBindBuffer(GL2GL3.GL_ELEMENT_ARRAY_BUFFER, inxBufferHandle);
@@ -309,6 +398,36 @@ public class MeshDrawActor implements GLActor {
         logger.info("Done uploading buffers");
     }
     
+    private void dumpFloatBuffer(FloatBuffer attribBuffer) {
+        attribBuffer.rewind();
+        StringBuilder bldr = new StringBuilder();
+        for (int i = 0; i < attribBuffer.capacity(); i++) {
+            if (i % 3 == 0) {
+                bldr.append("\n");
+            }
+            float nextF = attribBuffer.get();
+            bldr.append(nextF + "f, ");
+        }
+        System.out.println("[------------- Buffer Contents -------------]");
+        logger.info(bldr.toString());
+        attribBuffer.rewind();
+    }
+
+    private void dumpIntBuffer(IntBuffer inxBuf) {
+        inxBuf.rewind();
+        StringBuilder bldr = new StringBuilder();
+        for (int i = 0; i < inxBuf.capacity(); i++) {
+            if (i % 3 == 0) {
+                bldr.append("\n");
+            }
+            int nextI = inxBuf.get();
+            bldr.append(nextI + ", ");
+        }
+        System.out.println("[------------- Index Buffer Contents -------------]");
+        logger.info(bldr.toString());
+        inxBuf.rewind();
+    }
+
     private void uploadBuffers(GL2GL3 gl, boolean flag) {
         // Push the coords over to GPU.
         // Make handles for subsequent use.
@@ -328,6 +447,7 @@ public class MeshDrawActor implements GLActor {
                         .get(configurator.getRenderableId());
         FloatBuffer attribBuffer = buffersBean.getAttributesBuffer();
         long bufferBytes = (long) (attribBuffer.capacity() * (BYTES_PER_FLOAT));
+        dumpFloatBuffer(attribBuffer);
         attribBuffer.rewind();
         gl.glBufferData(
                 GL2GL3.GL_ARRAY_BUFFER,
@@ -338,8 +458,10 @@ public class MeshDrawActor implements GLActor {
         reportError( gl, "Buffer Data" );
 
         IntBuffer inxBuf = buffersBean.getIndexBuffer();
+        dumpIntBuffer(inxBuf);
         inxBuf.rewind();
         indexCount = inxBuf.capacity();
+        logger.info("Index count=" + indexCount);
         gl.glBindBuffer(GL2GL3.GL_ELEMENT_ARRAY_BUFFER, inxBufferHandle);
         reportError(gl, "Bind Inx Buf");
         bufferBytes = (long)(inxBuf.capacity() * BYTES_PER_INT);
