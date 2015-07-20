4c51a5334d963b0fa94fdebb09e8fa59e32e0e3d
seandmurphy <seandmurphy@yahoo.com>
2015-07-20 16:39:52 -0400

    Geometric search update
    Former-commit-id: 80c4467a49fb155eac898dcb6cb45065a0a2416b

---
+++ ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/gl/oitarr/ArraySortShader_fragment.glsl
@@ -0,0 +1,75 @@
+#version 430
+
+layout (early_fragment_tests) in;
+
+in vec2 vs_tex_coord;
+
+struct OITArrNodeType {
+    vec4 color;
+    float depth;
+};
+
+uniform usampler2D head_pointer_image;
+
+uniform int hpi_width;
+uniform int hpi_height;
+uniform int hpi_depth;
+
+layout (binding = 0, rgba32ui) uniform coherent uimage1D fragment_buffer;
+
+layout(binding=0, std430) buffer fragmentArrays {
+    OITArrNodeType nodes[];
+};
+
+layout (location=0) out vec4 output_color;
+
+struct OITArrNodeType frags[hpi_depth];
+
+int build_local_fragment_list(void)
+{
+    int frag_count=0;
+    uint hpiFragCount = texelFetch(head_pointer_image, ivec2(gl_FragCoord.xy), 0).x;
+    int nodeOffset = (gl_FragCoord.y * hpi_width + gl_FragCoord.x) * hpi_depth;
+    while(hpiFragCount != 0xFFFFFFFF && frag_count < hpiFragCount && frag_count < hpi_depth) { // sanity test
+        frags[frag_count] = nodes[nodeOffset + frag_count];
+        frag_count++;
+    }
+    return frag_count;
+}
+
+void sort_fragment_list(int frag_count)
+{
+    int i;
+    int j;
+    for (i=0;i<frag_count;i++) {
+        for (j=i+1; j<frag_count;j++) {
+            float depth_i = frags[i].depth;
+            float depth_j = frags[j].depth;
+            if (depth_i > depth_j) {
+                struct OITArrNodeType temp = frags[i];
+                frags[i] = frags[j];
+                frags[j] = temp;
+            }
+        }
+    }
+}
+
+vec4 blend(vec4 current_color, vec4 new_color) {
+    return mix(current_color, new_color, new_color.a);
+}
+
+vec4 calculate_final_color(int frag_count) {
+    int i;
+    vec4 final_color = vec4(0.0, 0.0, 0.0, 0.0);
+    for (i=0; i < frag_count; i++) {
+       final_color = blend(final_color, frags[i].color);
+    }
+    return final_color;
+}
+
+void main(void) {
+    int frag_count=0;
+    frag_count = build_local_fragment_list();
+    sort_fragment_list(frag_count);
+    output_color = calculate_final_color(frag_count);
+}
---
+++ ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/gl/oitarr/ArrayTransparencyContext.java
@@ -0,0 +1,127 @@
+package org.janelia.it.workstation.gui.geometric_search.gl.oitarr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.media.opengl.GL4;
+import javax.media.opengl.glu.GLU;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+/**
+ * Created by murphys on 7/20/2015.
+ */
+public class ArrayTransparencyContext {
+
+    private final Logger logger = LoggerFactory.getLogger(ArrayTransparencyContext.class);
+
+    protected static GLU glu = new GLU();
+
+    public static int width;
+    public static int height;
+    public static int depth;
+
+    public static final int NODE_SIZE = 20; // vec4 color, float depth
+
+    IntBuffer headPointerId = IntBuffer.allocate(1);
+    IntBuffer headPointerInitializerId = IntBuffer.allocate(1);
+    IntBuffer fragmentSSBO = IntBuffer.allocate(1);
+    IntBuffer zeroValueBuffer = IntBuffer.allocate(1);
+
+    public ArrayTransparencyContext(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public int getHeadPointerTextureId() {
+        return headPointerId.get(0);
+    }
+
+    public int getFragmentSSBOId() {
+        return fragmentSSBO.get(0);
+    }
+
+    public int getHeadPointerInitializerId() {
+        return headPointerInitializerId.get(0);
+    }
+
+    protected void checkGlError(GL4 gl, String message) {
+        int errorNumber = gl.glGetError();
+        if (errorNumber <= 0)
+            return;
+        String errorStr = glu.gluErrorString(errorNumber);
+        logger.error( "OpenGL Error " + errorNumber + ": " + errorStr + ": " + message );
+    }
+
+    public void init(GL4 gl) throws Exception {
+
+        zeroValueBuffer.put(0,0);
+
+        int headPointerTotalPixels = width * height;
+
+        // Create PBO from which to clear the headPointerTexture
+        ByteBuffer bb = ByteBuffer.allocate(headPointerTotalPixels * 4);
+        byte ffb = -1; // 0xFF for java is -1, we want the shader to see unsigned int==0xFFFFFFFF
+        for (int i=0;i<bb.capacity();i++) {
+            bb.put(i, ffb);
+        }
+
+        gl.glGenBuffers(1, headPointerInitializerId);
+        checkGlError(gl, "i1 ArrayTransparencyContext glGenBuffers() error");
+
+        gl.glBindBuffer(GL4.GL_PIXEL_UNPACK_BUFFER, headPointerInitializerId.get(0));
+        checkGlError(gl, "i2 ArrayTransparencyContext glBindBuffer() error");
+
+        gl.glBufferData(GL4.GL_PIXEL_UNPACK_BUFFER,
+                headPointerTotalPixels * 4,
+                bb,
+                GL4.GL_STATIC_DRAW);
+        checkGlError(gl, "i3 ArrayTransparencyContext glBufferData() error");
+
+        // Allocate empty texture of correct size
+        gl.glGenTextures(1, headPointerId);
+        checkGlError(gl, "ArrayTransparencyContext glGenTextures() error");
+
+        gl.glBindTexture(GL4.GL_TEXTURE_2D, headPointerId.get(0));
+        checkGlError(gl, "ArrayTransparencyContext glBindTexture() error");
+
+        // MAJOR MYSTERY: glTexImage2D did not work with glBindImageTexture - silent non-functionality
+        gl.glTexStorage2D(GL4.GL_TEXTURE_2D, 1, GL4.GL_R32UI, width, height);
+        checkGlError(gl, "ArrayTransparencyContext glTexStorage2D() error");
+
+        // WHY IS THIS TRIGGERING AN INVALID ERROR? - may be harmless since working (?) in display stage
+        gl.glTexSubImage2D(GL4.GL_TEXTURE_2D,
+                0,
+                0,
+                0,
+                height,
+                width,
+                GL4.GL_RED_INTEGER,
+                GL4.GL_UNSIGNED_INT,
+                0);
+        checkGlError(gl, "i0 ArrayTransparencyContext glTexSubImage2D() error");
+
+        gl.glBindBuffer(GL4.GL_PIXEL_UNPACK_BUFFER, 0);
+        checkGlError(gl, "i1 ArrayTransparencyContext glBindBuffer() error");
+
+        // SSBO
+        gl.glGenBuffers(1, fragmentSSBO);
+        gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 0, fragmentSSBO.get(0));
+        gl.glBufferData(GL4.GL_SHADER_STORAGE_BUFFER, headPointerTotalPixels * NODE_SIZE, null, GL4.GL_DYNAMIC_DRAW);
+
+    }
+
+}
