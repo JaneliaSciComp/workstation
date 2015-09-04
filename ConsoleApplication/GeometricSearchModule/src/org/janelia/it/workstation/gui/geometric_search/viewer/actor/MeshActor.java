547a6a8959d654cfd6e76c6b5f22b1114879c81e
seandmurphy <seandmurphy@yahoo.com>
2015-09-04 16:29:28 -0400

    Geometric search update
    Former-commit-id: 3498f6255ffd9ffafc11af623cd98cc4a9cad9c7

--- ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/viewer/actor/MeshActor.java
+++ ConsoleApplication/GeometricSearchModule/src/org/janelia/it/workstation/gui/geometric_search/viewer/actor/MeshActor.java
@@ -6,8 +6,10 @@ import org.janelia.it.workstation.gui.geometric_search.viewer.gl.GL4SimpleActor;
  * Created by murphys on 8/6/2015.
  */
 public class MeshActor extends Actor {
+
     @Override
     public GL4SimpleActor createAndSetGLActor() {
         return null;
     }
+
 }
