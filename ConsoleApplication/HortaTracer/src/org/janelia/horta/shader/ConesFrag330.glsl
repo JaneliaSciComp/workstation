108005014b8d7bbf8fe68edceadc293822f520b0
Christopher Bruns <brunsc@janelia.hhmi.org>
2015-09-10 09:46:39 -0400

    Add license text to imposter shaders.
    Former-commit-id: 42c1753830df2ea84e4305575e976bc54d061524

--- ConsoleApplication/HortaTracer/src/org/janelia/horta/shader/ConesFrag330.glsl
+++ ConsoleApplication/HortaTracer/src/org/janelia/horta/shader/ConesFrag330.glsl
@@ -4,9 +4,39 @@
  * Truncated cone imposter fragment shader.
  */
 
+/* 
+ * Licensed under the Janelia Farm Research Campus Software Copyright 1.1
+ * 
+ * Copyright (c) 2014, Howard Hughes Medical Institute, All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     1. Redistributions of source code must retain the above copyright notice, 
+ *        this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright 
+ *        notice, this list of conditions and the following disclaimer in the 
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the Howard Hughes Medical Institute nor the names 
+ *        of its contributors may be used to endorse or promote products derived 
+ *        from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * REASONABLE ROYALTIES; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 uniform vec4 color = vec4(0, 1, 0, 1); // one color for all cones
 uniform mat4 projectionMatrix; // needed for proper depth calculation
-uniform sampler2D lightProbe;
+uniform sampler2D lightProbe; // for image-based-lighting (IBL)
 
 
 in float fragRadius; // average radius of cone
@@ -46,7 +76,7 @@ void main() {
     float qe_half_a, discriminant;
     cone_nonlinear_coeffs(tAP, qe_c, qe_half_b, qe_undot_half_a, qe_half_a, discriminant);
     if (discriminant < 0)
-        discard; // not on cone
+        discard; // ray does not intersect cone
 
     // Compute projected surface of cone
     vec3 s = cone_surface_from_coeffs(imposterPos, qe_half_b, qe_half_a, discriminant);
