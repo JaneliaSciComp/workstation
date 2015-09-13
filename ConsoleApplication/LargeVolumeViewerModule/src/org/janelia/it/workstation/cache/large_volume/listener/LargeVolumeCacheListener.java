8eacce548cd2ee5d6ae468b44db3da684ee0fc05
lesfoster <lfoster.se.be@gmail.com>
2015-09-13 14:44:57 -0400

    Added a listener.
    Former-commit-id: 57abfb297d56a7304668fa7b80a01fe741428a0e

---
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/cache/large_volume/listener/LargeVolumeCacheListener.java
@@ -0,0 +1,54 @@
+package org.janelia.it.workstation.cache.large_volume.listener;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Leslie L Foster
+ */
+public class LargeVolumeCacheListener implements CacheEventListener {
+
+	private Logger log = LoggerFactory.getLogger(LargeVolumeCacheListener.class);
+	
+	@Override
+	public void notifyElementRemoved(Ehcache ehch, Element elmnt) throws CacheException {
+		log.info("Element {} removed.", elmnt.getKey());
+	}
+
+	@Override
+	public void notifyElementPut(Ehcache ehch, Element elmnt) throws CacheException {
+	}
+
+	@Override
+	public void notifyElementUpdated(Ehcache ehch, Element elmnt) throws CacheException {
+	}
+
+	@Override
+	public void notifyElementExpired(Ehcache ehch, Element elmnt) {
+	}
+
+	@Override
+	public void notifyElementEvicted(Ehcache ehch, Element elmnt) {
+	}
+
+	@Override
+	public void notifyRemoveAll(Ehcache ehch) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+	
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+}
+/*
+CacheEventListener 
+*/
