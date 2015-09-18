adb86588feeda5136714e34b0e7c1e365124aee9
porterbot <schauderd@janelia.hhmi.org>
2015-09-18 10:20:23 -0400

    added Spock unit test for RESTful server API
    Former-commit-id: 5adcac9a5ffb70ebc53e5eef98473004a44889f0

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/api/facade/impl/RESTDomainFacade.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/api/facade/impl/RESTDomainFacade.java
@@ -297,9 +297,11 @@ public class RESTDomainFacade implements DomainFacade {
         }
         Response response = serviceEndpoints.get("treenode")
                 .path("children")
-                .queryParam("subjectKey", SessionMgr.getSubjectKey())
+                .queryParam("subjectKey", SessionMgr.getSubjectKey()),
+                .queryParam("treeNodeId", treeNode.getId())
+                .queryParam("children", references)
                 .request("application/json")
-                .post(Entity.json(treeNode));
+                .post();
         TreeNode updatedTreeNode = response.readEntity(TreeNode.class);
         int responseStatus = response.getStatus();
 
