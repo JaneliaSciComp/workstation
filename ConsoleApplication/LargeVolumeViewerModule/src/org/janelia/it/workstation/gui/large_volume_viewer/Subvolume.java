ba9231ded3ff05328a700a09fa021088d36d74fa
Les Foster <fosterl@janelia.hhmi.org>
2015-10-22 12:34:02 -0400

    Removed unused import.
    Former-commit-id: 8ba3dd54d5d0ad0c0c0fd0b8b58d6f4c7ec4c547

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/Subvolume.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/Subvolume.java
@@ -26,7 +26,6 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import org.janelia.it.jacs.shared.utils.ThreadUtils;
