297f7b7accd07898714673e6e1b030a355445aea
Les Foster <fosterl@janelia.hhmi.org>
2015-11-04 16:56:00 -0500

    Fixing up the path report.
    Former-commit-id: bdd4f74f9c609386a9f173a6eab989e4686bae3b

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/action/TileLocToClipboardAction.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/action/TileLocToClipboardAction.java
@@ -15,12 +15,15 @@ import org.janelia.it.workstation.gui.large_volume_viewer.MicronCoordsFormatter;
 import org.janelia.it.workstation.gui.large_volume_viewer.TileFormat;
 import org.janelia.it.workstation.gui.large_volume_viewer.TileIndex;
 import org.janelia.it.workstation.gui.large_volume_viewer.camera.BasicObservableCamera3d;
+import org.janelia.it.workstation.shared.util.SystemInfo;
 
 /**
  * Converts the text, as it is expected, in the status label, into a tile
  * location, and copies that into the clipboard, in a simple format.
  */
 public class TileLocToClipboardAction extends AbstractAction {
+    private final static String FILE_SEP = System.getProperty("path.separator");
+    private final static String LINUX_FILE_SEP = "/";
 
     private final JLabel statusLabel;
     private final TileFormat tileFormat;
@@ -49,7 +52,15 @@ public class TileLocToClipboardAction extends AbstractAction {
         
         TileIndex index = tileFormat.tileIndexForXyz(vec, tileFormat.zoomLevelForCameraZoom(camera.getPixelsPerSceneUnit()), axis);
         File path = BlockTiffOctreeLoadAdapter.getOctreeFilePath(index, tileFormat, true);
-        StringSelection selection = new StringSelection(path.getAbsolutePath());
+        String filePathStr = path.toString().replace(FILE_SEP, LINUX_FILE_SEP);
+        // Not truly looking for the file path; just the legs of the path.
+        if (SystemInfo.isWindows) {
+            int colonPos = filePathStr.indexOf(":");
+            if (colonPos != -1) {
+                filePathStr = filePathStr.substring(colonPos+1);
+            }
+        }
+        StringSelection selection = new StringSelection(filePathStr);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(selection, selection);
     }
