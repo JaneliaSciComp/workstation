1dfb16732fac130cbb65ece959a00dc825ba62a6
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2015-11-18 11:06:42 -0500

    added minimal workspace info dialog
    Former-commit-id: e7197b11e76b40e2d4ee246a3a4d86adbe634fee

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/AnnotationManager.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/AnnotationManager.java
@@ -1406,6 +1406,26 @@ public class AnnotationManager implements UpdateAnchorListener, PathTraceListene
         // worker.executeWithEvents();
     }
 
+    public void showWorkspaceInfoDialog() {
+        // implementation note; this is sloppy; in Raveler, I have a SessionInfoDialog class
+        //  that has a nice table layout and methods for adding new lines to that table
+        //  (including using blank lines as dividers); next time we need to add info
+        //  to this dialog, refactor and do it right
+
+        // you can also look to the Raveler dialog to see what kinds of info we could add
+        //  eg,  move sample name, path to here
+
+        int nneurons = annotationModel.getCurrentWorkspace().getNeuronList().size();
+        int nannotations = 0;
+        for (TmNeuron neuron: annotationModel.getCurrentWorkspace().getNeuronList()) {
+            nannotations += neuron.getGeoAnnotationMap().size();
+        }
+        JOptionPane.showMessageDialog(quadViewUi,
+                "# neurons = " + nneurons + "\n# annotations (total) = " + nannotations + "\n",
+                "Info",
+                JOptionPane.PLAIN_MESSAGE);
+    }
+
     public void exportAllNeuronsAsSWC(final File swcFile, final int downsampleModulo) {
         final List<Long> neuronIDList = new ArrayList<>();
         int nannotations = 0;
