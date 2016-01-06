b09cb0b27db93615ffc664a20775032b3c5991f1
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-01-06 09:46:03 -0500

    fixed re-selection
    Former-commit-id: a3bf0b3349e2cbd9b967f62acf2905c7a962e38c

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/events/selection/SelectionModel.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/events/selection/SelectionModel.java
@@ -42,20 +42,34 @@ public abstract class SelectionModel<T,S> {
     public final void select(T object, boolean clearAll) {
         log.debug("select {}, clear={}",object,clearAll);
         S id = getId(object);
-        if (clearAll && selected.contains(id) && selected.size()==1) {
-            // Already single selected
-            return;
+        if (!isCorrectlySelected(id, clearAll)) {
+            if (clearAll) {
+                selected.clear();
+            }
+            selected.add(id);
         }
-        if (!clearAll && selected.contains(id) && selected.size()>1) {
-            // Already multiple selected
-            return;
-        }
-        if (clearAll) {
-            selected.clear();
-        }
-        selected.add(id);
         selectionChanged(object, id, true, clearAll);
-        
+    }
+    
+    private boolean isCorrectlySelected(S id, boolean clearAll) {
+        if (selected.contains(id)) {
+            if (clearAll) {
+                if (selected.size()==1) {
+                    // Already single selected
+                    return true;
+                }
+                else {
+                    // Multi-selected, but needs to be single selected
+                    return false;
+                }
+            }
+            else {
+                // Already multi-selected
+                return true;
+            }
+        }
+        // Not selected
+        return false;
     }
 
     /**
