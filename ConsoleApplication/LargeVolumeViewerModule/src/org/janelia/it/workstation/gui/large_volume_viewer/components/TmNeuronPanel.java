8b694e0dba50dbecfc989ee8a04a20f1d33a0436
fosterl <fosterl@janalia.hhmi.org>
2016-03-01 10:07:32 -0500

    Added some checking of the contents of annotation parents.  Flagging cases where parent makes no sense.
    Former-commit-id: 0c202917b5e0cf1d0af43fa23460f0ebfda0d697

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/components/TmNeuronPanel.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/components/TmNeuronPanel.java
@@ -38,7 +38,17 @@ public class TmNeuronPanel extends JPanel {
         bldr.append("Geo Annotations").append(LINE_SEP);
         Map<Long,TmGeoAnnotation> annoMap = neuron.getGeoAnnotationMap();
         for (Long geoAnnoId: annoMap.keySet()) {
-            bldr.append("    ").append(geoAnnoId).append("->").append(display(annoMap.get(geoAnnoId))).append(LINE_SEP);
+            final TmGeoAnnotation geoAnno = annoMap.get(geoAnnoId);
+            bldr.append("    ").append(geoAnnoId).append("->").append(display(geoAnno)).append(LINE_SEP);
+            if (! annoMap.keySet().contains(geoAnno.getParentId())) {
+                if (! geoAnno.getParentId().equals(neuron.getId())) {
+                    bldr.append("*** ERROR: ").append(geoAnno.getId())
+                        .append(" has invalid parent. ")
+                        .append(geoAnno.getParentId())
+                        .append(" Neither geo anno nor neuron.")
+                        .append(LINE_SEP);
+                }
+            }
         }
         
         textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
@@ -56,6 +66,21 @@ public class TmNeuronPanel extends JPanel {
         geoAnnoBldr.append(" XYZ=(" + tmGeoAnno.getX() + "," + tmGeoAnno.getY() + "," + tmGeoAnno.getZ() + ")");
         geoAnnoBldr.append(" Parent=" + tmGeoAnno.getParentId());
         geoAnnoBldr.append(" Radius=" + tmGeoAnno.getRadius());
+        if (tmGeoAnno.isRoot()) {
+            geoAnnoBldr.append(" o--");
+        }
+        else if (tmGeoAnno.isBranch()) {
+            geoAnnoBldr.append(" --<");
+        }
+        else if (tmGeoAnno.isEnd()) {
+            geoAnnoBldr.append(" --o");
+        }
+        else if (tmGeoAnno.isLink()) {
+            geoAnnoBldr.append(" ---");
+        }
+        else {
+            geoAnnoBldr.append(" ???");
+        }                
                 
         return geoAnnoBldr;
     }
