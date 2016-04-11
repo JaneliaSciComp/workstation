5accb4bde4b05b272e78bc3b4db798d467b4abd6
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-04-11 17:04:26 -0400

    ETL fixes
    Former-commit-id: 231634732fc02b55785d357dac1bed1f2d50c380

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/api/facade/impl/rest/RESTClientImpl.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/api/facade/impl/rest/RESTClientImpl.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class RESTClientImpl {
 
-    private static final Logger log = LoggerFactory.getLogger(TestHarness.class);
+    private static final Logger log = LoggerFactory.getLogger(RESTClientImpl.class);
     
     protected boolean checkBadResponse(int responseStatus, String failureError) {
         if (responseStatus<200 || responseStatus>=300) {
