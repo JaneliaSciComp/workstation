e395a560b29883c580e84427e17663ce9b22baf4
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2016-04-22 14:59:28 -0400

    add modification date to geo ann; filtered ann list sorts by mod date, not creation date
    Former-commit-id: 591b82c003528331a7d6fc6cd9b9b2db352eb818

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/InterestingAnnotation.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/InterestingAnnotation.java
@@ -13,16 +13,19 @@ public class InterestingAnnotation {
     private String noteText;
     private AnnotationGeometry geometry;
     private Date creationDate;
+    private Date modificationDate;
 
-    public InterestingAnnotation(Long annotationID, Long neuronID, Date creationDate, AnnotationGeometry geometry) {
-        new InterestingAnnotation(annotationID, neuronID, creationDate, geometry, "");
+    public InterestingAnnotation(Long annotationID, Long neuronID, Date creationDate,
+        Date modificationDate, AnnotationGeometry geometry) {
+        new InterestingAnnotation(annotationID, neuronID, creationDate, modificationDate, geometry, "");
     }
 
     public InterestingAnnotation(Long annotationID, Long neuronID, Date creationDate,
-         AnnotationGeometry geometry, String noteText) {
+        Date modificationDate, AnnotationGeometry geometry, String noteText) {
         this.annotationID = annotationID;
         this.neuronID = neuronID;
         this.creationDate = creationDate;
+        this.modificationDate = modificationDate;
         this.noteText = noteText;
         this.geometry = geometry;
     }
@@ -42,6 +45,10 @@ public class InterestingAnnotation {
 
     public Date getCreationDate() {
         return creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
     }
 
     public boolean hasNote() {
