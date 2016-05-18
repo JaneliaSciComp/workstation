5a4ab9925ad3bd92dec998fd79ae184bc6e03745
Christopher Bruns <brunsc@janelia.hhmi.org>
2016-05-18 15:20:44 -0400

    Fuss with compartment shader to get specular looking better. At least with dark backgrounds.
    Former-commit-id: ea1764ccb69a021ec9a318025fcbcb20d89b7b72

--- ConsoleApplication/GLViewerTools/src/org/janelia/gltools/material/shader/TransparentEnvelopeFrag.glsl
+++ ConsoleApplication/GLViewerTools/src/org/janelia/gltools/material/shader/TransparentEnvelopeFrag.glsl
@@ -14,10 +14,10 @@ uniform vec3 diffuseColor = vec3(1.0, 0.8, 0.5);
 
 const vec4 lightPosition = vec4(-10, 20, 6, 0);
 const vec3 lightColor = vec3(1, 1, 1);
-const float ambientScale = 0.5; // unshaded component
-const float diffuseScale = 0.3; // shaded component
-const float specularScale = 0.5; // shininess component
-const float specularPower = 20.0;
+const float ambientScale = 0.7; // unshaded component vs diffuse
+// const float diffuseScale = 0.5; // shaded component, = 1 - ambientScale
+const float specularScale = 0.35; // shininess component
+const float specularPower = 15.0;
 vec3 specularColor = lightColor;
 vec3 diffuseColor1 = 
     // vec3(1, 0, 0); // for testing
@@ -38,8 +38,9 @@ void main(void)
   }
   vec3 L = lightPosition.xyz - lightPosition.w * camPos;
   L = normalize(L);
-  vec3 ambient = ambientScale * ambientColor;
-  vec3 diffuse = diffuseScale * max(dot(n, L), 0) * diffuseColor1;
+  vec3 shadedDiffuse = max(dot(n, L), 0) * diffuseColor;
+  vec3 ambientNDiffuseColor = mix(shadedDiffuse, ambientColor, ambientScale);
+
   // specular http://www.opengl-tutorial.org/beginners-tutorials/tutorial-8-basic-shading/
   vec3 eyeDirection = normalize(camPos);
   vec3 specReflect = reflect(L, n);
@@ -48,11 +49,16 @@ void main(void)
   vec3 specular = specularScale * specularIntensity * specularColor;
 
   // Only show edges viewed obliquely, revealing a transparent outline
-  const float angleCosineCutoff = 0.8;
+  const float angleCosineCutoff = 0.9;
   const float maxOpacity = 0.70;
   float straight_on_ness = abs(dot(n, eyeDirection)) / angleCosineCutoff;
   straight_on_ness = clamp(straight_on_ness, 0, 1);
   if (straight_on_ness >= 1.0) discard; // Don't even draw viewed-face-on geometry
   float edginess = maxOpacity * (1.0 - straight_on_ness);
-  fragColor = vec4(diffuse + ambient + specular, edginess);
+  // show specular even over otherwise transparent regions
+  float specularOpacity = specularIntensity * specularScale;
+  float opacity = max(edginess, specularOpacity);
+  if (opacity < 0.001) discard;
+  vec3 combinedColor = mix(ambientNDiffuseColor, specularColor, specularOpacity / (specularOpacity + edginess));
+  fragColor = vec4(combinedColor, opacity);
 }
