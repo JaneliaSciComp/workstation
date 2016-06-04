3e6885066a896ab43339ed7ef3b6af6ebac22d55
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-06-04 16:36:46 -0400

    ensure ontology explorer is open when creating new ontology
    Former-commit-id: 0e759aed901ca2866476e32a34efb121f7908d61

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/nb_action/NewOntologyAction.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/gui/browser/nb_action/NewOntologyAction.java
@@ -10,6 +10,7 @@ import org.janelia.it.jacs.model.domain.ontology.Ontology;
 import org.janelia.it.jacs.shared.utils.StringUtils;
 import org.janelia.it.workstation.gui.browser.api.DomainMgr;
 import org.janelia.it.workstation.gui.browser.api.DomainModel;
+import org.janelia.it.workstation.gui.browser.components.OntologyExplorerTopComponent;
 import org.janelia.it.workstation.gui.framework.session_mgr.SessionMgr;
 import org.janelia.it.workstation.shared.workers.SimpleWorker;
 import org.openide.awt.ActionID;
@@ -48,6 +49,13 @@ public class NewOntologyAction implements ActionListener {
         if (StringUtils.isEmpty(name)) {
             return;
         }
+
+        // Make sure the ontology explorer is visible first
+        OntologyExplorerTopComponent tc = OntologyExplorerTopComponent.getInstance();
+        if (!tc.isOpened()) {
+            tc.open();
+        }
+        tc.requestVisible();
 
         SimpleWorker worker = new SimpleWorker() {
 
