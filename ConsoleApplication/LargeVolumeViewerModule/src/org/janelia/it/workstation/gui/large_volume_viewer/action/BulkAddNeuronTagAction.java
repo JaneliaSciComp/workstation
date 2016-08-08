3c6c303627d09d66d8552f13682ffaeaed6ff1d8
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2016-08-08 15:13:57 -0400

    continued work on bulk add UI; untested
    Former-commit-id: 54a3ecdf6629c92ab277814f8d5eb21a1cee68a6

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/action/BulkAddNeuronTagAction.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/action/BulkAddNeuronTagAction.java
@@ -1,25 +1,31 @@
 package org.janelia.it.workstation.gui.large_volume_viewer.action;
 
+import java.awt.Window;
 import java.awt.event.ActionEvent;
+import java.util.Arrays;
 import java.util.List;
 
-import javax.swing.AbstractAction;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import org.janelia.it.jacs.model.user_data.tiledMicroscope.TmNeuron;
+import org.janelia.it.workstation.gui.large_volume_viewer.ComponentUtil;
 import org.janelia.it.workstation.gui.large_volume_viewer.annotation.AnnotationModel;
 import org.janelia.it.workstation.gui.large_volume_viewer.annotation.NeuronListProvider;
 
 /**
- * Created by olbrisd on 8/5/16.
+ * this action opens a dialog that allows the user to add
+ * a neuron tag to all the neurons that are visible in
+ * the neuron list
  */
 public class BulkAddNeuronTagAction extends AbstractAction {
 
     private AnnotationModel annModel;
     private NeuronListProvider listProvider;
+
+    private JPanel mainPanel;
+    private JList existingList;
+    private JTextField newTagField;
+
 
     public BulkAddNeuronTagAction(AnnotationModel annotationModel, NeuronListProvider listProvider) {
         this.annModel = annotationModel;
@@ -49,14 +55,83 @@ public class BulkAddNeuronTagAction extends AbstractAction {
         );
     }
 
+    private void requestAddTag(String tag) {
+        // dialog: are you sure
+        int ans = JOptionPane.showConfirmDialog(
+                ComponentUtil.getLVVMainWindow(),
+                String.format("You are about to add the tag '%s' to %d neurons. This action cannot be undone! Continue?",
+                        tag, listProvider.getNeuronList().size()),
+                "Add tags?",
+                JOptionPane.OK_CANCEL_OPTION
+                );
+        if (ans == JOptionPane.OK_OPTION) {
+            addTag(tag);
+        }
+    }
+
+    private void addTag(String tag) {
+
+
+        System.out.println("actually adding tag " + tag);
+
+
+        // should I add a bulk add function annModel.addNeuronTags(String tag, List<TmNeuron> neuronList)?
+        for (TmNeuron neuron: listProvider.getNeuronList()) {
+            annModel.addNeuronTag(tag, neuron);
+        }
+    }
+
     private JPanel getInterface() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+
+        JPanel existingPanel = new JPanel();
+        existingPanel.setLayout(new BoxLayout(existingPanel, BoxLayout.LINE_AXIS));
+
+        JButton existingButton = new JButton("Add");
+        existingButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                requestAddTag((String) existingList.getSelectedValue());
+            }
+        });
+        existingPanel.add(existingButton);
+
+        existingPanel.add(new JLabel("existing tag"));
+
+        existingList = new JList();
+        List<TmNeuron> neurons = listProvider.getNeuronList();
+        String[] existingTags = neurons.toArray(new String[neurons.size()]);
+        Arrays.sort(existingTags);
+        existingList.setListData(existingTags);
+        existingPanel.add(existingList);
+
+        existingPanel.add(Box.createHorizontalGlue());
+        mainPanel.add(existingPanel);
 
 
-        panel.add(new JLabel("placeholder"));
+        JPanel newTagPanel = new JPanel();
+        newTagPanel.setLayout(new BoxLayout(newTagPanel, BoxLayout.LINE_AXIS));
 
+        JButton newTagButton = new JButton("Add");
+        newTagButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                requestAddTag(newTagField.getText());
+            }
+        });
+        newTagPanel.add(newTagButton);
+        newTagPanel.add(new JLabel("new tag"));
+        newTagField = new JTextField();
+        newTagPanel.add(newTagField);
 
-        return panel;
+        newTagPanel.add(Box.createHorizontalGlue());
+        mainPanel.add(newTagPanel);
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(existingButton);
+        group.add(newTagButton);
+
+        return mainPanel;
     }
 }
