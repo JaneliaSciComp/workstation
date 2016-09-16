2a387682261cee1370e04e7566f1ed26f2b48ade
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-09-16 19:17:05 -0400

    cleaned up actions
    Former-commit-id: 9a7902f073622282b34f386100f2a7392ccaf988

--- ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/framework/console/nb_action/ToolsMenuAction.java
+++ ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/framework/console/nb_action/ToolsMenuAction.java
@@ -23,7 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ActionID(
-        category = "tools",
+        category = "Tools",
         id = "ToolsMenuAction"
 )
 @ActionRegistration(
