5f9fa924edea2dbd2c453bf09132cd690afc17c1
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-09-28 10:08:05 -0400

    Begin trying to debug failure to render ephemeral vertex actors in Horta.
    Former-commit-id: 95912c461e1f2d3c658b8cac12649e72b3c9489a

--- ConsoleApplication/HortaTracer/src/org/janelia/horta/actors/VertexHighlightActor.java
+++ ConsoleApplication/HortaTracer/src/org/janelia/horta/actors/VertexHighlightActor.java
@@ -1,7 +1,12 @@
 package org.janelia.horta.actors;
 
 import java.io.IOException;
+import javax.media.opengl.GL3;
 import org.janelia.console.viewerapi.model.NeuronModel;
+import org.janelia.geometry3d.AbstractCamera;
+import org.janelia.geometry3d.Matrix4;
+import org.janelia.geometry3d.Object3d;
+import org.janelia.gltools.MeshActor;
 import org.janelia.gltools.texture.Texture2d;
 import org.openide.util.Exceptions;
 
@@ -19,6 +24,30 @@ public class VertexHighlightActor extends SpheresActor {
         material.manageLightProbeTexture = true;
     }
 
+    @Override
+    public void display(GL3 gl, AbstractCamera camera, Matrix4 parentModelViewMatrix) 
+    {
+        // I'm overriding this display() method for debugging.
+        if (! isVisible())
+            return;
+        // 1) Deep check to see whether this actor is actually going to draw
+        int geometrySize = 0;
+        for (Object3d child : getChildren()) {
+            if (child instanceof MeshActor) {
+                MeshActor ma = (MeshActor) child;
+                geometrySize += ma.getGeometry().size();
+            }
+        }
+        if (geometrySize < 1)
+            return;
+        // If we get this far, the actor is probably supposed to draw
+        gl.glEnable(GL3.GL_BLEND);
+        gl.glBlendEquation(GL3.GL_FUNC_ADD);
+        gl.glBlendFunc(GL3.GL_SRC_ALPHA, GL3.GL_ONE_MINUS_SRC_ALPHA);
+        gl.glDisable(GL3.GL_DEPTH_TEST);
+        super.display(gl, camera, parentModelViewMatrix);
+    }
+    
     private static class VertexHighlightTexture extends Texture2d {
         public VertexHighlightTexture() {
             try {
