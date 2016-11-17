4019497b1fbb02aea5f952b7a1577f97d28d62bb
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-11-17 14:47:09 -0500

    disable InterceptingEventQueue
    Former-commit-id: 968330abe9900304a3e991e156ef175aeb96e8f8

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/api/lifecycle/ShowingHook.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/api/lifecycle/ShowingHook.java
@@ -1,21 +1,14 @@
 package org.janelia.it.workstation.browser.api.lifecycle;
 
-import java.awt.AWTEvent;
 import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Toolkit;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import org.janelia.it.workstation.browser.gui.support.WindowLocator;
-import org.janelia.it.workstation.browser.lifecycle.InterceptingEventQueue;
-import org.janelia.it.workstation.browser.lifecycle.LoggingEventListener;
-import org.janelia.it.workstation.browser.lifecycle.MessageSource;
-import org.janelia.it.workstation.browser.lifecycle.ReportRunner;
 import org.janelia.it.workstation.browser.util.ConsoleProperties;
 import org.openide.windows.OnShowing;
 import org.slf4j.Logger;
@@ -38,21 +31,22 @@ public class ShowingHook implements Runnable {
         String title = ConsoleProperties.getString("console.Title") + " " + ConsoleProperties.getString("console.versionNumber");
         frame.setTitle(title);
         
-        // Log events.
-        final InterceptingEventQueue interceptingEventQueue = new InterceptingEventQueue();
-        Toolkit.getDefaultToolkit().getSystemEventQueue().push(
-                interceptingEventQueue);
-        final LoggingEventListener loggingEventListener = new LoggingEventListener();
-        Toolkit.getDefaultToolkit().addAWTEventListener(
-                loggingEventListener, AWTEvent.MOUSE_EVENT_MASK);
-        
-        List<String> discriminators = new ArrayList<>();
-        List<MessageSource> sources = new ArrayList<>();
-        sources.add(interceptingEventQueue);
-        discriminators.add(ReportRunner.MOUSE_EVENT_DISCRIMINATOR);
-        //sources.add(loggingEventListener);
-        //discriminators.add(ReportRunner.BUTTON_EVENT_DISCRIMINATOR);
-        new ReportRunner(sources, discriminators); // This starts a thread
+        // Log events. 
+        // KR: After consulting with Les, I'm disabling this for now as it creates a lot of information we don't really have a use for. 
+//        final InterceptingEventQueue interceptingEventQueue = new InterceptingEventQueue();
+//        Toolkit.getDefaultToolkit().getSystemEventQueue().push(
+//                interceptingEventQueue);
+//        final LoggingEventListener loggingEventListener = new LoggingEventListener();
+//        Toolkit.getDefaultToolkit().addAWTEventListener(
+//                loggingEventListener, AWTEvent.MOUSE_EVENT_MASK);
+//        
+//        List<String> discriminators = new ArrayList<>();
+//        List<MessageSource> sources = new ArrayList<>();
+//        sources.add(interceptingEventQueue);
+//        discriminators.add(ReportRunner.MOUSE_EVENT_DISCRIMINATOR);
+//        //sources.add(loggingEventListener);
+//        //discriminators.add(ReportRunner.BUTTON_EVENT_DISCRIMINATOR);
+//        new ReportRunner(sources, discriminators); // This starts a thread
 
         log.info("Showing main window");
         frame.setVisible(true);
