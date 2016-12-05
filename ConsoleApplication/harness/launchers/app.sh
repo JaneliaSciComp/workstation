49f75a8b8e00af67d135d0be7e3cc1076e9d0d24
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-12-05 10:07:48 -0500

    upgraded platform and harness to the versions from NetBeans 8.2 and then repeated the same changes we had made to 7.4
    Former-commit-id: a9c89622b4ccdb55f65b671b27dbf690191ae4fb

--- ConsoleApplication/harness/launchers/app.sh
+++ ConsoleApplication/harness/launchers/app.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#
+# Copyright 2005, 2016 Oracle and/or its affiliates. All rights reserved.
+#
+# Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+# Other names may be trademarks of their respective owners.
+#
+# The contents of this file are subject to the terms of either the GNU
+# General Public License Version 2 only ("GPL") or the Common
+# Development and Distribution License("CDDL") (collectively, the
+# "License"). You may not use this file except in compliance with the
+# License. You can obtain a copy of the License at
+# http://www.netbeans.org/cddl-gplv2.html
+# or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+# specific language governing permissions and limitations under the
+# License.  When distributing the software, include this License Header
+# Notice in each file and include the License file at
+# nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+# particular file as subject to the "Classpath" exception as provided
+# by Oracle in the GPL Version 2 section of the License file that
+# accompanied this code. If applicable, add the following below the
+# License Header, with the fields enclosed by brackets [] replaced by
+# your own identifying information:
+# "Portions Copyrighted [year] [name of copyright owner]"
+#
+# If you wish your version of this file to be governed by only the CDDL
+# or only the GPL Version 2, indicate your decision by adding
+# "[Contributor] elects to include this software in this distribution
+# under the [CDDL or GPL Version 2] license." If you do not indicate a
+# single choice of license, a recipient has the option to distribute
+# your version of this file under either the CDDL, the GPL Version 2 or
+# to extend the choice of license to its licensees as provided above.
+# However, if you add GPL Version 2 code and therefore, elected the GPL
+# Version 2 license, then the option applies only if the new code is
+# made subject to such option by the copyright holder.
+#
+# Contributor(s):
+
 #
 # resolve symlinks
 #
@@ -50,7 +88,8 @@ fi
 
 if [ -n "$jdkhome" -a \! -d "$jdkhome" -a -d "$progdir/../$jdkhome" ]; then
     # #74333: permit jdkhome to be defined as relative to app dir
-    jdkhome="$progdir/../$jdkhome"
+    # jdkhome="$progdir/../$jdkhome"
+    --jdkhome '"`/usr/libexec/java_home -v 1.8`"' \
 fi
 
 readClusters() {
@@ -87,8 +126,7 @@ nbexec=`echo "$progdir"/../platform*/lib/nbexec`
 case "`uname`" in
     Darwin*)
         eval exec sh '"$nbexec"' \
-            --jdkhome '"`/usr/libexec/java_home -v 1.7`"' \
-            -J-Dcom.apple.mrj.application.apple.menu.about.name='"$APPNAME"' \
+            --jdkhome '"$jdkhome"' \
             -J-Xdock:name='"$APPNAME"' \
             '"-J-Xdock:icon=$progdir/../$APPNAME.icns"' \
             --clusters '"$clusters"' \
