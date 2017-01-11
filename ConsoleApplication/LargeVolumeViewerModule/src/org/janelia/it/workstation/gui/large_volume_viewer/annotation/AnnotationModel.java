a03313c91fde4a13250a2f02fa26828655524494
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2017-01-11 11:13:51 -0500

    fixed bug: an update after move annotation wasn't being run in the EDT
    Former-commit-id: f1702b8d40d10cd823581da9e5b552223f8cef3e

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/AnnotationModel.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/AnnotationModel.java
@@ -680,8 +680,14 @@ called from a  SimpleWorker thread.
             // Update value in database.
             synchronized(neuron) {
                 neuronManager.saveNeuronData(neuron);
-                fireAnnotationMoved(annotationID);
             }
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    fireAnnotationMoved(annotationID);
+                    activityLog.logEndOfOperation(getWsId(), location);
+                }
+            });
         } catch (Exception e) {
             // error means not persisted; however, in the process of moving,
             //  the marker's already been moved, to give interactive feedback
