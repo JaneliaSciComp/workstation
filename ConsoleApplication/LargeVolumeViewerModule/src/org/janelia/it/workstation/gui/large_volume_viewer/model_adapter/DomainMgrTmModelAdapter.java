c097765992e0aedb8418da7214f6249d47e8b067
Konrad Rokicki <rokickik@janelia.hhmi.org>
2017-01-28 09:42:56 -0500

    removed unnecessary progress handle
    Former-commit-id: 7a58625ffc97c0c3ef3b9d3a6b0c43cd38df72f1

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/model_adapter/DomainMgrTmModelAdapter.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/model_adapter/DomainMgrTmModelAdapter.java
@@ -37,33 +37,25 @@ public class DomainMgrTmModelAdapter implements TmModelAdapter {
     private static Logger log = LoggerFactory.getLogger(DomainMgrTmModelAdapter.class);
 
     private RequestProcessor loadProcessor = new RequestProcessor("Tm-Save-Queue", 1, true);
- 
     private TiledMicroscopeDomainMgr tmDomainMgr = TiledMicroscopeDomainMgr.getDomainMgr();
 
     @Override
     public List<TmNeuronMetadata> loadNeurons(TmWorkspace workspace) throws Exception {
         log.info("Checking neurons for workspace: "+workspace);
         List<TmNeuronMetadata> neurons = new ArrayList<>();
-        final ProgressHandle progress = ProgressHandleFactory.createHandle("Loading neuron data");
 
-        try {
-            progress.setInitialDelay(0);
-            progress.start();
-            progress.switchToIndeterminate();
-            
+        try {            
             StopWatch stopWatch = new StopWatch();
             List<TmNeuronMetadata> neuronList = tmDomainMgr.getWorkspaceNeurons(workspace.getId());
             log.info("Loading {} neurons took {} ms", neuronList.size(), stopWatch.getElapsedTime());
             
             if (ClientDomainUtils.hasWriteAccess(workspace)) {
                 if (ApplicationPanel.isVerifyNeurons()) {
-                    progress.setDisplayName("Verifying neuron data");
                         
                     // check neuron consistency and repair (some) problems
                     for (TmNeuronMetadata neuron: neuronList) {
                         log.debug("Checking neuron data for TmNeuronMetadata#{}", neuron.getId());
                         List<String> results = neuron.checkRepairNeuron();
-                        // List<String> results = neuron.checkNeuron();
                         if (results.size() > 0) {
                             // save results, then output to log; this is unfortunately
                             //  not visible to the user; we aren't in a place in the
@@ -87,9 +79,6 @@ public class DomainMgrTmModelAdapter implements TmModelAdapter {
         }
         catch (Exception ex) {
             throw ex;
-        }
-        finally {
-            progress.finish();
         }
 
         return neurons;
