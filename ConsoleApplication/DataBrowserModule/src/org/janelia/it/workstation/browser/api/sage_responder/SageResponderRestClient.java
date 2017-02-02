60d5248cc958c5d19698f9acf5ebfd2848c490c0
Konrad Rokicki <rokickik@janelia.hhmi.org>
2017-02-02 16:20:50 -0500

    JW-27798: allow search of shared annotations
    Former-commit-id: a306488564c29547f5d3fc5b26e88ff5986d4b17

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/api/sage_responder/SageResponderRestClient.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/api/sage_responder/SageResponderRestClient.java
@@ -1,7 +1,6 @@
 package org.janelia.it.workstation.browser.api.sage_responder;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
