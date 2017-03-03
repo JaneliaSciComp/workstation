f7bd2d717c949c8e24ffd457f4fd4e855ab8a817
Cristian Goina <goinac@janelia.hhmi.org>
2017-03-02 23:37:48 -0500

    trying workstealing pool
    Former-commit-id: 99a2b03237f2a837e779ef1493c41afc1dfd8863

--- jacs2-services/src/main/java/org/janelia/jacs2/cdi/ExecutorProducer.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/cdi/ExecutorProducer.java
@@ -39,7 +39,7 @@ public class ExecutorProducer {
                 .setNameFormat("JACS-%d")
                 .setDaemon(true)
                 .build();
-        return Executors.newFixedThreadPool(threadPoolSize, threadFactory);
+        return Executors.newWorkStealingPool();
     }
 
     @SuspendedTaskExecutor
