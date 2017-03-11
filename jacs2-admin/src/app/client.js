e6cadd2560134dacd662bf7e06a2fe12a9faa467
porterbot <schauderd@janelia.hhmi.org>
2017-03-10 22:57:11 -0500

    got basic Services Registry displaying and finish setup of react-redux
    Former-commit-id: 0bb53dad48df4c1e753782d332edee370397e25a

--- jacs2-admin/src/app/client.js
+++ jacs2-admin/src/app/client.js
@@ -5,15 +5,15 @@ import ReactDOM from 'react-dom';
 import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { fetchServices } from './actions'
-import { rootReducer } from './reducers'
+import { servicesRegistry } from './reducers'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 const store = createStore(
 	combineReducers({
-		rootReducer,
+		servicesRegistry,
 	    routing: routerReducer
 	}),
+    {servicesRegistry: {services: []}},
     applyMiddleware(
        thunkMiddleware // lets us dispatch() functions
     )
@@ -22,26 +22,31 @@ const store = createStore(
 // view
 import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router';
 import App from 'grommet/components/App';
+import Menu from 'grommet/components/Menu';
+import Anchor from 'grommet/components/Anchor';
 import Split from 'grommet/components/Split';
 import Sidebar from 'grommet/components/Sidebar';
 import Section from 'grommet/components/Section';
+
+import { fetchServices } from './actions'
+import ServiceTable from './ServiceTable';
 import 'grommet/scss/vanilla/index';
 
 var Main = React.createClass({
     render: function() {
         return (
             <App centered={true}>
-                <Split fixed={true}>
-                    <Sidebar>
-                        <ul>
-                            <li><Link to="/Services">Services Queue</Link></li>
-                            <li><Link to="/Pipes">Services Performance</Link></li>
-                            <li><Link to="/servicesDetail">Services Detail</Link></li>
-                            <li><Link to="/pipesQueue">Pipes Queue</Link></li>
-                            <li><Link to="/pipesDetail">Pipes Detail</Link></li>
-                            <li><Link to="/serviceRegistry">Service Registry</Link></li>
-                            <li><Link to="/servicesAPI">Service API Detail</Link></li>
-                        </ul>
+                <Split flex={"right"}>
+                    <Sidebar colorIndex="neutral-1">
+                        <Menu fill={true} primary={true}>
+                            <Anchor key="Services Queue" path="/services" label="Services Queue" />
+                            <Anchor key="Services Performance" path="/services" label="Services Performance" />
+                            <Anchor key="Services Detail" path="/services" label="Services Detail" />
+                            <Anchor key="Pipes Queue" path="/pipes" label="Pipes Queue" />
+                            <Anchor key="Pipes Detail" path="/pipes" label="Pipes Detail" />
+                            <Anchor key="Service Registry" path="/pipes" label="Service Registry" />
+                            <Anchor key="Service API Detail" path="/services" label="Service API Detail" />
+                        </Menu>
                     </Sidebar>
                     <Section>
                         {this.props.children}
@@ -52,42 +57,18 @@ var Main = React.createClass({
     }
 });
 
-var Services = React.createClass({
-    render: function() {
-        return (
-            <div>
-               This is a services test
-            </div>
-        );
-    }
-});
-
-var Pipes = React.createClass({
-    render: function() {
-        return (
-            <div>
-               This is a pipes test
-            </div>
-        );
-    }
-});
-
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
 <Provider store={store}>
     <Router history={history}>
         <Route path="/" component={Main}>
-            <IndexRoute component={Services}/>
-            <Route path="services" component={Services}/>
-            <Route path="pipes" component={Pipes} />
+            <IndexRoute component={ServiceTable}/>
+            <Route path="services" component={ServiceTable}/>
+            <Route path="pipes" component={ServiceTable} />
         </Route>
     </Router>
 </Provider>,
 document.getElementById('app')
 
 );
-
-store.dispatch(fetchServices('localhost:8080')).then(() =>
-  console.log(store.getState())
-)
