89bf3ce72a25324c4eecb48b851d48a334a3bc89
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2017-04-05 08:47:00 -0400

    added stub for next algorithm
    Former-commit-id: b6aea5ff303fbb51ee1ee1ba633e72eabf697fa6

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/SmartMergeAlgorithms.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/SmartMergeAlgorithms.java
@@ -12,7 +12,7 @@ public class SmartMergeAlgorithms {
     //  two neurites, given an annotation on each neurite
 
     public static List<TmGeoAnnotation> mergeChosenPoints(TmGeoAnnotation selectedAnnotation,
-      TmGeoAnnotation targetAnnotation) {
+                                                          TmGeoAnnotation targetAnnotation) {
 
         // for testing; merge using the two annotations that are input
         List<TmGeoAnnotation> result = new ArrayList<>();
@@ -22,7 +22,7 @@ public class SmartMergeAlgorithms {
     }
 
     public static List<TmGeoAnnotation> mergeClosestEndpoints(TmGeoAnnotation selectedAnnotation,
-        TmGeoAnnotation targetAnnotation) {
+                                                              TmGeoAnnotation targetAnnotation) {
 
 
         List<TmGeoAnnotation> result = new ArrayList<>();
@@ -33,14 +33,10 @@ public class SmartMergeAlgorithms {
         result.add(targetAnnotation);
 
 
-
         return result;
 
 
-
-
     }
-
 
 
     // -------------------------------------------------------
@@ -50,7 +46,25 @@ public class SmartMergeAlgorithms {
     // default: return target annotation, which is the
     public static TmGeoAnnotation nextParentTarget(TmGeoAnnotation sourceAnnotation,
         TmGeoAnnotation targetAnnotation) {
-         return targetAnnotation;
+        return targetAnnotation;
     }
 
+    public static TmGeoAnnotation plausibleNextParent(TmGeoAnnotation sourceAnnotation,
+        TmGeoAnnotation targetAnnotation) {
+
+        // remember that we do this calculation before the merge, so the parenting
+        //  may not match what you expect!
+
+        // if the source neurite is straight, find the far end from the source annotation
+        //  (which might not be at the near end, remember, but we expect it'll be close)
+
+
+        // if source neurite is branched, ugh; need to move from the source annotation
+        //  "away" from the target until you hit a branch
+
+
+
+
+        return targetAnnotation;
+    }
 }
