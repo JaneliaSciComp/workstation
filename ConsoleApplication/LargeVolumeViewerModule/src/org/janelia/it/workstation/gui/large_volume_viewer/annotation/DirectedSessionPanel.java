529cffe5b5c2f715fbb02410e67204746cd4b50e
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2017-04-26 09:09:17 -0400

    added basic content to directed panel
    Former-commit-id: a075acff32e5fd2579256a571c4c9e5b1bbf7911

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/DirectedSessionPanel.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/annotation/DirectedSessionPanel.java
@@ -6,7 +6,6 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 
 import org.janelia.it.workstation.gui.large_volume_viewer.controller.ViewStateListener;
@@ -17,12 +16,16 @@ import org.janelia.it.workstation.gui.large_volume_viewer.controller.ViewStateLi
  * usual manual tracing workflow
  */
 public class DirectedSessionPanel extends AnnotationPanel {
+    // as in BasicAnnPanel, we have a constant height unit so
+    //  if we have subcomponents, we can tell each one how tall
+    //  to be in multiples of the standard height
     public static final int SUBPANEL_STD_HEIGHT = 150;
 
     private AnnotationManager annotationMgr;
     private LargeVolumeViewerTranslator lvvTranslator;
 
     private ViewStateListener viewStateListener;
+
 
     // major UI panels
 
@@ -31,13 +34,13 @@ public class DirectedSessionPanel extends AnnotationPanel {
     private static final int width = 250;
 
 
+
+
     public DirectedSessionPanel(AnnotationManager annotationMgr, LargeVolumeViewerTranslator lvvTranslator) {
         this.annotationMgr = annotationMgr;
         this.lvvTranslator = lvvTranslator;
 
         setupUI();
-
-
 
     }
 
@@ -53,6 +56,7 @@ public class DirectedSessionPanel extends AnnotationPanel {
 
     private void setupUI() {
 
+        // pack up your troubles in your old grid bag and smile, smile, smile...
         setLayout(new GridBagLayout());
 
         GridBagConstraints cTop = new GridBagConstraints();
@@ -77,7 +81,52 @@ public class DirectedSessionPanel extends AnnotationPanel {
         cVert.fill = GridBagConstraints.HORIZONTAL;
         cVert.weighty = 0.0;
 
+        // ---------- information section
+        // in basic tracing, this section is partially handled by
+        //  WorkspaceInfoPanel; we'll cram it all in here and
+        //  separate it out later if it makes sense
 
+        // spacer, then some info labels:
+        add(Box.createRigidArea(new Dimension(0, 10)), cVert);
+
+        add(new JLabel("Name: " + annotationMgr.getInitialObject().getName(), JLabel.LEADING), cVert);
+        add(new JLabel("Sample: " + annotationMgr.getCurrentSample().getName(), JLabel.LEADING), cVert);
+
+        // session menu; I use the gear icon in other place, so I should keep
+        //  using it; that being said, I don't like how it looks alone, without
+        //  + and or - buttons near it (which we don't have here)
+
+        // could just put in a couple buttons instead of a menu, if you
+        //  think there will only be a few of them?
+
+
+
+
+
+
+        // big spacer, just to separate stuff while developing
+        add(Box.createRigidArea(new Dimension(0, 150)), cVert);
+
+
+
+        // ---------- decision handling section
+
+        add(new JLabel("decision controls here!", JLabel.CENTER), cVert);
+
+
+        // this adds the button, with right name, but it's grayed out
+        //  (and ctrl-N still works); tried multiple variations of
+        //  instantiating button and action as class variables
+        // add(new JButton(new NextDecisionAction()), cVert);
+
+
+
+
+        add(Box.createRigidArea(new Dimension(0, 40)), cVert);
+
+
+
+        add(new JLabel("Progress: xxx/yyy (zzz%)", JLabel.CENTER), cVert);
 
 
 
