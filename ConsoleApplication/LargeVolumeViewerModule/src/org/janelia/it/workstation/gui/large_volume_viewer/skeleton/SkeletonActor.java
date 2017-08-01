7af56db9fe4ee48cf721981ecf8ba2516f3bb24a
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2017-08-01 13:50:13 -0400

    fixed bug: when no lines were to be drawn (only points and possibly traced paths), updateVertices() was not called, eventually resulting in OpenGL error and crash
    Former-commit-id: 061a8ecb41b76f0e55291b520740df6c66d066f7

--- ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/skeleton/SkeletonActor.java
+++ ConsoleApplication/LargeVolumeViewerModule/src/org/janelia/it/workstation/gui/large_volume_viewer/skeleton/SkeletonActor.java
@@ -266,9 +266,11 @@ public class SkeletonActor implements GLActor {
     }
     
     private synchronized void displayLines(GLAutoDrawable glDrawable) {
-        if (model.getNeuronLineIndices().isEmpty()) {
-            return;
-        }
+
+        // at one point, this routine tested model.getNeuronLineIndices().isEmpty() and
+        //  returned immediately if true; this is a bug--it prevents model.updateVertices()
+        //  from running when it needs to; instead, in the loop, skip past the iterations
+        //  that have no elements to draw
 
         GL2GL3 gl = glDrawable.getGL().getGL2GL3();
         GL2 gl2 = gl.getGL2();
@@ -309,6 +311,11 @@ public class SkeletonActor implements GLActor {
 
         n=0;
         for (ElementDataOffset lineElementOffset : lineOffsets) {
+            // see comment at top of method
+            if (lineElementOffset.size == 0) {
+                continue;
+            }
+
             ElementDataOffset vertexElementOffset = vertexOffsets.get(n);
             ElementDataOffset colorElementOffset = colorOffsets.get(n);
 
