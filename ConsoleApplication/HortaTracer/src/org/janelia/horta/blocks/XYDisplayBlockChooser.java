b6b554ec5cde5a44796a6a556124c18cc228828c
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2017-09-06 10:07:29 -0400

    started work on x-y Horta block loader
    Former-commit-id: 0805d0d2d86a1b363b3f5cb74ea901ac3a3c1657

---
+++ ConsoleApplication/HortaTracer/src/org/janelia/horta/blocks/XYDisplayBlockChooser.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed under the Janelia Farm Research Campus Software Copyright 1.1
+ * 
+ * Copyright (c) 2014, Howard Hughes Medical Institute, All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     1. Redistributions of source code must retain the above copyright notice, 
+ *        this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright 
+ *        notice, this list of conditions and the following disclaimer in the 
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the Howard Hughes Medical Institute nor the names 
+ *        of its contributors may be used to endorse or promote products derived 
+ *        from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, ANY 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * REASONABLE ROYALTIES; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.janelia.horta.blocks;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.janelia.geometry3d.ConstVector3;
+import org.janelia.geometry3d.Vector3;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * generate a list of blocks in same xy plane as focus, but wider than usual;
+ * this is for testing
+ */
+public class XYDisplayBlockChooser 
+implements BlockChooser
+{
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @Override
+    public List<BlockTileKey> chooseBlocks(BlockTileSource source, ConstVector3 focus, ConstVector3 previousFocus) 
+    {
+        BlockTileResolution resolution = source.getMaximumResolution();
+        
+        ConstVector3 blockSize = ((KtxOctreeBlockTileSource)source).getBlockSize(resolution);
+
+        // this gives you a (2nx + 1) by (2ny + 1) square
+        
+        int nx = 1;
+        int ny = 1;
+        
+        // float [] dxa = {0f, -blockSize.getX(), +blockSize.getX()};
+        // float [] dya = {0f, -blockSize.getY(), +blockSize.getY()};
+        
+        
+        
+        // this isn't working...
+        float [] xarray = new float[2 * nx + 1];
+        float [] yarray = new float[2 * ny + 1];
+        
+        for (int i=0; i<2 * nx + 1; i++) {
+            xarray[i] = (i - nx) * blockSize.getX();
+        }
+        for (int j=0; j<2 * ny + 1; j++) {
+            xarray[j] = (j - ny) * blockSize.getY();
+        }
+        
+
+        List<BlockTileKey> result = new ArrayList<>();
+        for (float dx: xarray) {
+            for (float dy: yarray) {
+                ConstVector3 location = focus.plus(new Vector3(dx, dy, 0.0f));
+                BlockTileKey tileKey = source.getBlockKeyAt(location, resolution);
+                if (tileKey != null) {
+                    result.add(tileKey);
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+}
