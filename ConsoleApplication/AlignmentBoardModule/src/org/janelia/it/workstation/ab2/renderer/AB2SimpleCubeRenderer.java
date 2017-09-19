543f9a48908fd28edb41e319b20359aba0009cd8
seandmurphy <seandmurphy@yahoo.com>
2017-09-19 09:37:35 -0400

    Version with interleaved points.
    Former-commit-id: a451244f27732264d9a4833c12cfd141bf6cadb3

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/renderer/AB2SimpleCubeRenderer.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/renderer/AB2SimpleCubeRenderer.java
@@ -21,9 +21,13 @@ public class AB2SimpleCubeRenderer extends AB2Basic3DRenderer {
     IntBuffer vertexArrayId=IntBuffer.allocate(1);
     IntBuffer vertexBufferId=IntBuffer.allocate(1);
     IntBuffer colorBufferId=IntBuffer.allocate(1);
+    IntBuffer interleavedBufferId=IntBuffer.allocate(1);
 
     FloatBuffer cubeFb;
     FloatBuffer colorFb;
+    FloatBuffer interleavedFb;
+
+    boolean useInterleaved=true;
 
 
     public AB2SimpleCubeRenderer() {
@@ -37,6 +41,16 @@ public class AB2SimpleCubeRenderer extends AB2Basic3DRenderer {
         logger.info("init() called");
 
         //                   X        Y        Z        R        G        B
+
+        float[] interleavedData = { -0.25f,  -0.25f,   0.25f, 0f, 0f, 1f,
+                                    -0.25f,   0.25f,   0.25f, 1f, 0f, 0f,
+                                     0.25f,   0.25f,   0.25f, 0f, 0f, 1f,
+                                     0.25f,  -0.25f,   0.25f, 1f, 0f, 0f,
+                                    -0.10f,  -0.10f,   0.75f, 0f, 1f, 0f,
+                                    -0.10f,   0.10f,   0.75f, 0f, 1f, 0f,
+                                     0.10f,   0.10f,   0.75f, 0f, 1f, 0f,
+                                     0.10f,  -0.10f,   0.75f, 0f, 1f, 0f };
+
 
         float[] cubeData = { -0.25f,  -0.25f,   0.25f,
                              -0.25f,   0.25f,   0.25f,
@@ -58,34 +72,58 @@ public class AB2SimpleCubeRenderer extends AB2Basic3DRenderer {
 
         cubeFb=createGLFloatBuffer(cubeData);
         colorFb=createGLFloatBuffer(colorData);
+        interleavedFb=createGLFloatBuffer(interleavedData);
 
-        gl.glGenVertexArrays(1, vertexArrayId);
-        checkGlError(gl, "i1 glGenVertexArrays error");
+        if (useInterleaved) {
 
-        gl.glBindVertexArray(vertexArrayId.get(0));
-        checkGlError(gl, "i2 glBindVertexArray error");
+            gl.glGenVertexArrays(1, vertexArrayId);
+            checkGlError(gl, "i1 glGenVertexArrays error");
 
-        gl.glGenBuffers(1, vertexBufferId);
-        checkGlError(gl, "i3 glGenBuffers() error");
+            gl.glBindVertexArray(vertexArrayId.get(0));
+            checkGlError(gl, "i2 glBindVertexArray error");
 
-        gl.glGenBuffers(1, colorBufferId);
-        checkGlError(gl, "i3 glGenBuffers() error");
+            gl.glGenBuffers(1, interleavedBufferId);
+            checkGlError(gl, "i3 glGenBuffers() error");
 
-        gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, vertexBufferId.get(0));
-        checkGlError(gl, "i4 glBindBuffer error");
+            gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, interleavedBufferId.get(0));
+            checkGlError(gl, "i4 glBindBuffer error");
 
-        gl.glBufferData(GL4.GL_ARRAY_BUFFER, cubeFb.capacity()*4, cubeFb, GL4.GL_STATIC_DRAW);
-        checkGlError(gl, "i5 glBufferData error");
+            gl.glBufferData(GL4.GL_ARRAY_BUFFER, interleavedFb.capacity() * 4, interleavedFb, GL4.GL_STATIC_DRAW);
+            checkGlError(gl, "i5 glBufferData error");
 
-        gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+            gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
 
-        gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, colorBufferId.get(0));
-        checkGlError(gl, "i4 glBindBuffer error");
+        } else {
 
-        gl.glBufferData(GL4.GL_ARRAY_BUFFER, colorFb.capacity()*4, colorFb, GL4.GL_STATIC_DRAW);
-        checkGlError(gl, "i5 glBufferData error");
+            gl.glGenVertexArrays(1, vertexArrayId);
+            checkGlError(gl, "i1 glGenVertexArrays error");
 
-        gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+            gl.glBindVertexArray(vertexArrayId.get(0));
+            checkGlError(gl, "i2 glBindVertexArray error");
+
+            gl.glGenBuffers(1, vertexBufferId);
+            checkGlError(gl, "i3 glGenBuffers() error");
+
+            gl.glGenBuffers(1, colorBufferId);
+            checkGlError(gl, "i3 glGenBuffers() error");
+
+            gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, vertexBufferId.get(0));
+            checkGlError(gl, "i4 glBindBuffer error");
+
+            gl.glBufferData(GL4.GL_ARRAY_BUFFER, cubeFb.capacity() * 4, cubeFb, GL4.GL_STATIC_DRAW);
+            checkGlError(gl, "i5 glBufferData error");
+
+            gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+
+            gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, colorBufferId.get(0));
+            checkGlError(gl, "i4 glBindBuffer error");
+
+            gl.glBufferData(GL4.GL_ARRAY_BUFFER, colorFb.capacity() * 4, colorFb, GL4.GL_STATIC_DRAW);
+            checkGlError(gl, "i5 glBufferData error");
+
+            gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+
+        }
 
         logger.info("init() finished");
 
@@ -99,38 +137,74 @@ public class AB2SimpleCubeRenderer extends AB2Basic3DRenderer {
 
                 //logger.info("update() start");
 
-                AB2SimpleCubeShader cubeShader=(AB2SimpleCubeShader)shader;
-                cubeShader.setMVP(gl, mvp);
-                gl.glPointSize(3.0f);
+                if (useInterleaved) {
 
-                gl.glBindVertexArray(vertexArrayId.get(0));
-                checkGlError(gl, "d1 ArraySortShader glBindVertexArray() error");
+                    AB2SimpleCubeShader cubeShader = (AB2SimpleCubeShader) shader;
+                    cubeShader.setMVP(gl, mvp);
+                    gl.glPointSize(3.0f);
 
-                gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, vertexBufferId.get(0));
-                checkGlError(gl, "d2 ArraySortShader glBindBuffer error");
+                    gl.glBindVertexArray(vertexArrayId.get(0));
+                    checkGlError(gl, "d1 ArraySortShader glBindVertexArray() error");
 
-                gl.glVertexAttribPointer(0, 3, GL4.GL_FLOAT, false, 0, 0);
-                checkGlError(gl, "d3 ArraySortShader glVertexAttribPointer 0 () error");
+                    gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, interleavedBufferId.get(0));
+                    checkGlError(gl, "d2 ArraySortShader glBindBuffer error");
 
-                gl.glEnableVertexAttribArray(0);
-                checkGlError(gl, "d4 ArraySortShader glEnableVertexAttribArray 0 () error");
+                    gl.glVertexAttribPointer(0, 3, GL4.GL_FLOAT, false, 24, 0);
+                    checkGlError(gl, "d3 ArraySortShader glVertexAttribPointer 0 () error");
 
-                gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, colorBufferId.get(0));
-                checkGlError(gl, "d2 ArraySortShader glBindBuffer error");
+                    gl.glEnableVertexAttribArray(0);
+                    checkGlError(gl, "d4 ArraySortShader glEnableVertexAttribArray 0 () error");
 
-                gl.glVertexAttribPointer(1, 3, GL4.GL_FLOAT, false, 0, 0);
-                checkGlError(gl, "d3 ArraySortShader glVertexAttribPointer 0 () error");
+                    gl.glVertexAttribPointer(1, 3, GL4.GL_FLOAT, false, 24, 12);
+                    checkGlError(gl, "d3 ArraySortShader glVertexAttribPointer 0 () error");
 
-                gl.glEnableVertexAttribArray(1);
-                checkGlError(gl, "d4 ArraySortShader glEnableVertexAttribArray 0 () error");
+                    gl.glEnableVertexAttribArray(1);
+                    checkGlError(gl, "d4 ArraySortShader glEnableVertexAttribArray 0 () error");
 
-                gl.glDrawArrays(GL4.GL_POINTS, 0, 8);
-                checkGlError(gl, "d7 ArraySortShader glDrawArrays() error");
+                    gl.glDrawArrays(GL4.GL_POINTS, 0, 8);
+                    checkGlError(gl, "d7 ArraySortShader glDrawArrays() error");
 
-                //gl.glDrawArrays(GL4.GL_LINES, 0, 4);
-                //checkGlError(gl, "d8 ArraySortShader glDrawArrays() error");
+                    //gl.glDrawArrays(GL4.GL_LINES, 0, 4);
+                    //checkGlError(gl, "d8 ArraySortShader glDrawArrays() error");
 
-                gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+                    gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+
+                } else {
+
+                    AB2SimpleCubeShader cubeShader = (AB2SimpleCubeShader) shader;
+                    cubeShader.setMVP(gl, mvp);
+                    gl.glPointSize(3.0f);
+
+                    gl.glBindVertexArray(vertexArrayId.get(0));
+                    checkGlError(gl, "d1 ArraySortShader glBindVertexArray() error");
+
+                    gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, vertexBufferId.get(0));
+                    checkGlError(gl, "d2 ArraySortShader glBindBuffer error");
+
+                    gl.glVertexAttribPointer(0, 3, GL4.GL_FLOAT, false, 0, 0);
+                    checkGlError(gl, "d3 ArraySortShader glVertexAttribPointer 0 () error");
+
+                    gl.glEnableVertexAttribArray(0);
+                    checkGlError(gl, "d4 ArraySortShader glEnableVertexAttribArray 0 () error");
+
+                    gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, colorBufferId.get(0));
+                    checkGlError(gl, "d2 ArraySortShader glBindBuffer error");
+
+                    gl.glVertexAttribPointer(1, 3, GL4.GL_FLOAT, false, 0, 0);
+                    checkGlError(gl, "d3 ArraySortShader glVertexAttribPointer 0 () error");
+
+                    gl.glEnableVertexAttribArray(1);
+                    checkGlError(gl, "d4 ArraySortShader glEnableVertexAttribArray 0 () error");
+
+                    gl.glDrawArrays(GL4.GL_POINTS, 0, 8);
+                    checkGlError(gl, "d7 ArraySortShader glDrawArrays() error");
+
+                    //gl.glDrawArrays(GL4.GL_LINES, 0, 4);
+                    //checkGlError(gl, "d8 ArraySortShader glDrawArrays() error");
+
+                    gl.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
+
+                }
 
                 //logger.info("update() finish");
             }
