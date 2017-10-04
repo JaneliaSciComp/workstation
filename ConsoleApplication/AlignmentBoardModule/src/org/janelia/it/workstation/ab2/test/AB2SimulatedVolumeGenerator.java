8223a29057fe9cf572f178042969a8f889960284
seandmurphy <seandmurphy@yahoo.com>
2017-10-03 22:24:33 -0400

    Adding volume simulation.
    Former-commit-id: d67f18f85e2638e85792f1f5813671c58b3bce83

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/test/AB2SimulatedVolumeGenerator.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/test/AB2SimulatedVolumeGenerator.java
@@ -1,7 +1,50 @@
 package org.janelia.it.workstation.ab2.test;
 
+import org.janelia.geometry3d.Vector4;
+import org.janelia.it.workstation.ab2.model.AB2Image3D_R32I;
+import org.janelia.it.workstation.ab2.model.AB2Image3D_RGBA8UI;
+import org.janelia.it.workstation.ab2.model.AB2NeuronSkeleton;
+
 public class AB2SimulatedVolumeGenerator {
 
+    AB2Image3D_RGBA8UI rawImage;
+    AB2Image3D_R32I indexImage;
 
+    int xDim;
+    int yDim;
+    int zDim;
+
+    public AB2SimulatedVolumeGenerator(int xDim, int yDim, int zDim) {
+        this.xDim=xDim;
+        this.yDim=yDim;
+        this.zDim=zDim;
+        rawImage=new AB2Image3D_RGBA8UI(xDim, yDim, zDim);
+        indexImage=new AB2Image3D_R32I(xDim, yDim, zDim);
+    }
+
+    // Returns assigned index of skeleton
+    public int addSkeleton(AB2NeuronSkeleton skeleton) {
+        return 0;
+    }
+
+    public int addSkeleton(AB2NeuronSkeleton skeleton, Vector4 color) {
+        return 0;
+    }
+
+    public AB2Image3D_RGBA8UI getRawImage() {
+        return rawImage;
+    }
+
+    public AB2Image3D_R32I getIndexImage() {
+        return indexImage;
+    }
+
+    public void addUniformNoise(double probability, double amplitude) {
+
+    }
+
+    public void performDilation(double sensitivity, double noise) {
+
+    }
 
 }
