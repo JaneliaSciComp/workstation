e941b8d46cbeb4d7004f5d8e0cad06e2aa197ddc
seandmurphy <seandmurphy@yahoo.com>
2017-10-04 16:40:52 -0400

    Creating volume simulator.
    Former-commit-id: fc525f43bc3c5945a63adb7463062e0b9691b1df

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/test/AB2SimulatedVolumeGenerator.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/test/AB2SimulatedVolumeGenerator.java
@@ -1,6 +1,14 @@
 package org.janelia.it.workstation.ab2.test;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.janelia.geometry3d.Vector3;
 import org.janelia.geometry3d.Vector4;
+import org.janelia.it.workstation.ab2.compute.AB2SkeletonWalker;
+import org.janelia.it.workstation.ab2.compute.AB2SkeletonWalkerCallback;
 import org.janelia.it.workstation.ab2.model.AB2Image3D_R32I;
 import org.janelia.it.workstation.ab2.model.AB2Image3D_RGBA8UI;
 import org.janelia.it.workstation.ab2.model.AB2NeuronSkeleton;
@@ -9,6 +17,7 @@ public class AB2SimulatedVolumeGenerator {
 
     AB2Image3D_RGBA8UI rawImage;
     AB2Image3D_R32I indexImage;
+    Random random=new Random(new Date().getTime());
 
     int xDim;
     int yDim;
@@ -22,14 +31,57 @@ public class AB2SimulatedVolumeGenerator {
         indexImage=new AB2Image3D_R32I(xDim, yDim, zDim);
     }
 
-    // Returns assigned index of skeleton
+    int labelCount=0;
+    Map<Integer, Vector4> colorMap=new HashMap<>();
+
+    /* Returns assigned index of skeleton
+
+    The idea here is to trace the skeleton at a high resolution and add all participating voxels.
+
+     */
+
     public int addSkeleton(AB2NeuronSkeleton skeleton) {
-        return 0;
+        Vector3 rc3=new Vector3(random.nextFloat(), random.nextFloat(), random.nextFloat());
+        rc3.normalize();
+        Vector4 rc4=new Vector4(rc3.getX(), rc3.getY(), rc3.getZ(), 1.0f);
+        addSkeleton(skeleton, rc4);
     }
 
-    public int addSkeleton(AB2NeuronSkeleton skeleton, Vector4 color) {
-        return 0;
+    public int addSkeleton(AB2NeuronSkeleton skeleton, final Vector4 color) {
+        labelCount++;
+        colorMap.put(labelCount, color);
+        int maxDim = getMaxDim();
+        double dimUnit = 1.0 / (1.0 * maxDim);
+        double sampleResolution = dimUnit / 5.0;
+        AB2SkeletonWalker skeletonWalker = new AB2SkeletonWalker(skeleton, sampleResolution);
+        byte rgba[]=new byte[4];
+        getRgbaFromVector4(color, rgba);
+
+        skeletonWalker.walkSkeleton(new AB2SkeletonWalkerCallback() {
+
+            int imageXYZ[] = new int[3];
+
+            @Override
+            public void processPosition(AB2NeuronSkeleton.Node parentNode, AB2NeuronSkeleton.Node childNode, double edgeFraction) {
+                if (childNode==null) {
+                    getImageXYZFromSkeletonXYZ(parentNode.x(), parentNode.y(), parentNode.z(), imageXYZ);
+                    rawImage.setVoxel(imageXYZ[0], imageXYZ[1], imageXYZ[2], rgba);
+                    indexImage.setVoxel(imageXYZ[0], imageXYZ[1], imageXYZ[2], labelCount);
+                } else {
+                    Vector3 parentPosition=new Vector3( (float)parentNode.x(), (float)parentNode.y(), (float)parentNode.z());
+                    Vector3 childPosition=new Vector3((float)childNode.x(), (float)childNode.y(), (float)childNode.z());
+                    Vector3 edge=childPosition.minus(parentPosition);
+                    edge.multiplyScalar((float)edgeFraction);
+                    Vector3 position=parentPosition.plus(edge);
+                    getImageXYZFromSkeletonXYZ(position.getX(), position.getY(), position.getZ(), imageXYZ);
+                    rawImage.setVoxel(imageXYZ[0], imageXYZ[1], imageXYZ[2], rgba);
+                    indexImage.setVoxel(imageXYZ[0], imageXYZ[1], imageXYZ[2], labelCount);
+                }
+            }
+
+        });
     }
+
 
     public AB2Image3D_RGBA8UI getRawImage() {
         return rawImage;
@@ -45,6 +97,33 @@ public class AB2SimulatedVolumeGenerator {
 
     public void performDilation(double sensitivity, double noise) {
 
+    }
+
+    public int getMaxDim() {
+        int maxDim=xDim;
+        if (yDim>maxDim) {
+            maxDim=yDim;
+        }
+        if (zDim>maxDim) {
+            maxDim=zDim;
+        }
+        return maxDim;
+    }
+
+    public void getImageXYZFromSkeletonXYZ(double x, double y, double z, int[] imageXYZ) {
+        int iX=(int)(x*xDim);
+        int iY=(int)(y*yDim);
+        int iZ=(int)(z*zDim);
+        imageXYZ[0]=iX;
+        imageXYZ[1]=iY;
+        imageXYZ[2]=iZ;
+    }
+
+    public void getRgbaFromVector4(Vector4 color, byte[] rgba) {
+        rgba[0]=(byte)(color.get(0)*255f);
+        rgba[1]=(byte)(color.get(1)*255f);
+        rgba[2]=(byte)(color.get(2)*255f);
+        rgba[3]=(byte)(color.get(3)*255f);
     }
 
 }
