c0aa324ecbe1be53ff5f07d31fe2b806c6a2e80d
seandmurphy <seandmurphy@yahoo.com>
2017-10-19 21:05:43 -0400

    Adding logging to AB2 Domain Acceptor.
    Former-commit-id: 24080d930444bbc08c9f1fe56ac96a3af578b719

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/AB2DomainObjectAcceptor.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/AB2DomainObjectAcceptor.java
@@ -1,9 +1,15 @@
 package org.janelia.it.workstation.ab2;
 
 
+import java.util.List;
+
 import org.janelia.it.jacs.integration.framework.domain.DomainObjectAcceptor;
 import org.janelia.it.jacs.model.domain.DomainObject;
+import org.janelia.it.jacs.model.domain.sample.ObjectiveSample;
+import org.janelia.it.jacs.model.domain.sample.PipelineResult;
 import org.janelia.it.jacs.model.domain.sample.Sample;
+import org.janelia.it.jacs.model.domain.sample.SamplePipelineRun;
+import org.janelia.it.jacs.model.domain.support.DomainUtils;
 import org.janelia.it.workstation.ab2.model.AB2DomainObject;
 import org.openide.util.lookup.ServiceProvider;
 import org.slf4j.Logger;
@@ -21,6 +27,37 @@ public class AB2DomainObjectAcceptor implements DomainObjectAcceptor  {
 
     @Override
     public void acceptDomainObject(DomainObject dObj) {
+        logger.info("acceptDomainObject() dObj type="+dObj.getClass().getName());
+        if (dObj instanceof Sample) {
+            Sample sample=(Sample)dObj;
+            logger.info("Sample id="+sample.getId());
+            List<ObjectiveSample> objectiveSamples=sample.getObjectiveSamples();
+            if (objectiveSamples==null || objectiveSamples.size()==0) {
+                logger.info("No ObjectSamples found");
+            } else {
+                for (ObjectiveSample objectiveSample : objectiveSamples) {
+                    SamplePipelineRun samplePipelineRun=objectiveSample.getLatestRun();
+                    if (samplePipelineRun==null) {
+                        logger.info("No SamplePipelineRuns found");
+                    } else {
+                        PipelineResult pipelineResult = samplePipelineRun.getLatestResult();
+                        if (pipelineResult == null) {
+                            logger.info("No PipelineResults found");
+                        }
+                        else {
+                            String filepath = DomainUtils.getDefault3dImageFilePath(pipelineResult);
+                            if (filepath == null) {
+                                logger.info("Filepath for 3dImage is null");
+                            }
+                            else {
+                                logger.info("3D filepath=" + filepath);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
         AB2TopComponent ab2TopComponent=AB2TopComponent.findComp();
         if (ab2TopComponent!=null) {
             if (!ab2TopComponent.isOpened()) {
