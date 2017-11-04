a92ed8cbe8e7dce0614deca8a46753d2b6fb499a
seandmurphy <seandmurphy@yahoo.com>
2017-11-04 15:09:16 -0400

    Moved dim compute to shader.
    Former-commit-id: 57c09068860bd76cd2a0fc8b846b989da12143f2

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/shader/AB2Volume3DShader.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/shader/AB2Volume3DShader.java
@@ -3,15 +3,10 @@ package org.janelia.it.workstation.ab2.shader;
 import javax.media.opengl.GL4;
 
 import org.janelia.geometry3d.Matrix4;
-import org.janelia.geometry3d.Vector4;
+import org.janelia.geometry3d.Vector3;
 import org.janelia.it.workstation.ab2.gl.GLShaderProgram;
 
 public class AB2Volume3DShader extends GLShaderProgram {
-
-    public static final int TEXTURE_TYPE_NONE   =0;
-    public static final int TEXTURE_TYPE_2D_RGBA=1;
-    public static final int TEXTURE_TYPE_2D_R8  =2;
-    public static final int TEXTURE_TYPE_3D_RGBA=3;
 
     @Override
     public String getVertexShaderResourceName() {
@@ -31,6 +26,16 @@ public class AB2Volume3DShader extends GLShaderProgram {
     public void setTextureMVP3d(GL4 gl, Matrix4 mvp) {
         setUniformMatrix4fv(gl, "textureMvp3d", false, mvp.asArray());
         checkGlError(gl, "AB2Volume3DShader setTextureMVP3d() error");
+    }
+
+    public void setImageDim(GL4 gl, Vector3 dim) {
+        setUniform3v(gl, "image_dim", 1, dim.toArray());
+        checkGlError(gl, "AB2Volume3DShader setImageDim() error");
+    }
+
+    public void setImageMaxDIm(GL4 gl, float maxDim) {
+        setUniform(gl, "image_max_dim", maxDim);
+        checkGlError(gl, "AB2Volume3DShader setImageMaxDim() error");
     }
 
 }
