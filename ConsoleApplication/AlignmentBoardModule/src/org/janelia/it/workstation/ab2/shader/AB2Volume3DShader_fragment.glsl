0bb70b9a089b9e4906b9c78cc2e83652ee85567c
seandmurphy <seandmurphy@yahoo.com>
2017-11-04 23:51:51 -0400

    Moved dim compute to shader.
    Former-commit-id: 602bb95d7691be869f020ec46aeb3650d96c9ea8

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/shader/AB2Volume3DShader_fragment.glsl
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/shader/AB2Volume3DShader_fragment.glsl
@@ -1,4 +1,4 @@
-#version 410
+#version 450
 
 in vec3 tc_out;
 out vec4 color;
@@ -11,11 +11,23 @@ uniform sampler2DArray image_texture3d;
 void main() {
   //ivec3 imageSize3d=textureSize(image_texture3d, 0);
   float midMax=image_max_dim/2.0;
-  float xNormPos=(image_dim.x/2.0) + (tc_out.x*image_max_dim-midMax);
-  float yNormPos=(image_dim.y/2.0) + (tc_out.y*image_max_dim-midMax);
-  float zNormPos=(image_dim.z/2.0) + (tc_out.z*image_max_dim-midMax);
+  vec3 image_dim2=image_dim/2.0;
+  float xNormPos=image_dim2.x + (tc_out.x*image_max_dim-midMax);
+  float yNormPos=image_dim2.y + (tc_out.y*image_max_dim-midMax);
+  float zNormPos=image_dim2.z + (tc_out.z*image_max_dim-midMax);
 
   //ivec3 tci3=ivec3(imageSize3d.x*tc_out.x, imageSize3d.y*tc_out.y, imageSize3d.z*tc_out.z);
-  ivec3 tci3=ivec3(xNormPos, yNormPos, zNormPos);
-  color=texelFetch(image_texture3d, tci3, 0);
+  if (xNormPos<0.0 || yNormPos<0.0 || zNormPos<0.0) {
+     discard;
+  } else {
+     ivec3 tci3=ivec3(xNormPos, yNormPos, zNormPos);
+     if (tci3.x>image_dim.x || tci3.y>image_dim.y || tci3.z>image_dim.z) {
+        discard;
+     } else {
+        color=texelFetch(image_texture3d, tci3, 0);
+        if (color.r<0.05 && color.g<0.05 && color.b<0.05) {
+           discard;
+        }
+     }
+  }
 }
