467bce64b5b1993b2c72ca4a394db58c190a4009
seandmurphy <seandmurphy@yahoo.com>
2017-11-13 21:46:03 -0500

    Adding GLRegion code
    Former-commit-id: c7149e78a94766ffcbd3cfa33c7db1427a15a1bc

---
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/gl/GLRegion.java
@@ -0,0 +1,29 @@
+package org.janelia.it.workstation.ab2.gl;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+
+public class GLRegion implements GLEventListener {
+
+    public GLRegion(){}
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+
+    }
+}
---
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/gl/GLRegionManager.java
@@ -0,0 +1,4 @@
+package org.janelia.it.workstation.ab2.gl;
+
+public class GLRegionManager {
+}
