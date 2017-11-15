860b4a0182328d6376d86a2bfc3dbdf337bfeceb
seandmurphy <seandmurphy@yahoo.com>
2017-11-14 22:16:39 -0500

    Adding GLRegion code
    Former-commit-id: 36ab245051b915f5a5d7a11f33dce04582b602aa

--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/gl/GLRegion.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/gl/GLRegion.java
@@ -3,9 +3,7 @@ package org.janelia.it.workstation.ab2.gl;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 
-public class GLRegion implements GLEventListener {
-
-    public GLRegion(){}
+public abstract class GLRegion implements GLEventListener {
 
     @Override
     public void init(GLAutoDrawable drawable) {
@@ -26,4 +24,8 @@ public class GLRegion implements GLEventListener {
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 
     }
+
+    public abstract void setMode(String modeName, int x0, int y0, int x1, int y1);
+
+    public abstract String getMode();
 }
--- ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/gl/GLRegionManager.java
+++ ConsoleApplication/AlignmentBoardModule/src/org/janelia/it/workstation/ab2/gl/GLRegionManager.java
@@ -1,4 +1,26 @@
 package org.janelia.it.workstation.ab2.gl;
 
-public class GLRegionManager {
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+
+public abstract class GLRegionManager implements GLEventListener {
+    @Override
+    public void init(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+
+    }
 }
