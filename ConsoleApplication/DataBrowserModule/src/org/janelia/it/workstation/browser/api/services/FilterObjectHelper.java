aab6450b0d7c512d31a9f0991b6c63bfb2a7e1c0
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-02-08 16:16:57 -0500

    separated helpers into individual classes
    Former-commit-id: f4d12e22421fb51749b3608730939b9b4775b7a7

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/api/services/FilterObjectHelper.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/api/services/FilterObjectHelper.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 import org.janelia.it.jacs.integration.framework.domain.DomainObjectHelper;
 import org.janelia.it.workstation.browser.api.DomainMgr;
 import org.janelia.it.workstation.browser.api.DomainModel;
+import org.janelia.it.workstation.browser.gui.editor.FilterEditorPanel;
 import org.janelia.it.workstation.browser.gui.editor.ParentNodeSelectionEditor;
-import org.janelia.it.workstation.browser.gui.editor.TreeNodeEditorPanel;
-import org.janelia.it.workstation.browser.nodes.TreeNodeNode;
+import org.janelia.it.workstation.browser.nodes.FilterNode;
 import org.janelia.model.domain.DomainObject;
-import org.janelia.model.domain.workspace.TreeNode;
+import org.janelia.model.domain.gui.search.Filtering;
 import org.openide.nodes.ChildFactory;
 import org.openide.nodes.Node;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
- * A helper for working with TreeNodes and Filters.
+ * A helper for Filters.
  * 
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
 @ServiceProvider(service = DomainObjectHelper.class, path = DomainObjectHelper.DOMAIN_OBJECT_LOOKUP_PATH)
-public class TreeNodeObjectHelper implements DomainObjectHelper {
+public class FilterObjectHelper implements DomainObjectHelper {
 
     @Override
     public boolean isCompatible(DomainObject domainObject) {
@@ -29,7 +29,7 @@ public class TreeNodeObjectHelper implements DomainObjectHelper {
     
     @Override
     public boolean isCompatible(Class<? extends DomainObject> clazz) {
-        if (TreeNode.class.isAssignableFrom(clazz)) {
+        if (Filtering.class.isAssignableFrom(clazz)) {
             return true;
         }
         return false;
@@ -37,17 +37,17 @@ public class TreeNodeObjectHelper implements DomainObjectHelper {
 
     @Override
     public Node getNode(DomainObject domainObject, ChildFactory parentChildFactory) throws Exception {
-        return new TreeNodeNode(parentChildFactory, (TreeNode)domainObject);    
+        return new FilterNode(parentChildFactory, (Filtering)domainObject);
     }
 
     @Override
     public Class<? extends ParentNodeSelectionEditor<? extends DomainObject,?,?>> getEditorClass(DomainObject domainObject) {
-        return TreeNodeEditorPanel.class;
+        return FilterEditorPanel.class;
     }
     
     @Override   
     public String getLargeIcon(DomainObject domainObject) {
-        return "folder_large.png";
+        return "search_large.png";
     }
 
     @Override
@@ -58,7 +58,6 @@ public class TreeNodeObjectHelper implements DomainObjectHelper {
     @Override
     public void remove(DomainObject domainObject) throws Exception {
         DomainModel model = DomainMgr.getDomainMgr().getModel();
-        model.remove(Arrays.asList((TreeNode)domainObject));
+        model.remove(Arrays.asList(((Filtering)domainObject)));
     }
-
 }
