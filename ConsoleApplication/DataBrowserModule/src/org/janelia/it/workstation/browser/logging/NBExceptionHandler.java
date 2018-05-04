a1f2bfaece7eb281fcb44f091027c3487a2d06ff
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-05-04 08:51:01 -0400

    ignore out of memory issues for reporting
    Former-commit-id: 349e3bf2d73de0a05e95528b754cea757ad469d1

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/logging/NBExceptionHandler.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/logging/NBExceptionHandler.java
@@ -166,6 +166,11 @@ public class NBExceptionHandler extends Handler implements Callable<JButton>, Ac
         if (stacktrace.contains("No space left on device")) {
             return true;
         }
+
+        // Ignore memory issues, these do not represent bugs.
+        if (stacktrace.contains("java.lang.OutOfMemoryError") || stacktrace.contains("Java heap space")) {
+            return true;
+        }
         
         // Ignore all broken pipes, because these are usually caused by user initiated cancellation or network issues.
         if (stacktrace.contains("Caused by: java.io.IOException: Broken pipe")) {
