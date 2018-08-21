c90db428cedddd5a46d15709e332df3023b51ff9
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-08-21 08:51:35 -0400

    JW-37145: fixed NPE
    Former-commit-id: 9fe1fede53720276ecd0bce63f990fe14836fdbd

--- ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/gui/editor/SampleEditorPanel.java
+++ ConsoleApplication/DataBrowserModule/src/org/janelia/it/workstation/browser/gui/editor/SampleEditorPanel.java
@@ -1151,7 +1151,9 @@ public class SampleEditorPanel
             values.put("Objective", alignment.getObjective());
             values.put("Optical Resolution", alignment.getOpticalResolution());
             values.put("Message", alignment.getMessage());
-            values.put("Container Id", alignment.getContainerRef().getTargetId());
+            if (alignment.getContainerRef()!=null) {
+                values.put("Container Id", alignment.getContainerRef().getTargetId());
+            }
             
             Long bridgeParentAlignmentId = alignment.getBridgeParentAlignmentId();
             if (bridgeParentAlignmentId!=null) {
