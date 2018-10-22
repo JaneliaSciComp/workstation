0c2e3ed635b80480647db92e2e6619ad4d89e949
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-10-22 16:21:41 -0400

    removed unused code
    Former-commit-id: 8979f034929e841fa766bd8ea070a87729cfec57

--- archive/ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/framework/console/BrowserObserver.java
+++
@@ -1,8 +0,0 @@
-package org.janelia.it.workstation.gui.framework.console;
-
-public interface BrowserObserver {
-
-    public void openBrowserCountChanged(int browserCount);
-//  public void masterEditorChanged(String newMasterEditor, boolean subEditorAvailable);
-//  public void editorSpecificMenusChanged(JMenuItem[] menus);
-}
--- archive/ConsoleApplication/ConsoleModule/src/org/janelia/it/workstation/gui/framework/session_mgr/GenericModelListener.java
+++
@@ -1,7 +0,0 @@
-package org.janelia.it.workstation.gui.framework.session_mgr;
-
-public interface GenericModelListener {
-
-    public void modelPropertyChanged(Object key, Object oldValue, Object newValue);
-
-}
