6d7cb1efd83868dbef3cf63db811828707f33630
Cristian Goina <goinac@hhmi.org>
2018-10-25 18:30:26 -0400

    reformat
    Former-commit-id: 38a2b916bc9697c3eeea35d1ddb1b84af52449d3

--- ConsoleApplication/HortaTracer/src/org/janelia/horta/loader/HortaKtxLoader.java
+++ ConsoleApplication/HortaTracer/src/org/janelia/horta/loader/HortaKtxLoader.java
@@ -27,7 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.janelia.horta.loader;
 
 import java.io.IOException;
@@ -38,45 +37,42 @@ import org.apache.commons.io.FilenameUtils;
 import org.janelia.horta.actors.TetVolumeActor;
 import org.janelia.horta.blocks.KtxBlockLoadRunner;
 import org.janelia.horta.render.NeuronMPRenderer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Christopher Bruns
  */
-public class HortaKtxLoader implements FileTypeLoader
-{
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+public class HortaKtxLoader implements FileTypeLoader {
+
     private final NeuronMPRenderer renderer;
-    
+
     public HortaKtxLoader(NeuronMPRenderer renderer) {
         this.renderer = renderer;
     }
-    
+
     @Override
-    public boolean supports(DataSource source)
-    {
+    public boolean supports(DataSource source) {
         String ext = FilenameUtils.getExtension(source.getFileName()).toUpperCase();
-        if (ext.equals("KTX"))
+        if (ext.equals("KTX")) {
             return true;
+        }
         return false;
     }
 
     @Override
-    public boolean load(final DataSource source, FileHandler handler) throws IOException
-    {
+    public boolean load(final DataSource source, FileHandler handler) throws IOException {
         InputStream stream = source.getInputStream();
-        
+
         final KtxBlockLoadRunner loader = new KtxBlockLoadRunner(stream);
         loader.addObserver(new Observer() {
             @Override
             public void update(Observable o, Object arg) {
-                if (loader.state != KtxBlockLoadRunner.State.LOADED)
+                if (loader.state != KtxBlockLoadRunner.State.LOADED) {
                     return;
+                }
                 TetVolumeActor parentActor = TetVolumeActor.getInstance();
                 parentActor.addChild(loader.blockActor);
-                if ( ! renderer.containsVolumeActor(parentActor) ) { // just add singleton actor once...
+                if (!renderer.containsVolumeActor(parentActor)) { // just add singleton actor once...
                     parentActor.setBrightnessModel(renderer.getBrightnessModel());
                     renderer.addVolumeActor(parentActor);
                 }
--- ConsoleApplication/HortaTracer/src/org/janelia/horta/modelapi/SwcVertex.java
+++ ConsoleApplication/HortaTracer/src/org/janelia/horta/modelapi/SwcVertex.java
@@ -27,23 +27,20 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.janelia.horta.modelapi;
 
 import org.janelia.console.viewerapi.model.NeuronVertex;
 
 /**
  * Full interface for SWC format neuron reconstructions
+ *
  * @author Christopher Bruns
  */
-public interface SwcVertex extends NeuronVertex
-{
+public interface SwcVertex extends NeuronVertex {
     int getLabel();
     void setLabel(int label);
-
     int getTypeIndex();
     void setTypeIndex(int index);
-    
     SwcVertex getParent();
     void setParent(SwcVertex parent);
 }
