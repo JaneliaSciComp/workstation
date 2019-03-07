3cc05cf095d963f75581d316c44ef19a769096a4
porterbot <schauderd@janelia.hhmi.org>
2019-03-07 09:54:46 -0500

    added some functionality to user details panel
    Former-commit-id: fc4ba8d9c70aefee4ba73fb2e55c9ae2d77c4af6

--- ConsoleApplication/Administration/AdministrationModule/src/org/janelia/it/workstation/admin/UserDetailsPanel.java
+++ ConsoleApplication/Administration/AdministrationModule/src/org/janelia/it/workstation/admin/UserDetailsPanel.java
@@ -5,13 +5,21 @@ import java.awt.event.ActionListener;
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.table.AbstractTableModel;
 import org.janelia.it.workstation.browser.api.AccessManager;
 import org.janelia.it.workstation.browser.api.DomainMgr;
+import org.janelia.it.workstation.browser.api.facade.interfaces.SubjectFacade;
+import org.janelia.it.workstation.browser.gui.support.MouseHandler;
 import org.janelia.model.security.GroupRole;
 import org.janelia.model.security.Subject;
 import org.janelia.model.security.User;
@@ -24,18 +32,73 @@ import org.slf4j.LoggerFactory;
  *
  * @author schauderd
  */
-public class UserDetailsPanel {
+public class UserDetailsPanel extends JPanel {
     private static final Logger log = LoggerFactory.getLogger(UserManagementPanel.class);
+    JComboBox newGroupSelector;
+    UserDetailsTableModel userDetailsTableModel;
+    JTable userDetailsTable;
+    GroupRolesModel groupRolesModel;
+    JTable groupRolesTable;
     private int COLUMN_NAME = 1;
     private int COLUMN_VALUE = 2;
     
+    public UserDetailsPanel() {
+        setupUI();
+    }
+    
     public void setupUI() {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        
         // display table of user editable attributes
+        userDetailsTableModel = new UserDetailsTableModel();
+        userDetailsTable = new JTable(userDetailsTableModel);
+        add (userDetailsTable);
         
         // show group edit table with permissions for that group
+        groupRolesModel = new GroupRolesModel();
+        groupRolesTable = new JTable(groupRolesModel);
+        add (groupRolesTable);
         
-        // add groups pulldown selection
-
+        // add groups pulldown selection for groups this person is a member of
+        newGroupSelector = new JComboBox();
+        JButton newGroupButton = new JButton("Add New Group");
+        newGroupButton.addActionListener(event -> addNewGroup());
+        JPanel newGroupPanel = new JPanel();
+        newGroupPanel.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+        newGroupPanel.add(newGroupSelector);
+        newGroupPanel.add(newGroupButton);
+        add(newGroupPanel);               
+    }
+    
+    public void editUserDetails(User user) throws Exception {
+        Subject rawAdmin = AccessManager.getAccessManager().getActualSubject();
+        if (rawAdmin instanceof User) {                
+            User admin = (User) rawAdmin;
+            
+            // load user details
+            userDetailsTableModel.loadUser(user);           
+            groupRolesModel.loadGroupRoles(user);
+            
+            // load up the new groups available to add to this user
+            Iterator<UserGroupRole> groupRoles = user.getUserGroupRoles().iterator();
+            List<String> rawGroups = new ArrayList<>();
+            while (groupRoles.hasNext()) {
+                UserGroupRole groupRole = groupRoles.next();
+                if (groupRole.getRole() == GroupRole.Admin
+                        || groupRole.getRole() == GroupRole.Owner) {
+                    if (user.getRole(groupRole.getGroupKey()) == null) {
+                        rawGroups.add(groupRole.getGroupKey());
+                    }
+                }
+            }            
+        }
+    }
+                
+    
+    // adds the user to a new group   
+    private void addNewGroup() {
+        String groupKey = (String)newGroupSelector.getSelectedItem();
+        groupRolesModel.addNewGroup(groupKey);        
     }
     
     class UserDetailsTableModel extends AbstractTableModel implements ActionListener {
@@ -115,9 +178,16 @@ public class UserDetailsPanel {
         }
         
         public void loadGroupRoles(User user) {
+            data = new ArrayList<>();
             this.user = user;
             data.addAll(user.getUserGroupRoles());
             fireTableRowsInserted(0, getRowCount()-1);
+        }
+        
+        public void addNewGroup(String groupKey) {
+            user.setUserGroupRole(groupKey, GroupRole.Reader);
+            data.add(user.getRole(groupKey));            
+            fireTableRowsInserted(getRowCount()-2, getRowCount()-1);
         }
 
         public String getColumnName(int col) {
