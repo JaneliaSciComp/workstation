ec32d535f5db53a6200c726bbf0c810d934c9b93
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-04-13 16:58:24 -0400

    cleaned up license templates

--- modules/LargeVolumeViewer/src/main/java/org/janelia/workstation/gui/large_volume_viewer/ColorButtonPanel.java
+++ modules/LargeVolumeViewer/src/main/java/org/janelia/workstation/gui/large_volume_viewer/ColorButtonPanel.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.janelia.workstation.gui.large_volume_viewer;
 
 import org.janelia.console.viewerapi.model.ChannelColorModel;
