b1858b8133e294dbc144f79757adaa97d1f499d7
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-04-17 10:59:15 -0400

    JW-38645: Data inspector not populating when click on something in explorer

--- modules/Core/src/main/java/org/janelia/workstation/core/nodes/IdentifiableNodeSelectionModel.java
+++ modules/Core/src/main/java/org/janelia/workstation/core/nodes/IdentifiableNodeSelectionModel.java
@@ -1,8 +1,11 @@
 package org.janelia.workstation.core.nodes;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.janelia.model.domain.DomainObject;
 import org.janelia.workstation.core.events.Events;
+import org.janelia.workstation.core.events.selection.DomainObjectSelectionEvent;
 import org.janelia.workstation.core.events.selection.SelectionModel;
 import org.janelia.model.domain.interfaces.HasIdentifier;
 import org.openide.nodes.Node;
@@ -24,8 +27,17 @@ public class IdentifiableNodeSelectionModel extends SelectionModel<Node,Long> {
         if (nodes.size()==1) {
             Node node = nodes.get(0);
             if (node instanceof IdentifiableNode) {
+
+                // Post a NodeSelectionEvent to the event bus
                 IdentifiableNode identifiableNode = (IdentifiableNode)node;
                 Events.getInstance().postOnEventBus(new NodeSelectionEvent(getSource(), identifiableNode, select, clearAll, isUserDriven));
+
+                // If the node contains a domain object, also post a DomainObjectSelectionEvent
+                if (identifiableNode instanceof DomainObjectNode) {
+                    DomainObjectNode domainObjectNode = (DomainObjectNode)identifiableNode;
+                    DomainObject domainObject = domainObjectNode.getDomainObject();
+                    Events.getInstance().postOnEventBus(new DomainObjectSelectionEvent(getSource(), Arrays.asList(domainObject), select, clearAll, isUserDriven));
+                }
             }
             else {
                 log.debug("Model does not support node type: "+node);
