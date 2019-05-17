91b0e84fa655662e70c0ed54891e595da8dfc7e4
porterbot <schauderd@janelia.hhmi.org>
2019-05-17 14:17:00 -0400

    fixed JW-38368 and JW-38856

--- modules/LargeVolumeViewer/src/main/java/org/janelia/workstation/gui/task_workflow/TaskWorkflowViewTopComponent.java
+++ modules/LargeVolumeViewer/src/main/java/org/janelia/workstation/gui/task_workflow/TaskWorkflowViewTopComponent.java
@@ -158,7 +158,9 @@ public final class TaskWorkflowViewTopComponent extends TopComponent implements 
     }
     
     @Override
-    public void componentOpened() {   
+    public void componentOpened() {
+        if (annManager==null || annManager.getCurrentWorkspace()==null)
+            return;
         loadHistory();
     }
 
@@ -1001,7 +1003,7 @@ public final class TaskWorkflowViewTopComponent extends TopComponent implements 
      * retrieve all review tasks 
      */
     public List<TmReviewTask> retrieveTasks () {
-        if (annManager==null)
+        if (annManager==null || annManager.getCurrentWorkspace()==null)
             this.close();
         List<TmReviewTask> reviewTasks = null;
         TiledMicroscopeDomainMgr persistenceMgr = TiledMicroscopeDomainMgr.getDomainMgr();
