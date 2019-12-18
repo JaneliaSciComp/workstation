915bf3d3f410c3a44855eba88fdcc62bb0bf40b5
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-12-17 19:08:42 -0500

    JW-42595: quietly log cancellations

--- modules/Core/src/main/java/org/janelia/workstation/core/web/FileProxyService.java
+++ modules/Core/src/main/java/org/janelia/workstation/core/web/FileProxyService.java
@@ -7,7 +7,6 @@ import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -32,7 +31,7 @@ public class FileProxyService extends AbstractHandler {
     private static final int BUFFER_SIZE = 8196;
 
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
+            throws IOException {
 
         Pattern pattern = Pattern.compile("/(\\w+)(/.+)");
         Matcher matcher = pattern.matcher(request.getPathInfo());
@@ -78,7 +77,8 @@ public class FileProxyService extends AbstractHandler {
                 } else {
                     response.setStatus(404);
                 }
-            } else if ("GET".equals(method)) {
+            }
+            else if ("GET".equals(method)) {
                 response.setContentType("application/octet-stream");
                 Long nbytes = fileProxy.estimateSizeInBytes();
                 if (nbytes != null) {
@@ -89,23 +89,27 @@ public class FileProxyService extends AbstractHandler {
                 input = fileProxy.openContentStream();
                 output = response.getOutputStream();
                 Utils.copyNio(input, output, BUFFER_SIZE);
-            } else {
+            }
+            else {
                 throw new IllegalStateException("Unsupported method for Workstation file proxy service: "+method);
             }
 
-        }  catch (FileNotFoundException e) {
-            log.error("File not found: "+standardPath);
+        }
+        catch (FileNotFoundException e) {
+            log.warn("File not found: "+standardPath);
             response.setContentType("text/plain");
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             response.getWriter().print("File not found\n");
-        } catch (Exception e) {
-            log.error("Error proxying file: "+standardPath,e);
+        }
+        catch (Exception e) {
+            log.warn("Error proxying file: "+standardPath,e);
             response.setContentType("text/plain");
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.getWriter().print("Error proxying file\n");
             e.printStackTrace(response.getWriter());
             FrameworkAccess.handleExceptionQuietly(e);
-        } finally {
+        }
+        finally {
             if (input != null) {
                 try {
                     input.close();
