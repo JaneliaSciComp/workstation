eb064276166e49202e3bfef73c5bc7c5d7cc2e35
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-02-22 12:38:49 -0500

    JW-44350: Batch search improvements

--- modules/DataBrowser/src/main/java/org/janelia/workstation/browser/gui/dialogs/identifiers/IdentifiersVisualPanel1.java
+++ modules/DataBrowser/src/main/java/org/janelia/workstation/browser/gui/dialogs/identifiers/IdentifiersVisualPanel1.java
@@ -1,17 +1,16 @@
 package org.janelia.workstation.browser.gui.dialogs.identifiers;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-
+import org.janelia.model.domain.DomainObject;
+import org.janelia.model.domain.DomainUtils;
+import org.janelia.model.domain.sample.Sample;
+import org.janelia.workstation.common.gui.support.buttons.DropDownButton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
 
 public final class IdentifiersVisualPanel1 extends JPanel {
 
@@ -22,12 +21,15 @@ public final class IdentifiersVisualPanel1 extends JPanel {
     
     // GUI
     private JTextArea textArea;
+
+    // State
+    private Class<? extends DomainObject> currSearchClass = Sample.class;
     
     @Override
     public String getName() {
-        return "Paste Identifiers";
+        return "Search Parameters";
     }
-    
+
     /**
      * Creates new form DownloadVisualPanel1
      */
@@ -35,9 +37,22 @@ public final class IdentifiersVisualPanel1 extends JPanel {
         this.wizardPanel = wizardPanel;
         setLayout(new BorderLayout());
 
+        DropDownButton typeCriteriaButton = new DropDownButton();
+        ButtonGroup typeGroup = new ButtonGroup();
+        for (final Class<? extends DomainObject> searchClass : DomainUtils.getSearchClasses()) {
+            final String type = DomainUtils.getTypeName(searchClass);
+            JMenuItem menuItem = new JRadioButtonMenuItem(type, searchClass.equals(currSearchClass));
+            menuItem.addActionListener(e -> {
+                currSearchClass = searchClass;
+                typeCriteriaButton.setText("Result Type: " + DomainUtils.getTypeName(searchClass));
+            });
+            typeGroup.add(menuItem);
+            typeCriteriaButton.addMenuItem(menuItem);
+        }
+        // Set default label
+        typeCriteriaButton.setText("Result Type: " + DomainUtils.getTypeName(currSearchClass));
+
         JLabel instructions = new JLabel("Paste in the identifiers of the items you'd like to search for (line names, slide codes, GUIDs...), one identifier per line.");
-        
-        JPanel outerPanel = new JPanel(new BorderLayout());
 
         this.textArea = new JTextArea();
         textArea.getDocument().addDocumentListener(new DocumentListener() {
@@ -59,8 +74,15 @@ public final class IdentifiersVisualPanel1 extends JPanel {
         scrollPane.setViewportView(textArea);
         scrollPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, 300));
         scrollPane.getVerticalScrollBar().setUnitIncrement(16); // Increase scroll speed
-                
-        outerPanel.add(instructions, BorderLayout.NORTH);
+
+        JPanel upperPanel = new JPanel();
+        upperPanel.setLayout(new BoxLayout(upperPanel, BoxLayout.PAGE_AXIS));
+        upperPanel.add(typeCriteriaButton);
+        upperPanel.add(Box.createRigidArea(new Dimension(0,10)));
+        upperPanel.add(instructions);
+
+        JPanel outerPanel = new JPanel(new BorderLayout());
+        outerPanel.add(upperPanel, BorderLayout.NORTH);
         outerPanel.add(scrollPane, BorderLayout.CENTER);
         
         removeAll();
@@ -77,6 +99,10 @@ public final class IdentifiersVisualPanel1 extends JPanel {
 
     public String getText() {
         return textArea.getText();
+    }
+
+    public Class<? extends DomainObject> getCurrSearchClass() {
+        return currSearchClass;
     }
 
     private void triggerValidation() {
--- modules/DataBrowser/src/main/java/org/janelia/workstation/browser/gui/dialogs/identifiers/IdentifiersWizardState.java
+++ modules/DataBrowser/src/main/java/org/janelia/workstation/browser/gui/dialogs/identifiers/IdentifiersWizardState.java
@@ -1,8 +1,23 @@
 package org.janelia.workstation.browser.gui.dialogs.identifiers;
 
+import org.janelia.model.domain.DomainObject;
+import org.janelia.model.domain.Reference;
+
+import java.util.List;
+
 public class IdentifiersWizardState {
-    
+
+    private Class<? extends DomainObject> searchClass;
     private String text;
+    private List<Reference> results;
+
+    public Class<? extends DomainObject> getSearchClass() {
+        return searchClass;
+    }
+
+    public void setSearchClass(Class<? extends DomainObject> searchClass) {
+        this.searchClass = searchClass;
+    }
 
     public String getText() {
         return text;
@@ -10,5 +25,13 @@ public class IdentifiersWizardState {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public List<Reference> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Reference> results) {
+        this.results = results;
     }
 }
