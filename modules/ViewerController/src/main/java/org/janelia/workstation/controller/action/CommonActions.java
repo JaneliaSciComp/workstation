a800244823c3a86c6143de955aba5a1a2b0910c1
porterbot <schauderd@janelia.hhmi.org>
2020-05-06 09:31:53 -0400

    fixed a few event wirings for InfoPanel

--- modules/ViewerController/src/main/java/org/janelia/workstation/controller/action/CommonActions.java
+++ modules/ViewerController/src/main/java/org/janelia/workstation/controller/action/CommonActions.java
@@ -1,7 +1,14 @@
 package org.janelia.workstation.controller.action;
 
+import org.janelia.model.domain.tiledMicroscope.TmGeoAnnotation;
+import org.janelia.model.domain.tiledMicroscope.TmNeuronMetadata;
+import org.janelia.model.domain.tiledMicroscope.TmStructuredTextAnnotation;
 import org.janelia.model.domain.tiledMicroscope.TmWorkspace;
+import org.janelia.workstation.controller.ComponentUtil;
+import org.janelia.workstation.controller.NeuronManager;
+import org.janelia.workstation.controller.dialog.AddEditNoteDialog;
 import org.janelia.workstation.controller.model.TmModelManager;
+import org.janelia.workstation.core.workers.SimpleWorker;
 import org.janelia.workstation.integration.util.FrameworkAccess;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +18,54 @@ import java.awt.*;
 
 public class CommonActions {
     private static final Logger log = LoggerFactory.getLogger(CommonActions.class);
+
+    /**
+     * pop a dialog to add, edit, or delete note at the given annotation
+     */
+    public static void addEditNote(final Long neuronID, final Long annotationID) {
+        NeuronManager neuronManager = NeuronManager.getInstance();
+      //  if (!checkOwnership(neuronID))
+      //      return;
+
+        String noteText = neuronManager.getNote(neuronID, annotationID);
+        TmGeoAnnotation annotation = neuronManager.getGeoAnnotationFromID(neuronID, annotationID);
+        TmNeuronMetadata neuron = neuronManager.getNeuronFromNeuronID(neuronID);
+        AddEditNoteDialog testDialog = new AddEditNoteDialog(
+                null,
+                noteText,
+                neuronManager.getNeuronFromNeuronID(neuronID),
+                annotationID);
+        testDialog.setVisible(true);
+        if (testDialog.isSuccess()) {
+            SimpleWorker noteExecuter = new SimpleWorker() {
+                @Override
+                protected void doStuff() throws Exception {
+                    String resultText = testDialog.getOutputText().trim();
+                    if (resultText.length() > 0) {
+                        neuronManager.setNote(annotation, resultText);
+                    } else {
+                        final TmStructuredTextAnnotation textAnnotation = neuron.getStructuredTextAnnotationMap().get(annotationID);
+                        neuronManager.removeNote(neuronID, textAnnotation);
+                    }
+                }
+
+                @Override
+                protected void hadSuccess() {
+                    // nothing to see
+                }
+
+                @Override
+                protected void hadError(Throwable error) {
+                    FrameworkAccess.handleException(error);
+                }
+            };
+            noteExecuter.execute();
+
+        } else {
+            // canceled
+            return;
+        }
+    }
 
     public static void saveQuadViewColorModel() {
         log.info("saveQuadViewColorModel()");
--- modules/ViewerController/src/main/java/org/janelia/workstation/controller/model/TmSelectionState.java
+++ modules/ViewerController/src/main/java/org/janelia/workstation/controller/model/TmSelectionState.java
@@ -8,10 +8,7 @@ import org.janelia.model.domain.tiledMicroscope.TmWorkspace;
 import org.janelia.workstation.controller.AnnotationCategory;
 import org.janelia.workstation.controller.eventbus.SelectionEvent;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -19,41 +16,35 @@ import java.util.concurrent.CopyOnWriteArrayList;
  // support the capability to select multiple neurons and multiple annotation points at the same time
  */
 public class TmSelectionState {
-    private Map<String, List<DomainObject>> selections = new HashMap<>();
+    private Map<String, Object> selections;
     private AnnotationCategory selectionMode;
     public enum SelectionCode {
         PRIMARY, SECONDARY
     };
+    long foo = 0;
+    private static TmSelectionState instance;
 
-    public TmSelectionState() {
+    public static TmSelectionState getInstance() {
+        if (instance == null)
+            instance = new TmSelectionState();
+        return instance;
+    }
+
+    private TmSelectionState() {
         selections = new HashMap<>();
-        selections.put(SelectionCode.PRIMARY.name(),new CopyOnWriteArrayList());
     }
 
-    public List<DomainObject> getSelectionGroup(String selectionKey) {
-        return selections.get(selectionKey);
-    }
-
-    public void setSelectionGroup(String selectionKey, List<DomainObject> selectionGroup) {
-        selections.put(selectionKey, selectionGroup);
-    }
-
-    public List<DomainObject> getPrimarySelections() {
+    public Object getPrimarySelections() {
         return selections.get(SelectionCode.PRIMARY.name());
     }
 
-    public DomainObject getCurrentSelection() {
-        List<DomainObject> currentSelections = selections.get(SelectionCode.PRIMARY.name());
-        if (currentSelections.size()==1)
-            return currentSelections.get(0);
-        else
-            return null;
+    public Object getCurrentSelection() {
+        Object currentSelections = selections.get(SelectionCode.PRIMARY.name());
+        return currentSelections;
     }
 
-    public void setCurrentSelection(DomainObject selection) {
-        List currentSelections = selections.get(SelectionCode.PRIMARY.name());
-        currentSelections.clear();
-        currentSelections.add(selection);
+    public void setCurrentSelection(Object selection) {
+        selections.put(SelectionCode.PRIMARY.name(), selection);
     }
 
     public void clearAllSelections() {
