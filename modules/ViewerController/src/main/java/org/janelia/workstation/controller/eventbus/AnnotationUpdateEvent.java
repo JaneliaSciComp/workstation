02b26999e70555819fb1e400f3e388b38cbea41a
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2020-07-31 10:33:28 -0400

    fixed split annotation NPE

--- modules/ViewerController/src/main/java/org/janelia/workstation/controller/eventbus/AnnotationUpdateEvent.java
+++ modules/ViewerController/src/main/java/org/janelia/workstation/controller/eventbus/AnnotationUpdateEvent.java
@@ -4,7 +4,6 @@ import org.janelia.model.domain.tiledMicroscope.TmGeoAnnotation;
 import java.util.Collection;
 
 public class AnnotationUpdateEvent extends AnnotationEvent {
-    protected Collection<TmGeoAnnotation> annotations;
     public Collection<TmGeoAnnotation> getAnnotations() {
         return annotations;
     }
