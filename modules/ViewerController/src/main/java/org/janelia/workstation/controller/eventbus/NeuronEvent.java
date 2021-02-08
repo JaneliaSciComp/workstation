8e2f1e626a1510b655815a38197862d91a766b1f
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2021-02-08 15:01:45 -0500

    protected against null collection (should be empty)

--- modules/ViewerController/src/main/java/org/janelia/workstation/controller/eventbus/NeuronEvent.java
+++ modules/ViewerController/src/main/java/org/janelia/workstation/controller/eventbus/NeuronEvent.java
@@ -1,13 +1,19 @@
 package org.janelia.workstation.controller.eventbus;
 
 import org.janelia.model.domain.tiledMicroscope.TmNeuronMetadata;
+
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class NeuronEvent extends ViewerEvent {
     public NeuronEvent(Object source,
                        Collection<TmNeuronMetadata> neurons) {
         super(source);
-        this.neurons = neurons;
+        if (neurons != null) {
+            this.neurons = neurons;
+        } else {
+            this.neurons = new ArrayList<>();
+        }
     }
     public Collection<TmNeuronMetadata> getNeurons() {
         return neurons;
