7b2c296583bb72ff1ad9389391096e0e424be02c
Cristian Goina <goinac@hhmi.org>
2023-03-17 13:10:37 -0400

    upgrade javamail

--- modules/Core/src/main/java/org/janelia/workstation/core/util/MailHelper.java
+++ modules/Core/src/main/java/org/janelia/workstation/core/util/MailHelper.java
@@ -44,9 +44,9 @@ public class MailHelper {
             properties.put("mail.smtp.port", port);
             Authenticator authenticator;
             if (mailUser.trim().length() > 0 && mailPassword.trim().length() > 0) {
-                properties.put("mail.transport.protocol", "smtp");
                 properties.put("mail.smtp.auth", "true");
                 properties.put("mail.smtp.starttls.enable", "true");
+                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
                 authenticator = new Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(
